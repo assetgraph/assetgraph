822f5879981ae90a3d16c83e079911f87429d5cd
Andreas Lind Petersen <andreas@one.com>
2011-01-15 18:35:15 +0100

    Cleaned up assets/index.js a little.

--- assets/index.js
+++ assets/index.js
@@ -3,25 +3,24 @@ exports.byType = {};
 exports.typeByExtension = {};
 exports.typeByContentType = {};
 
-function register(config) {
-    var type = config.type,
-        Constructor = require('./' + type)[type];
+function register(type, extensions) {
+    var Constructor = require('./' + type)[type];
     Constructor.prototype.type = type;
-    exports.byType[config.type] = Constructor;
-    exports.typeByContentType[Constructor.prototype.contentType] = type; // Support path.extname
-    if (config.extensions) {
-        config.extensions.forEach(function (extension) {
+    exports.byType[type] = Constructor;
+    exports.typeByContentType[Constructor.prototype.contentType] = type;
+    if (extensions) {
+        extensions.forEach(function (extension) {
             exports.typeByExtension[extension] = type;
             exports.typeByExtension['.' + extension] = type; // Support path.extname
         });
     }
 }
 
-register({type: 'JavaScript', extensions: ['js']});
-register({type: 'HTML', extensions: ['template', 'html', 'xhtml', 'php']});
-register({type: 'CSS', extensions: ['css']});
-register({type: 'HTC', extensions: ['htc']});
-register({type: 'PNG', extensions: ['png']});
-register({type: 'GIF', extensions: ['gif']});
-register({type: 'JPEG', extensions: ['jpg', 'jpeg']});
-register({type: 'ICO', extensions: ['ico']});
+register('JavaScript', ['js']);
+register('HTML', ['template', 'html', 'xhtml', 'php']);
+register('CSS', ['css']);
+register('HTC', ['htc']);
+register('PNG', ['png']);
+register('GIF', ['gif']);
+register('JPEG', ['jpg', 'jpeg']);
+register('ICO', ['ico']);
