4eeb8ebb7df7fcc162a904cba3df553c2eba784e
Andreas Lind Petersen <andreas@one.com>
2011-01-16 03:42:27 +0100

    transforms/bundleRelations: Re-register the relations of the bundled assets to the bundle.

--- transforms/bundleRelations.js
+++ transforms/bundleRelations.js
@@ -4,8 +4,11 @@ var _ = require('underscore'),
     relations = require('../relations'),
     step = require('step');
 
+// Will break the existing assets pretty badly, be careful!
+
 exports.bundleRelations = function bundleRelations(siteGraph, relationsToBundle, cb) {
     assets[relationsToBundle[0].to.type].makeBundle(_.pluck(relationsToBundle, 'to'), error.passToFunction(cb, function (bundleAsset) {
+        bundleAsset.baseUrl = relationsToBundle[0].from.baseUrl;
         siteGraph.registerAsset(bundleAsset);
         var bundleRelation = new relations[relationsToBundle[0].type]({
             from: relationsToBundle[0].from,
@@ -14,6 +17,11 @@ exports.bundleRelations = function bundleRelations(siteGraph, relationsToBundle,
         siteGraph.attachAndRegisterRelation(bundleRelation, 'before', relationsToBundle[0]);
         relationsToBundle.forEach(function (relation) {
             siteGraph.detachAndUnregisterRelation(relation);
+            siteGraph.findRelations('from', relation.to).forEach(function (relrel) {
+                siteGraph.unregisterRelation(relrel);
+                relrel.from = bundleAsset;
+                siteGraph.registerRelation(relrel);
+            });
         });
         cb();
     }));
