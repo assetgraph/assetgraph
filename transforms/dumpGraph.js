c5f4bf72b552c0ecf2840d05e24d8f16caefa86c
Andreas Lind Petersen <andreas@one.com>
2011-01-25 15:03:37 +0100

    transforms.dumpGraph: Register and mark orphaned nodes.

--- transforms/dumpGraph.js
+++ transforms/dumpGraph.js
@@ -2,13 +2,25 @@ var path = require('path'),
     graphviz = require('graphviz');
 
 exports.dumpGraph = function dumpGraph(siteGraph, targetFileName, cb) {
-    var g = graphviz.digraph("G");
-    siteGraph.assets.forEach(function (asset) {
+    var g = graphviz.digraph("G"),
+        seenNodes = {};
+
+    function registerAssetAsNode(asset, namePrefix) {
+        seenNodes[asset.id] = true;
         g.addNode(asset.id.toString(), {
-            label: "\"" + (asset.url ? '' : 'i:') + asset.toString() + "\""
+            label: "\"" + (namePrefix || '') + (asset.url ? '' : 'i:') + asset.toString() + "\""
         });
+    }
+    siteGraph.assets.forEach(function (asset) {
+        registerAssetAsNode(asset);
     });
     siteGraph.relations.forEach(function (relation) {
+        if (!seenNodes[relation.from.id]) {
+            registerAssetAsNode(relation.from, 'o:');
+        }
+        if (!seenNodes[relation.to.id]) {
+            registerAssetAsNode(relation.to, 'o:');
+        }
         g.addEdge(relation.from.id.toString(), relation.to.id.toString());
     });
     g.output(path.extname(targetFileName).substr(1), targetFileName);
