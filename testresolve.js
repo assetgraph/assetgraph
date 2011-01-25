b731cd1103bd20f73c2f1c0df35028b370eeed67
Andreas Lind Petersen <andreas@one.com>
2011-01-25 22:09:15 +0100

    Removed old cruft.

--- testresolve.js
+++
@@ -1,4 +0,0 @@
-var resolvers = require('./resolvers');
-var resolver = new resolvers.FindParentDirectory({root:"../calendar-frontend/http-pub"});
-
-resolver.resolve({type: "js", url: "Ext-addDragAndDropIconsToIconsSprite.css", baseUrl: "js/cal/model"}, function (err, res){console.log(res);});
