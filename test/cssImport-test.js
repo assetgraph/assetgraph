665b6f3383738bd76e355ec40832e28e5cd5fd7f
Andreas Lind Petersen <andreas@one.com>
2011-02-16 14:55:48 +0100

    Added CSS @import support. Requires bleeding edge (>= d7ac379) CSSOM.
    Not yet supported by transforms.flattenStaticIncludes (needs to be reworked anyway).
    
    Closes #8

---
+++ test/cssImport-test.js
@@ -0,0 +1,35 @@
+var URL = require('url'),
+    vows = require('vows'),
+    assert = require('assert'),
+    AssetGraph = require('../lib/AssetGraph'),
+    transforms = require('../lib/transforms'),
+    assets = require('../lib/assets'),
+    relations = require('../lib/relations'),
+    step = require('step');
+
+vows.describe('getStaticUrl in JavaScript asset').addBatch({
+    'After loading test case': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/cssImport/'}).transform(
+                transforms.loadAssets('index.css'),
+                transforms.populate(),
+                transforms.escapeToCallback(this.callback)
+            );
+        },
+        'the graph should contain two CSS assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'CSS'}).length, 2);
+        },
+        'the graph should contain one CSSImport relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'CSSImport'}).length, 1);
+        },
+        'then removing the CSSImport relation': {
+            topic: function (assetGraph) {
+                assetGraph.findRelations({type: 'CSSImport'})[0].remove();
+                return assetGraph;
+            },
+            'there should only be a single rule left in index.css': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({url: /\/index.css$/})[0].parseTree.cssRules.length, 1);
+            }
+        }
+    }
+})['export'](module);
