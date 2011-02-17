070b350c8ebb9a1d6aeddbe9783341052737da9a
Andreas Lind Petersen <andreas@one.com>
2011-02-17 23:31:57 +0100

    assets.CSS.getOriginalRelations: Traverse the entire CSSOM tree.
    Closes #12

---
+++ test/cssMediaRule-test.js
@@ -0,0 +1,19 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    AssetGraph = require('../lib/AssetGraph'),
+    transforms = require('../lib/transforms');
+
+vows.describe('css @media rule').addBatch({
+    'After loading test case': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/cssMediaRule/'}).transform(
+                transforms.loadAssets('relationInMediaRule.css'),
+                transforms.populate(),
+                transforms.escapeToCallback(this.callback)
+            );
+        },
+        'the graph should contain two CSSBackgroundImage relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'CSSBackgroundImage'}).length, 2);
+        }
+    }
+})['export'](module);
