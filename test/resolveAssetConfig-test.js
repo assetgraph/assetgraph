1ebb2da667cc068c5c5e75e2111d4f5f41dea7f9
Andreas Lind Petersen <andreas@one.com>
2011-02-17 22:50:41 +0100

    Moved AssetGraph.resolveAssetConfig into its own file.
    Closes #1

--- test/resolveAssetConfig-test.js
+++ test/resolveAssetConfig-test.js
@@ -7,7 +7,7 @@ var vows = require('vows'),
 function resolveAssetConfig(assetConfig, fromUrl, cb) {
     return function () {
         var assetGraph = new AssetGraph({root: assetGraphRoot});
-        assetGraph.resolveAssetConfig(assetConfig, fromUrl || assetGraph.root, cb || this.callback);
+        assetGraph.resolver.resolve(assetConfig, fromUrl || assetGraph.root, cb || this.callback);
     };
 }
 
