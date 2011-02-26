assetgraph/assetgraph

commit e26b196901d7222105a268b03985e282fb11282a

Changed transforms to take an err object as the first argument to get rid of the transforms.escapeToCallback wtf.
None of the transforms actually perform any error handling yet, and AssetGraph.transform just throws if a transform passes an error to its callback. Still not sure how to go about the error handling.

lib/transforms/moveAssetsToDirectory.js
@@ -1,7 +1,7 @@
 var transforms = require('../transforms');
 
 exports.moveAssetsToDirectory = function (queryObj, url) {
-    return function moveAssetsToDirectory(assetGraph, cb) {
+    return function moveAssetsToDirectory(err, assetGraph, cb) {
         if (/^\//.test(url)) {
             url = assetGraph.resolver.root + url.substr(1);
         }

lib/transforms/moveAssetsToNewRoot.js
@@ -3,7 +3,7 @@ var URL = require('url'),
     transforms = require('../transforms');
 
 exports.moveAssetsToNewRoot = function (queryObj, newRoot, oldRoot) {
-    return function moveAssetsToNewRoot(assetGraph, cb) {
+    return function moveAssetsToNewRoot(err, assetGraph, cb) {
         oldRoot = fileUtils.ensureTrailingSlash(oldRoot || assetGraph.resolver.root);
         if (/^\//.test(newRoot)) {
             newRoot = assetGraph.resolver.root + url.substr(1);

lib/transforms/parallel.js
@@ -3,7 +3,7 @@ var _ = require('underscore'),
 
 exports.parallel = function () { // transform1, transform2, ...
     var childTransforms = _.toArray(arguments);
-    return function parallel(assetGraph, cb) {
+    return function parallel(err, assetGraph, cb) {
         if (childTransforms.length) {
             step(
                 function () {

lib/transforms/serial.js
@@ -2,7 +2,7 @@ var _ = require('underscore');
 
 exports.serial = function () { // transform1, transform2, ...
     var childTransforms = _.toArray(arguments);
-    return function serial(assetGraph, cb) {
+    return function serial(err, assetGraph, cb) {
         var nextTransformIndex = 0;
         function proceed() {
             if (nextTransformIndex < childTransforms.length) {

lib/AssetGraph.js
@@ -337,7 +337,7 @@ AssetGraph.prototype = {
         return subgraph;
     },
 
-    // Add your callback as the last transform at the end of the list (transforms.escapeToCallback yadda)
+    // Add your callback as the last transform at the end of the list
     transform: function () { // ...
         var that = this,
             transforms = _.toArray(arguments),
@@ -350,7 +350,7 @@ AssetGraph.prototype = {
                 var nextTransform = transforms[nextStepNo],
                     startTime = new Date();
                 nextStepNo += 1;
-                nextTransform(that, error.logAndExit(function () {
+                nextTransform(null, that, error.logAndExit(function () {
 //                    console.log(nextTransform.name + ': ' + (new Date() - startTime));
                     executeNextStep();
                 }));

lib/JavaScriptLoader.js
@@ -42,7 +42,7 @@ JavaScriptLoader.prototype.load = function (assetConfigs, cb) {
         transforms.populate(function (relation) {
             return relation.type === 'JavaScriptStaticInclude';
         }),
-        transforms.escapeToCallback(function (err, assetGraph) {
+        function (err, assetGraph) {
             if (!assetGraph.assets.length) {
                 return cb(null, that.window); // Very boring case
             }
@@ -74,7 +74,7 @@ JavaScriptLoader.prototype.load = function (assetConfigs, cb) {
                     cb(err, that.window);
                 })
             );
-        })
+        }
     );
 };
 

lib/transforms/addCacheManifestSinglePage.js
@@ -6,7 +6,7 @@ var URL = require('url'),
     query = require('../query');
 
 exports.addCacheManifestSinglePage = function (queryObj) {
-    return function addCacheManifestSinglePage(assetGraph, cb) {
+    return function addCacheManifestSinglePage(err, assetGraph, cb) {
         assetGraph.findAssets(queryObj).forEach(function (htmlAsset) {
             // Look for an existing manifests for htmlAsset:
             var manifest,

lib/transforms/addCacheManifestSiteMap.js
@@ -8,7 +8,7 @@ var URL = require('url'),
     query = require('../query');
 
 exports.addCacheManifestSiteMap = function (queryObj) {
-    return function addCacheManifestSiteMap(assetGraph, cb) {
+    return function addCacheManifestSiteMap(err, assetGraph, cb) {
         var allManifests = [],
             sharedManifest;
         assetGraph.findAssets(queryObj).forEach(function (htmlAsset) {

commit 0a64ba6706f503d87d0520b00fbbddb31fbc5512

Fixed a couple of errors introduced in e26b196 (forgetting to pass err as the first argument to a transform).

lib/transforms/moveAssetsToDirectory.js
@@ -10,6 +10,6 @@ exports.moveAssetsToDirectory = function (queryObj, url) {
         }
         transforms.moveAssets(queryObj, function (asset) {
             return url + asset.id + '.' + asset.defaultExtension;
-        })(assetGraph, cb);
+        })(null, assetGraph, cb);
     };
 };

lib/transforms/moveAssetsToNewRoot.js
@@ -11,6 +11,6 @@ exports.moveAssetsToNewRoot = function (queryObj, newRoot, oldRoot) {
         newRoot = fileUtils.ensureTrailingSlash(newRoot);
         transforms.moveAssets(queryObj, function (asset) {
             return URL.resolve(newRoot, fileUtils.buildRelativeUrl(oldRoot, asset.url));
-        })(assetGraph, cb);
+        })(null, assetGraph, cb);
     };
 };

lib/transforms/parallel.js
@@ -8,7 +8,7 @@ exports.parallel = function () { // transform1, transform2, ...
             step(
                 function () {
                     childTransforms.forEach(function (childTransform) {
-                        childTransform(assetGraph, this.parallel());
+                        childTransform(null, assetGraph, this.parallel());
                     }, this);
                 },
                 cb

lib/transforms/serial.js
@@ -8,7 +8,7 @@ exports.serial = function () { // transform1, transform2, ...
             if (nextTransformIndex < childTransforms.length) {
                 var nextTransform = childTransforms[nextTransformIndex];
                 nextTransformIndex += 1; // Don't break if nextTransform invokes callback in same tick
-                nextTransform(assetGraph, proceed);
+                nextTransform(null, assetGraph, proceed);
             } else {
                 process.nextTick(cb);
             }