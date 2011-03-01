assetgraph/assetgraph

commit 64bd1b75f090c8dc3c1fca87a99125627ac9998b

transforms.spriteBackgroundImages: Rewrote packers so that they don't clobber the existing imageInfos, but instead create copies with 'x' and 'y' properties added.
Fixes the 'tryAll' packer, which always returned a packingData object specifying best dimensions, but the image positions were from the last packing that was tried.

lib/transforms/spriteBackgroundImages/packers/vertical.js
@@ -1,17 +1,20 @@
+var _ = require('underscore');
+
 exports.pack = function (imageInfos) {
-    var y = 0,
-        width = 0,
-        previousBottomPadding = 0;
-    imageInfos.forEach(function (imageInfo) {
-        y += Math.max(previousBottomPadding, imageInfo.padding[0]);
-        imageInfo.y = y;
+    var previousBottomPadding = 0,
+        packingData = {
+            width: 0,
+            height: 0,
+            imageInfos: []
+        };
+    imageInfos.forEach(function (existingImageInfo) {
+        var imageInfo = _.extend({}, existingImageInfo);
+        packingData.height += Math.max(previousBottomPadding, imageInfo.padding[0]);
+        imageInfo.y = packingData.height;
         imageInfo.x = 0;
-        y += imageInfo.height;
+        packingData.height += imageInfo.height;
         previousBottomPadding = imageInfo.padding[2];
-        width = Math.max(width, imageInfo.width);
+        packingData.width = Math.max(packingData.width, imageInfo.width);
     });
-    return {
-        height: y,
-        width: width
-    };
+    return packingData;
 };

lib/transforms/spriteBackgroundImages.js
@@ -102,6 +102,7 @@ exports.spriteBackgroundImages = function () {
                             var packingData = require('./spriteBackgroundImages/packers/' + packerName).pack(imageInfos),
                                 canvas = new Canvas(packingData.width, packingData.height),
                                 ctx = canvas.getContext('2d');
+                            imageInfos = packingData.imageInfos;
                             if ('backgroundColor' in spriteInfo) {
                                 ctx.fillStyle = spriteInfo.imageBackgroundColor;
                                 ctx.fillRect(0, 0, canvas.width, canvas.height);

lib/transforms/spriteBackgroundImages/packers/horizontal.js
@@ -1,17 +1,21 @@
+var _ = require('underscore');
+
 exports.pack = function (imageInfos) {
-    var height = 0,
-        x = 0,
-        previousRightPadding = 0;
-    imageInfos.forEach(function (imageInfo) {
-        x += Math.max(previousRightPadding, imageInfo.padding[3]);
-        imageInfo.x = x;
+    var previousRightPadding = 0,
+        packingData = {
+            width: 0,
+            height: 0,
+            imageInfos: []
+        };
+    imageInfos.forEach(function (existingImageInfo) {
+        var imageInfo = _.extend({}, existingImageInfo);
+        packingData.width += Math.max(previousRightPadding, imageInfo.padding[3]);
+        imageInfo.x = packingData.width;
         imageInfo.y = 0;
-        x += imageInfo.width;
+        packingData.width += imageInfo.width;
         previousRightPadding = imageInfo.padding[1];
-        height = Math.max(height, imageInfo.height);
+        packingData.height = Math.max(packingData.height, imageInfo.height);
+        packingData.imageInfos.push(imageInfo);
     });
-    return {
-        width: x,
-        height: height
-    };
+    return packingData;
 };

lib/transforms/spriteBackgroundImages/packers/jimScott.js
@@ -1,5 +1,3 @@
-var _ = require('underscore');
-
 /*
  * Very quick adaptation of http://pollinimini.net/blog/rectangle-packing-2d-packing
  * which is a JavaScript version of Jim Scott's original algorithm found
@@ -8,6 +6,8 @@ var _ = require('underscore');
  * It uses a binary tree to partition the space of the parent rectangle and allocate
  * the passed rectangles by dividing the partitions into filled and empty.
  */
+
+var _ = require('underscore');
 
 function findCoords(node, width, height) {
     // If we are not at a leaf then go deeper
@@ -76,7 +76,14 @@ exports.pack = function (imageInfos, config) {
         return (b.width * b.height) - (a.width * a.height);
     });
 
-    imageInfos.forEach(function (imageInfo) {
+    var packingData = {
+        imageInfos: [],
+        width: 0,
+        height: 0
+    };
+
+    imageInfos.forEach(function (existingImageInfo) {
+        var imageInfo = _.extend({}, existingImageInfo);
         if (imageInfo.padding && imageInfo.padding.some(function (v) {return v > 0;})) {
             throw new Error("jimScott.pack: Sprite padding not supported");
         }
@@ -84,15 +91,13 @@ exports.pack = function (imageInfos, config) {
         var coords = findCoords(root, imageInfo.width, imageInfo.height);
         // If fitted then recalculate the used dimensions
         if (coords) {
-            packingWidth = Math.max(packingWidth, coords.x + imageInfo.width);
-            packingHeight = Math.max(packingHeight, coords.y + imageInfo.height);
+            packingData.width = Math.max(packingData.width, coords.x + imageInfo.width);
+            packingData.height = Math.max(packingData.height, coords.y + imageInfo.height);
         } else {
             throw new Error("jimScott.pack: Cannot fit image");
         }
         _.extend(imageInfo, coords);
+        packingData.imageInfos.push(imageInfo);
     });
-    return {
-        width: packingWidth,
-        height: packingHeight
-    };
+    return packingData;
 };

commit 4b5ea2cf3854ea0a279af2a18d31c12f80f3b566

spriteBackgroundImages: Fixed bug in 'vertical' packer (introduced in 64bd1b7).

lib/transforms/spriteBackgroundImages/packers/vertical.js
@@ -15,6 +15,7 @@ exports.pack = function (imageInfos) {
         packingData.height += imageInfo.height;
         previousBottomPadding = imageInfo.padding[2];
         packingData.width = Math.max(packingData.width, imageInfo.width);
+        packingData.imageInfos.push(imageInfo);
     });
     return packingData;
 };