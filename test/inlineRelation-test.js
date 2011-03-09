3cf7c97ebbbd319711463a825e2f20360e2d4264
Andreas Lind Petersen <andreas@one.com>
2011-03-09 09:22:14 +0100

    Added failing inlineRelations test (see #18).

---
+++ test/inlineRelation-test.js
@@ -0,0 +1,44 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    step = require('step'),
+    AssetGraph = require('../lib/AssetGraph'),
+    transforms = require('../lib/transforms'),
+    query = require('../lib/query');
+
+vows.describe('Inlining an asset').addBatch({
+    'After loading a test case with an HTML asset that has an external CSS asset in a conditional comment': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/inlineRelation/'}).transform(
+                transforms.loadAssets('index.html'),
+                transforms.populate(),
+                this.callback
+            );
+        },
+        'the graph should contain 4 assets': function (assetGraph) {
+            assert.equal(assetGraph.assets.length, 4);
+        },
+        'the graph should contain 2 HTML assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'HTML'}).length, 2);
+        },
+        'the graph should contain one CSS asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'CSS'}).length, 1);
+        },
+        'the graph should contain one PNG asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'PNG'}).length, 1);
+        },
+        'then inlining the CSS and serializing the HTML': {
+            topic: function (assetGraph) {
+                var callback = this.callback;
+                assetGraph.inlineRelation(assetGraph.findRelations({type: 'HTMLStyle'})[0], function (err) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    assetGraph.serializeAsset(assetGraph.findAssets({type: 'HTML', url: query.defined})[0], callback);
+                });
+            },
+            'the CSSBackgroundImage url should be relative to the HTML asset': function (src) {
+                assert.notEqual(src.indexOf("url(some/directory/foo.png)"), -1);
+            }
+        }
+    }
+})['export'](module);
