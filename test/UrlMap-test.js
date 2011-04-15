9af02c0b277dd42dcac9d4aed5844389a929dc3b
Andreas Lind Petersen <andreas@one.com>
2011-04-15 02:13:57 +0200

    Implemented uglifyAST.astToObj and added tests.

--- test/UrlMap-test.js
+++ test/UrlMap-test.js
@@ -8,7 +8,7 @@ vows.describe('UrlMap').addBatch({
         topic: function () {
             var urlMap = new UrlMap({
                 originalUrl: 'file:///foo/*/*.json',
-                wildCardValueAsts: [
+                wildCardValueASTs: [
                     uglify.parser.parse('(b || "abc")')[1][0][1], // strip 'toplevel' and 'stat' nodes
                     uglify.parser.parse('(a < 10 ? 123 : (a < 100 ? 456 : 789))')[1][0][1]
                 ]
@@ -18,10 +18,10 @@ vows.describe('UrlMap').addBatch({
             urlMap.addExpandedUrl('file:///foo/def/789.json');
             urlMap.addExpandedUrl('file:///foo/ghi/123.json');
             urlMap.mapExpandedUrl('file:///foo/def/789.json', 'bogus');
-            return urlMap.toExpressionAst();
+            return urlMap.toExpressionAST();
         },
-        'should DTRT': function (expressionAst) {
-            var src = uglify.uglify.gen_code(expressionAst);
+        'should DTRT': function (expressionAST) {
+            var src = uglify.uglify.gen_code(expressionAST);
             assert.equal(new Function('var a = 4, b = false; return ' + src + ';')(), 'file:///foo/abc/123.json');
             assert.equal(new Function('var a = 50, b = false; return ' + src + ';')(), 'file:///foo/abc/456.json');
             assert.equal(new Function('var a = 150, b = "def"; return ' + src + ';')(), 'bogus');
---
+++ test/uglifyAST-test.js
@@ -0,0 +1,72 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    uglify = require('uglify-js'),
+    uglifyAST = require('../lib/uglifyAST');
+
+function testCase(ast, obj) {
+    return {
+        'from ast to obj': {
+            topic: function () {
+                return uglifyAST.astToObj(ast);
+            },
+            'should produce the expected JavaScript object': function (_obj) {
+                assert.deepEqual(_obj, obj);
+            }
+        },
+        'from ast to obj through uglify.uglify.gen_code and eval': {
+            topic: function () {
+                return eval('(' + uglify.uglify.gen_code(['toplevel', [['stat', ast]]]) + ')');
+            },
+            'should produce the expected JavaScript object': function (_obj) {
+                assert.deepEqual(_obj, obj);
+            }
+        },
+        'from obj to ast': {
+            topic: function () {
+                return uglifyAST.objToAST(obj);
+            },
+            'should produce the expected AST': function (_ast) {
+                assert.deepEqual(_ast, ast);
+            }
+        },
+        'from obj to ast through JSON.stringify and uglify.parser': {
+            topic: function () {
+                return uglify.parser.parse('(' + JSON.stringify(obj) + ')')[1][0][1]; // Strip 'toplevel' and 'stat' nodes
+            },
+            'should produce the expected AST': function (_ast) {
+                assert.deepEqual(_ast, ast);
+            }
+        }
+    };
+}
+
+vows.describe('Converting JavaScript objects to Uglify ASTs and vice versa').addBatch({
+    'convert null': testCase(
+        ['name', 'null'],
+        null
+    ),
+    'convert false': testCase(
+        ['name', 'false'],
+        false
+    ),
+    'convert true': testCase(
+        ['name', 'true'],
+        true
+    ),
+    'convert string literal': testCase(
+        ['string', 'Hello, \u263a'],
+        'Hello, \u263a'
+    ),
+    'convert number literal': testCase(
+        ['num', 999],
+        999
+    ),
+    'convert array literal': testCase(
+        ['array', [['string', 'foo'], ['name', 'true'], ['array', [['name', 'null']]]]],
+        ['foo', true, [null]]
+    ),
+    'convert object literal': testCase(
+        ['object', [['keyName1', ['string', 'stringValue']], ['keyName2', ['array', [['name', 'null'], ['num', 10]]]]]],
+        {keyName1: 'stringValue', keyName2: [null, 10]}
+    )
+})['export'](module);
