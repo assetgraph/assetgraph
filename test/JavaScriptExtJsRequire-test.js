1a86c19d3adf5c9dd2bf3ef98c51493ee2217235
Andreas Lind Petersen <andreas@one.com>
2011-08-11 16:51:48 +0200

    Added support for detaching certain Ext.require relations.

--- test/JavaScriptExtJsRequire-test.js
+++ test/JavaScriptExtJsRequire-test.js
@@ -44,5 +44,50 @@ vows.describe('Ext.Loader, Ext.require and Ext.define (ExtJs 4)').addBatch({
                 ]);
             }
         }
+    },
+    'After loading the same test case again': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/JavaScriptExtJsRequire/'}).queue(
+                function (assetGraph) {
+                    assetGraph.resolverByProtocol.Foo = resolvers.fixedDirectory(urlTools.fsFilePathToFileUrl(__dirname + '/JavaScriptExtJsRequire/Foo'));
+                    assetGraph.resolverByProtocol.Quux = resolvers.fixedDirectory(urlTools.fsFilePathToFileUrl(__dirname + '/JavaScriptExtJsRequire/quuxroot'));
+                    assetGraph.resolverByProtocol.Ext = resolvers.extJs4Dir(urlTools.fsFilePathToFileUrl(__dirname + '/JavaScriptExtJsRequire/3rdparty/ext/src'));
+                },
+                transforms.loadAssets('index.html'),
+                transforms.populate()
+            ).run(this.callback);
+        },
+        // Need a vow here due to https://github.com/cloudhead/vows/issues/53
+        'the graph should contain 10 assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets().length, 10);
+        },
+        'then detach the first Ext.require relation from the inline script': {
+            topic: function (assetGraph) {
+                assetGraph.detachAndRemoveRelation(assetGraph.findRelations({from: assetGraph.findAssets({isInline: true, type: 'JavaScript'})[0]})[0]);
+                return assetGraph;
+            },
+            'the Ext.require(\'Ext.SomethingInCoreUtil\'); statement should be gone': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({isInline: true, type: 'JavaScript'})[0].text.indexOf("Ext.SomethingInCoreUtil"), -1);
+            },
+            'then detach the next Ext.require relation from the inline script': {
+                topic: function (assetGraph) {
+                    assetGraph.detachAndRemoveRelation(assetGraph.findRelations({from: assetGraph.findAssets({isInline: true, type: 'JavaScript'})[0]})[0]);
+                    return assetGraph;
+                },
+                'the Ext.require(\'Foo.Bar\', function () {...}); statement should have turned into Ext.require([], function () {...}': function (assetGraph) {
+                    console.warn(assetGraph.findAssets({isInline: true, type: 'JavaScript'})[0].text);
+                    assert.notEqual(assetGraph.findAssets({isInline: true, type: 'JavaScript'})[0].text.indexOf("Ext.require([]"), -1);
+                },
+                'then detach the next Ext.require relation from the inline script': {
+                    topic: function (assetGraph) {
+                        assetGraph.detachAndRemoveRelation(assetGraph.findRelations({from: assetGraph.findAssets({isInline: true, type: 'JavaScript'})[0]})[0]);
+                        return assetGraph;
+                    },
+                    'the Ext.require([\'Quux.Bar\'], function () {...}); statement should have turned into Ext.require([], function () {...}': function (assetGraph) {
+                        assert.equal(assetGraph.findAssets({isInline: true, type: 'JavaScript'})[0].text.match(/Ext\.require\(\[\]/g).length, 2);
+                    }
+                }
+            }
+        }
     }
 })['export'](module);
