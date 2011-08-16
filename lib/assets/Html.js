assetgraph/assetgraph

commit 692e6e193c375923c11f10bbc3ae8c7feb278b56

asset.(at|de)tachRelation => relation.(at|de)tach.

lib/assets/Html.js
@@ -316,48 +316,20 @@ extendWithGettersAndSetters(Html.prototype, {
         this.markDirty();
     },
 
-    attachRelation: function (relation, position, adjacentRelation) {
-        var document = this.parseTree;
-        position = position || 'first';
-        relation.from = this;
-        relation.node = relation.createNode(document);
-
-        if (position === 'first') {
-            if (relation.type === 'HtmlStyle') {
-                document.head.appendChild(relation.node);
-            } else if (relation.type === 'HtmlScript') {
-                var existingScriptNodes = document.getElementsByTagName('script');
-                if (existingScriptNodes.length > 0) {
-                    existingScriptNodes[0].parentNode.insertBefore(relation.node, existingScriptNodes[0]);
-                } else if (document.body) {
-                    document.body.insertBefore(relation.node, document.body.firstChild);
-                } else {
-                    document.head.appendChild(relation.node);
-                }
-            } else if (relation.type === 'HtmlCacheManifest') {
-                // Already attached to <html> as a result of relation.createNode above
-            } else {
-                throw "Html.attachRelation: position=first only supported for HtmlStyle, HtmlScript, and HtmlCacheManifest relations";
-            }
-        } else { // 'before' or 'after'
-            var parentNode = adjacentRelation.node.parentNode;
-            if (!parentNode) {
-                throw new Error("assets.Html.attachRelation: Adjacent relation has no parentNode property.");
-            }
-            if (position === 'after') {
-                parentNode.insertBefore(relation.node, adjacentRelation.node.nextSibling);
-            } else {
-                parentNode.insertBefore(relation.node, adjacentRelation.node);
-            }
+    // FIXME: Create superclass for Html* relations and put this method there?
+    _attachNode: function (node, position, adjacentNode) {
+        if (position !== 'before' && position !== 'after') {
+            throw new Error("assets.Html._attachNodeNextToOtherNode: The 'position' parameter must be either 'before' or 'after'");
         }
-        Text.prototype.attachRelation.call(this, relation, position, adjacentRelation);
-    },
-
-    detachRelation: function (relation) {
-        relation.node.parentNode.removeChild(relation.node);
-        delete relation.node;
-
-        Text.prototype.detachRelation.call(this, relation);
+        var parentNode = adjacentNode.parentNode;
+        if (!parentNode) {
+            throw new Error("assets.Html.attachRelation: Adjacent relation has no parentNode property.");
+        }
+        if (position === 'after') {
+            parentNode.insertBefore(node, adjacentNode.nextSibling);
+        } else {
+            parentNode.insertBefore(node, adjacentNode);
+        }
     }
 });
 

lib/relations/Base.js
@@ -17,6 +17,38 @@ Base.prototype = {
 
     toString: function () {
         return "[" + this.type + "/" + this.id + ": " + ((this.from && this.to) ? this.from.toString() + " => " + this.to.toString() : "unattached") + "]";
+    },
+
+    attach: function (asset, position, adjacentRelation) {
+        if (position === 'first') {
+            asset.outgoingRelations.unshift(this);
+        } else if (position === 'last') {
+            asset.outgoingRelations.push(this);
+        } else if (position === 'before' || position === 'after') { // before or after
+            if (!adjacentRelation) {
+                throw new Error("assets.Base.attachRelation: An adjacent relation is required for position === 'before'|'after'.");
+            }
+            if (this.type !== adjacentRelation.type) {
+                throw new Error("assets.Base.attachRelation: Relation must have same type as adjacent relation.");
+            }
+            var adjacentRelationIndex = this.from.outgoingRelations.indexOf(adjacentRelation);
+            if (adjacentRelationIndex === -1) {
+                throw new Error("asset.Base.attachRelation: Adjacent relation not found.");
+            }
+            this.from.outgoingRelations.splice(adjacentRelationIndex + (position === 'after' ? 1 : 0), 0, this);
+        } else {
+            throw new Error("asset.Base.attachRelation: 'position' parameter must be either 'first', 'last', 'before', or 'after'.");
+        }
+        this.from.markDirty();
+    },
+
+    detach: function () {
+        var indexInOutgoingRelations = this.from.outgoingRelations.indexOf(this);
+        if (indexInOutgoingRelations === -1) {
+            throw new Error("Css.detachRelation: Relation " + this + " not found in the outgoingRelations array");
+        }
+        this.from.outgoingRelations.splice(indexInOutgoingRelations, 1);
+        this.from.markDirty();
     }
 };
 

lib/relations/StaticUrlMapEntry.js
@@ -11,7 +11,78 @@ function StaticUrlMapEntry(config) {
 util.inherits(StaticUrlMapEntry, Base);
 
 extendWithGettersAndSetters(StaticUrlMapEntry.prototype, {
-    baseAssetQuery: {type: 'Html', isInline: false}
+    baseAssetQuery: {type: 'Html', isInline: false},
+
+    attach: function (asset, position, adjacentRelation) {
+        position = position || 'last'; // FIXME?
+        if (adjacentRelation && !this.wildCardValues) {
+            var wildCardValuesRegExp = new RegExp(adjacentRelation.href.replace(/^[^\*]*?\//, "").replace(/\./g, "\\.").replace(/\*\*?/g, "([^\/]+)") + "$"),
+                matchWildCardValues = this.to.url.match(wildCardValuesRegExp);
+            if (matchWildCardValues) {
+                this.wildCardValues = Array.prototype.slice.call(matchWildCardValues, 1);
+            } else {
+                throw new Error("relations.StaticUrlMapEntry.attach: Couldn't get wild card values");
+            }
+        }
+        // This happens after wildcard expansion, adjacentRelation is the one with the wildcard
+        if (this.wildCardValues) {
+            if (this.wildCardValues.length !== asset.wildCardValueAsts.length) {
+                throw new Error("assets.StaticUrlMap._attachRelationWithWildCardValues: Relation has the wrong number of wildcard values");
+            }
+            if (asset.wildCardValueAsts.length === 0) {
+                if (asset.relationByWildCardValues) {
+                    throw new Error("assets.StaticUrlMap._attachRelationWithWildCardValues: Cannot attach multiple relations to StaticUrlMap without wildcards.");
+                }
+                asset.relationByWildCardValues = this;
+            } else {
+                if (!asset.relationByWildCardValues) {
+                    asset.relationByWildCardValues = {};
+                } else if (asset.relationByWildCardValues.isRelation) {
+                    if (this.relationByWildCardValues.href.indexOf('*') === -1) {
+                        throw new Error("assets.StaticUrlMap._attachRelationWithWildCardValues: Cannot overwrite non-wildcard relation.");
+                    }
+                    this.relationByWildCardValues = {};
+                }
+                var cursor = asset.relationByWildCardValues;
+                this.wildCardValues.forEach(function (wildCardValue, i) {
+                    if (i === asset.wildCardValueAsts.length - 1) {
+                        cursor[wildCardValue] = this;
+                    } else {
+                        if (!(wildCardValue in cursor)) {
+                            cursor[wildCardValue] = {};
+                        }
+                        cursor = cursor[wildCardValue];
+                    }
+                }, this);
+            }
+        } else {
+            throw new Error("relations.StaticUrlMapEntry.attach: The relation must have an adjacentRelation or a wildCardValues property");
+        }
+        Base.prototype.attach.call(this, asset, position, adjacentRelation);
+    },
+
+    detach: function () {
+        if (this.wildCardValues) {
+            var cursor = this.from.relationByWildCardValues;
+            this.wildCardValues.forEach(function (wildCardValue, i) {
+                if (i === this.from.wildCardValueAsts.length - 1) {
+                    if (wildCardValue in cursor) {
+                        delete cursor[wildCardValue];
+                    } else {
+                        throw new Error("relation.StaticUrlMapEntry.detach: The relation's set of wild card values aren't mapped.");
+                    }
+                } else {
+                    if (!(wildCardValue in cursor)) {
+                        throw new Error("relation.StaticUrlMapEntry.detach: The relation's set of wild card values aren't mapped.");
+                    }
+                    cursor = cursor[wildCardValue];
+                }
+            });
+        } else if (this !== this.from.originalWildCardRelation) {
+            throw new Error("relation.StaticUrlMapEntry.detach: Cannot detach relation without wildCardValues property or asterisks in its href");
+        }
+        Base.prototype.detach.call(this);
+    }
 });
 
 module.exports = StaticUrlMapEntry;

lib/assets/Base.js
@@ -116,38 +116,6 @@ extendWithGettersAndSetters(Base.prototype, {
         return this._outgoingRelations;
     },
 
-    attachRelation: function (relation, position, adjacentRelation) {
-        if (position === 'first') {
-            this.outgoingRelations.unshift(relation);
-        } else if (position === 'last') {
-            this.outgoingRelations.push(relation);
-        } else if (position === 'before' || position === 'after') { // before or after
-            if (!adjacentRelation) {
-                throw new Error("assets.Base.attachRelation: An adjacent relation is required for position === 'before'|'after'.");
-            }
-            if (relation.type !== adjacentRelation.type) {
-                throw new Error("assets.Base.attachRelation: Relation must have same type as adjacent relation.");
-            }
-            var adjacentRelationIndex = this.outgoingRelations.indexOf(adjacentRelation);
-            if (adjacentRelationIndex === -1) {
-                throw new Error("asset.Base.attachRelation: Adjacent relation not found.");
-            }
-            this.outgoingRelations.splice(adjacentRelationIndex + (position === 'after' ? 1 : 0), 0, relation);
-        } else {
-            throw new Error("asset.Base.attachRelation: 'position' parameter must be either 'first', 'last', 'before', or 'after'.");
-        }
-        this.markDirty();
-    },
-
-    detachRelation: function (relation) {
-        var indexInOutgoingRelations = this.outgoingRelations.indexOf(relation);
-        if (indexInOutgoingRelations === -1) {
-            throw new Error("Css.detachRelation: Relation " + relation + " not found in the outgoingRelations array");
-        }
-        this.outgoingRelations.splice(indexInOutgoingRelations, 1);
-        this.markDirty();
-    },
-
     _clone: function () {
         return new this.constructor({
             rawSrc: this.rawSrc

examples/createBookmarklet.js
@@ -16,7 +16,7 @@ new AssetGraph().queue(
     transforms.minifyAssets(),
     function (assetGraph) {
         assetGraph.findRelations({type: 'JavaScriptOneInclude'}).forEach(function (relation) {
-            relation.from.detachRelation(relation);
+            relation.detach();
         });
         console.log('javascript:' + assetGraph.collectAssetsPostOrder(assetGraph.findAssets({isInitial: true})[0]).map(function (javaScriptAsset) {
             return javaScriptAsset.text;

lib/AssetGraph.js
@@ -59,28 +59,26 @@ _.extend(AssetGraph.prototype, {
             resolvedAssetConfigs = [resolvedAssetConfigs];
         }
         if (resolvedAssetConfigs.length === 0) {
-            originalRelation.from.detachRelation(originalRelation);
+            originalRelation.detach();
             originalRelation.from.markDirty();
             return [];
         } else if (resolvedAssetConfigs.length === 1) {
             originalRelation.to = resolvedAssetConfigs[0];
             return [originalRelation];
-        } else if (originalRelation.from.attachRelation) {
+        } else {
             var multipliedRelations = [];
             resolvedAssetConfigs.forEach(function (resolvedAssetConfig) {
                 var relation = new originalRelation.constructor({
                     from: originalRelation.from,
                     to: resolvedAssetConfig
                 });
-                relation.from.attachRelation(relation, 'before', originalRelation);
+                relation.attach(relation.from, 'before', originalRelation);
                 this.refreshRelationUrl(relation);
                 multipliedRelations.push(relation);
             }, this);
-            originalRelation.from.detachRelation(originalRelation);
+            originalRelation.detach();
             originalRelation.from.markDirty();
             return multipliedRelations;
-        } else {
-            throw new Error("assetConfig resolved to multiple, but " + originalRelation.type + " doesn't support attachRelation");
         }
     },
 
@@ -465,7 +463,7 @@ _.extend(AssetGraph.prototype, {
         if ((position === 'after' || position === 'before') && (!adjacentRelation || !adjacentRelation.isRelation)) {
             throw new Error("AssetGraph.attachAndAddRelation: Adjacent relation must be provided when position is 'before' or 'after'.");
         }
-        relation.from.attachRelation(relation, position, adjacentRelation);
+        relation.attach(relation.from, position, adjacentRelation);
         this.addRelation(relation, position, adjacentRelation);
         this.refreshRelationUrl(relation);
         relation.from.markDirty();
@@ -537,8 +535,8 @@ _.extend(AssetGraph.prototype, {
         if (!relation || !relation.isRelation) {
             throw new Error("AssetGraph.detachAndRemoveRelation: Not a relation: ", relation);
         }
+        relation.detach(relation);
         relation.from.markDirty();
-        relation.from.detachRelation(relation);
         this.removeRelation(relation);
     },
 

lib/assets/CacheManifest.js
@@ -115,35 +115,6 @@ extendWithGettersAndSetters(CacheManifest.prototype, {
             }, this);
         }
         return this._outgoingRelations;
-    },
-
-    attachRelation: function (relation, position, adjacentRelation) {
-        if (!relation.sectionName) {
-            relation.sectionName = 'CACHE';
-        }
-        // FIXME: Doesn't work with FALLBACK entries where there're two tokens.
-        _.extend(relation, {
-            from: this,
-            node: {
-                tokens: [relation.to.url] // Seems wrong
-            }
-        });
-        if (!(relation.sectionName in this.parseTree)) {
-            this.parseTree[relation.sectionName] = [];
-        }
-        this.parseTree[relation.sectionName].push(relation.node);
-
-        Text.prototype.attachRelation.call(this, relation, position, adjacentRelation);
-    },
-
-    detachRelation: function (relation) {
-        var indexInSection = this.parseTree[relation.sectionName].indexOf(relation.node);
-        if (indexInSection === -1 || indexInOutgoingRelations === -1) {
-            throw new Error("CacheManifest.detachRelation: Relation " + relation + " not found in the " + relation.sectionName + " section");
-        }
-        this.parseTree[relation.sectionName].splice(indexInSection, 1);
-
-        Text.prototype.detachRelation.call(this, relation);
     }
 });
 

lib/assets/Css.js
@@ -164,12 +164,6 @@ extendWithGettersAndSetters(Css.prototype, {
         this.isPretty = true;
         var parseTree = this.parseTree; // So markDirty removes this._text
         this.markDirty();
-    },
-
-    detachRelation: function (relation) {
-        relation.remove();
-
-        Text.prototype.detachRelation.call(this, relation);
     }
 });
 

commit aaf3eaf520c5e25df5b1a8975285da29eaa9015f

Fixed typos in error messages introduced in 692e6e1.

lib/assets/Html.js
@@ -319,11 +319,11 @@ extendWithGettersAndSetters(Html.prototype, {
     // FIXME: Create superclass for Html* relations and put this method there?
     _attachNode: function (node, position, adjacentNode) {
         if (position !== 'before' && position !== 'after') {
-            throw new Error("assets.Html._attachNodeNextToOtherNode: The 'position' parameter must be either 'before' or 'after'");
+            throw new Error("assets.Html._attachNode: The 'position' parameter must be either 'before' or 'after'");
         }
         var parentNode = adjacentNode.parentNode;
         if (!parentNode) {
-            throw new Error("assets.Html.attachRelation: Adjacent relation has no parentNode property.");
+            throw new Error("assets.Html._attachNode: Adjacent node has no parentNode.");
         }
         if (position === 'after') {
             parentNode.insertBefore(node, adjacentNode.nextSibling);

lib/relations/Base.js
@@ -26,18 +26,18 @@ Base.prototype = {
             asset.outgoingRelations.push(this);
         } else if (position === 'before' || position === 'after') { // before or after
             if (!adjacentRelation) {
-                throw new Error("assets.Base.attachRelation: An adjacent relation is required for position === 'before'|'after'.");
+                throw new Error("relations.Base.attach: An adjacent relation is required for position === 'before'|'after'.");
             }
             if (this.type !== adjacentRelation.type) {
-                throw new Error("assets.Base.attachRelation: Relation must have same type as adjacent relation.");
+                throw new Error("relations.Base.attach: Relation must have same type as adjacent relation.");
             }
             var adjacentRelationIndex = this.from.outgoingRelations.indexOf(adjacentRelation);
             if (adjacentRelationIndex === -1) {
-                throw new Error("asset.Base.attachRelation: Adjacent relation not found.");
+                throw new Error("relations.Base.attach: Adjacent relation not found.");
             }
             this.from.outgoingRelations.splice(adjacentRelationIndex + (position === 'after' ? 1 : 0), 0, this);
         } else {
-            throw new Error("asset.Base.attachRelation: 'position' parameter must be either 'first', 'last', 'before', or 'after'.");
+            throw new Error("relations.Base.attach: 'position' parameter must be either 'first', 'last', 'before', or 'after'.");
         }
         this.from.markDirty();
     },
@@ -45,7 +45,7 @@ Base.prototype = {
     detach: function () {
         var indexInOutgoingRelations = this.from.outgoingRelations.indexOf(this);
         if (indexInOutgoingRelations === -1) {
-            throw new Error("Css.detachRelation: Relation " + this + " not found in the outgoingRelations array");
+            throw new Error("relations.Base.detach: Relation " + this + " not found in the outgoingRelations array");
         }
         this.from.outgoingRelations.splice(indexInOutgoingRelations, 1);
         this.from.markDirty();

lib/relations/StaticUrlMapEntry.js
@@ -27,11 +27,11 @@ extendWithGettersAndSetters(StaticUrlMapEntry.prototype, {
         // This happens after wildcard expansion, adjacentRelation is the one with the wildcard
         if (this.wildCardValues) {
             if (this.wildCardValues.length !== asset.wildCardValueAsts.length) {
-                throw new Error("assets.StaticUrlMap._attachRelationWithWildCardValues: Relation has the wrong number of wildcard values");
+                throw new Error("relations.StaticUrlMapEntry.attach: Relation has the wrong number of wildcard values");
             }
             if (asset.wildCardValueAsts.length === 0) {
                 if (asset.relationByWildCardValues) {
-                    throw new Error("assets.StaticUrlMap._attachRelationWithWildCardValues: Cannot attach multiple relations to StaticUrlMap without wildcards.");
+                    throw new Error("relations.StaticUrlMapEntry.attach: Cannot attach multiple relations to StaticUrlMap without wildcards.");
                 }
                 asset.relationByWildCardValues = this;
             } else {
@@ -39,7 +39,7 @@ extendWithGettersAndSetters(StaticUrlMapEntry.prototype, {
                     asset.relationByWildCardValues = {};
                 } else if (asset.relationByWildCardValues.isRelation) {
                     if (this.relationByWildCardValues.href.indexOf('*') === -1) {
-                        throw new Error("assets.StaticUrlMap._attachRelationWithWildCardValues: Cannot overwrite non-wildcard relation.");
+                        throw new Error("relations.StaticUrlMapEntry.attach: Cannot overwrite non-wildcard relation.");
                     }
                     this.relationByWildCardValues = {};
                 }
@@ -69,17 +69,17 @@ extendWithGettersAndSetters(StaticUrlMapEntry.prototype, {
                     if (wildCardValue in cursor) {
                         delete cursor[wildCardValue];
                     } else {
-                        throw new Error("relation.StaticUrlMapEntry.detach: The relation's set of wild card values aren't mapped.");
+                        throw new Error("relations.StaticUrlMapEntry.detach: The relation's set of wild card values aren't mapped.");
                     }
                 } else {
                     if (!(wildCardValue in cursor)) {
-                        throw new Error("relation.StaticUrlMapEntry.detach: The relation's set of wild card values aren't mapped.");
+                        throw new Error("relations.StaticUrlMapEntry.detach: The relation's set of wild card values aren't mapped.");
                     }
                     cursor = cursor[wildCardValue];
                 }
             });
         } else if (this !== this.from.originalWildCardRelation) {
-            throw new Error("relation.StaticUrlMapEntry.detach: Cannot detach relation without wildCardValues property or asterisks in its href");
+            throw new Error("relations.StaticUrlMapEntry.detach: Cannot detach relation without wildCardValues property or asterisks in its href");
         }
         Base.prototype.detach.call(this);
     }