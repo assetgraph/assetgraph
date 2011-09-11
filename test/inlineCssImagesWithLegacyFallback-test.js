9f7d31f4a9edda7cf8b29179a8d0f7c57d549880
Andreas Lind Petersen <andreas@one.com>
2011-09-11 13:28:31 +0200

    Made transforms.inlineCssImagesWithLegacyFallback more configurable (-one-image-inline: inline + sizeThreshold).

--- test/inlineCssImagesWithLegacyFallback-test.js
+++ test/inlineCssImagesWithLegacyFallback-test.js
@@ -11,24 +11,24 @@ vows.describe('transforms.inlineCssImagesWithLegacyFallback').addBatch({
                 transforms.populate()
             ).run(this.callback);
         },
-        'the graph should contain 3 Css assets': function (assetGraph) {
-            assert.equal(assetGraph.findAssets({type: 'Css'}).length, 3);
+        'the graph should contain 4 Css assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Css'}).length, 4);
         },
-        'the graph should contain 3 images': function (assetGraph) {
-            assert.equal(assetGraph.findAssets({isImage: true}).length, 3);
+        'the graph should contain 4 images': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({isImage: true}).length, 4);
         },
         'then running the inlineCssImagesWithLegacyFallback transform': {
             topic: function (assetGraph) {
-                assetGraph.runTransform(transforms.inlineCssImagesWithLegacyFallback({isInitial: true}), this.callback);
+                assetGraph.runTransform(transforms.inlineCssImagesWithLegacyFallback({isInitial: true}, 32768 * 3/4), this.callback);
             },
-            'the graph should contain 4 Css assets': function (assetGraph) {
-                assert.equal(assetGraph.findAssets({type: 'Css'}).length, 4);
+            'the graph should contain 6 Css assets': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({type: 'Css'}).length, 6);
             },
-            'the graph should contain a HtmlConditionalComment relation': function (assetGraph) {
-                assert.equal(assetGraph.findRelations({type: 'HtmlConditionalComment'}).length, 1);
+            'the graph should contain 2 HtmlConditionalComment relation': function (assetGraph) {
+                assert.equal(assetGraph.findRelations({type: 'HtmlConditionalComment'}).length, 2);
             },
-            'the graph should contain two inline CssImage relations': function (assetGraph) {
-                assert.equal(assetGraph.findRelations({type: 'CssImage', to: {isInline: true}}).length, 2);
+            'the graph should contain 3 inline CssImage relations': function (assetGraph) {
+                assert.equal(assetGraph.findRelations({type: 'CssImage', to: {isInline: true}}).length, 3);
             }
         }
     }
