assetgraph/assetgraph

commit c438177e4cd98792e6f4da84faed86ed378a5361

Changed Asset.outgoingRelations property to work pretty much the same regardless of whether the asset is in a graph or not.
Removed indexing of Asset.to/Asset.from etc., it made the code too complicated and offered no real speedup, even with large graphs.

lib/assets/Asset.js
@@ -1,4 +1,3 @@
-
 /**
  * @class Asset
  * @extends EventEmitter
@@ -96,6 +95,8 @@ extendWithGettersAndSetters(Asset.prototype, {
      */
     isAsset: true,
 
+    isResolved: true,
+
     /**
      * asset.contentType
      * =================
@@ -173,6 +174,9 @@ extendWithGettersAndSetters(Asset.prototype, {
                     _.extend(that, metadata); // Might change url, contentType and encoding, and could add etag, lastModified, and date
                 }
                 delete that.rawSrcProxy;
+                if (that.assetGraph) {
+                    that.populate();
+                }
                 cb();
             }));
         } else {
@@ -249,7 +253,15 @@ extendWithGettersAndSetters(Asset.prototype, {
     },
 
     set rawSrc(rawSrc) {
+        var assetGraph = this.assetGraph;
+        if (assetGraph) {
+            assetGraph.removeAsset(this);
+        }
+        delete this._outgoingRelations;
         this._rawSrc = rawSrc;
+        if (assetGraph) {
+            assetGraph.addAsset(this);
+        }
         this.markDirty();
     },
 
@@ -316,7 +328,7 @@ extendWithGettersAndSetters(Asset.prototype, {
      * `markDirty()` will invalidate any cached serializations of the
      * asset.
      *
-     * @return {Relation} The asset itself (chaining-friendly).
+     * @return {Asset} The asset itself (chaining-friendly).
      * @api public
      */
     markDirty: function () {
@@ -340,14 +352,24 @@ extendWithGettersAndSetters(Asset.prototype, {
      * few subclasses (`Css`, `Html`, `CacheManifest`, and
      * `JavaScript`), all others return an empty array.
      *
+     * If the asset is part of an AssetGraph, it will be queried for
+     * the relations, otherwise the parse tree will be traversed.
+     *
      * @return {Array[Relation]} The outgoing relations.
      * @api public
      */
     get outgoingRelations() {
-        if (!this.assetGraph) {
-            throw new Error("Asset.outgoingRelations getter: Asset is not part of an AssetGraph");
+        if (this.assetGraph && this.isPopulated) {
+            return this.assetGraph.findRelations({from: this}, true);
         }
-        return this.assetGraph.findRelations({from: this});
+        if (!this._outgoingRelations) {
+            this._outgoingRelations = this.findOutgoingRelationsInParseTree();
+        }
+        return this._outgoingRelations;
+    },
+
+    findOutgoingRelationsInParseTree: function () {
+        return [];
     },
 
     /**
@@ -366,55 +388,50 @@ extendWithGettersAndSetters(Asset.prototype, {
         return this.assetGraph.findRelations({to: this});
     },
 
-    /**
-     * asset.createOriginalRelations()
-     * ===============================
-     *
-     * Traverses the parse tree of the asset and creates a relation
-     * object for each outgoing relation found.
-     *
-     * @return {Array[Relation]} The found relations.
-     * @api public
-     */
-    createOriginalRelations: function () {
-        return [];
+    get externalRelations() {
+        var externalRelations = [];
+        this.outgoingRelations.forEach(function (outgoingRelation) {
+            if (outgoingRelation.to.isInline) {
+                Array.prototype.push.apply(externalRelations, outgoingRelation.externalRelations);
+            } else {
+                externalRelations.push(outgoingRelation);
+            }
+        });
+        return externalRelations;
     },
 
     /**
-     * asset.populateRelationsToExistingAssets()
-     * =========================================
+     * asset.populate()
+     * ================
      *
-     * Go through the unresolved outgoing relations of an asset and
-     * add the ones that refer to assets that are already part of
-     * the graph. Recurses into inline assets.
+     * Go through the outgoing relations of the asset and add the ones
+     * that refer to assets that are already part of the
+     * graph. Recurses into inline assets.
      *
-     * You shouldn't need to worry about this. It's mostly useful as a
-     * helper for the `clone` method, but also used by the
-     * `compressJavaScript` transform.
+     * You shouldn't need to call this manually.
      *
      * @param {Asset} asset The asset.
+     * @return {Asset} The asset itself (chaining-friendly).
      * @api public
      */
-    populateRelationsToExistingAssets: function () {
+    populate: function () {
         if (!this.assetGraph) {
             throw new Error("Asset.populateRelationsToExistingAssets: Asset is not part of an AssetGraph");
         }
-        this.createOriginalRelations().forEach(function (outgoingRelation) {
-            var relativeUrl;
+        this.outgoingRelations.forEach(function (outgoingRelation) {
             if (outgoingRelation.to.url || typeof outgoingRelation.to === 'string') {
                 var targetUrl = urlTools.resolveUrl(outgoingRelation.baseAsset.url, outgoingRelation.to.url || outgoingRelation.to);
                 if (targetUrl in this.assetGraph.urlIndex) {
                     outgoingRelation.to = this.assetGraph.urlIndex[targetUrl];
-                    this.assetGraph.addRelation(outgoingRelation);
                 }
+                this.assetGraph.addRelation(outgoingRelation);
             } else {
                 // Inline asset
-                outgoingRelation.to = this.assetGraph.constructor.assets.create(outgoingRelation.to);
-                this.assetGraph.addAsset(outgoingRelation.to);
                 this.assetGraph.addRelation(outgoingRelation);
-                outgoingRelation.to.populateRelationsToExistingAssets();
+                this.assetGraph.addAsset(outgoingRelation.to);
             }
         }, this);
+        this.isPopulated = true;
     },
 
     /**
@@ -442,10 +459,11 @@ extendWithGettersAndSetters(Asset.prototype, {
         if (newAsset.id in this.assetGraph.idIndex) {
             throw new Error("asset.replaceWith: New asset is already in the graph: " + newAsset);
         }
-        this.assetGraph.addAsset(newAsset);
         this.incomingRelations.forEach(function (incomingRelation) {
-            incomingRelation.updateTarget(newAsset);
+            incomingRelation.to = newAsset;
+            incomingRelation.refreshHref();
         }, this);
+        this.assetGraph.addAsset(newAsset);
         this.assetGraph.removeAsset(this);
         if (this.url) {
             newAsset.url = this.url;
@@ -488,7 +506,6 @@ extendWithGettersAndSetters(Asset.prototype, {
             clone.url = urlTools.resolveUrl(this.url, clone.id + this.extension);
         }
         if (this.assetGraph) {
-            this.assetGraph.addAsset(clone);
             if (incomingRelations) {
                 if (incomingRelations.isRelation) {
                     incomingRelations = [incomingRelations];
@@ -498,15 +515,15 @@ extendWithGettersAndSetters(Asset.prototype, {
                         throw new Error("asset.clone(): Incoming relation is not a relation: ", incomingRelation);
                     }
                     if (incomingRelation.id in this.assetGraph.idIndex) {
-                        incomingRelation.updateTarget(clone);
+                        incomingRelation.to = clone;
                     } else {
                         incomingRelation.to = clone;
-                        this.assetGraph.addRelation(incomingRelation);
-                        incomingRelation.refreshHref();
+                        this.assetGraph.addRelation(incomingRelation); // Hmm, what about position and adjacentRelation?
                     }
+                    incomingRelation.refreshHref();
                 }, this);
             }
-            clone.populateRelationsToExistingAssets();
+            this.assetGraph.addAsset(clone);
         }
         return clone;
     },

lib/transforms/bundleAssets.js
@@ -16,14 +16,6 @@ function makeBundle(assetGraph, queryObj, assetsToBundle) {
         parseTree: assets[queryObj.type].mergeParseTrees(_.pluck(assetsToBundle, 'parseTree'))
     });
     bundleAsset.url = urlTools.resolveUrl(assetGraph.root, bundleAsset.id + bundleAsset.extension);
-    assetGraph.addAsset(bundleAsset);
-
-    var outgoingRelations = assetGraph.findRelations({from: assetsToBundle});
-    outgoingRelations.forEach(function (outgoingRelation) {
-        assetGraph.removeRelation(outgoingRelation);
-        outgoingRelation.from = bundleAsset;
-        assetGraph.addRelation(outgoingRelation, 'last'); // Preserves original order
-    });
 
     var seenReferringAssets = {};
     assetGraph.findRelations({type: queryObj.incoming.type, to: assetsToBundle}).forEach(function (incomingRelation) {
@@ -35,6 +27,8 @@ function makeBundle(assetGraph, queryObj, assetsToBundle) {
         }
         incomingRelation.detach();
     });
+
+    assetGraph.addAsset(bundleAsset);
 
     assetsToBundle.forEach(function (asset) {
         if (assetGraph.findRelations({to: asset}).length === 0) {

lib/AssetGraph.js
@@ -97,6 +97,7 @@ _.extend(AssetGraph.prototype, {
      * Add an asset to the graph.
      *
      * @param {Asset} asset The asset to add.
+     * @return {AssetGraph} The AssetGraph instance (chaining-friendly).
      * @api public
      */
     addAsset: function (asset) {
@@ -113,8 +114,12 @@ _.extend(AssetGraph.prototype, {
         this._addToIndices(asset);
         this._objInBaseAssetPaths[asset.id] = [];
         asset.assetGraph = this;
-
+        asset.isPopulated = false;
         this.emit('addAsset', asset);
+        if (asset.isLoaded) {
+            asset.populate();
+        }
+        return this;
     },
 
     /**
@@ -126,12 +131,14 @@ _.extend(AssetGraph.prototype, {
      *
      * @param {Asset} asset The asset to remove.
      * @param {Boolean} detachIncomingRelations Whether to also detach the incoming relations before removal (defaults to false).
+     * @return {AssetGraph} The AssetGraph instance (chaining-friendly).
      */
     removeAsset: function (asset, detachIncomingRelations) {
         if (!(asset.id in this.idIndex)) {
             throw new Error("AssetGraph.removeAsset: " + asset + " not in graph");
         }
-        this.findRelations({from: asset}, true).forEach(function (outgoingRelation) {
+        asset._outgoingRelations = this.findRelations({from: asset}, true);
+        asset._outgoingRelations.forEach(function (outgoingRelation) {
             this.removeRelation(outgoingRelation);
             if (outgoingRelation.to.isAsset && outgoingRelation.to.isInline) {
                 // Remove inline asset
@@ -162,6 +169,7 @@ _.extend(AssetGraph.prototype, {
         }, this);
         delete asset.assetGraph;
         this.emit('removeAsset', asset);
+        return this;
     },
 
     /**
@@ -178,6 +186,7 @@ _.extend(AssetGraph.prototype, {
      * @param {Relation} relation The relation to add to the graph.
      * @param {String} position "first", "last", "before", or "after".
      * @param {Relation} adjacentRelation The adjacent relation, mandatory if position is "before" or "after".
+     * @return {AssetGraph} The AssetGraph instance (chaining-friendly).
      * @api private
      */
     addRelation: function (relation, position, adjacentRelation) { // position and adjacentRelation are optional
@@ -193,8 +202,8 @@ _.extend(AssetGraph.prototype, {
         if (!(relation.from.id in this.idIndex)) {
             throw new Error("AssetGraph.addRelation: 'from' property of relation is not in the graph: " + relation.from);
         }
-        if (!relation.to || (!relation.to.isAsset && !relation.to.isResolved)) {
-            throw new Error("AssetGraph.addRelation: 'to' property of relation is not an asset or a resolved asset config: " + relation.to);
+        if (!relation.to) {
+            throw new Error("AssetGraph.addRelation: 'to' property of relation is missing");
         }
         position = position || 'last';
         relation.assetGraph = this;
@@ -218,6 +227,7 @@ _.extend(AssetGraph.prototype, {
         this._objInBaseAssetPaths[relation.id] = [];
         relation._registerBaseAssetPath();
         this.emit('addRelation', relation);
+        return this;
     },
 
     /**
@@ -228,6 +238,7 @@ _.extend(AssetGraph.prototype, {
      * to the source asset.
      *
      * @param {Relation} relation The relation to remove.
+     * @return {AssetGraph} The AssetGraph instance (chaining-friendly).
      * @api public
      */
     removeRelation: function (relation) {
@@ -255,6 +266,7 @@ _.extend(AssetGraph.prototype, {
         }, this);
         delete relation.assetGraph;
         this.emit('removeRelation', relation);
+        return this;
     },
 
     /**
@@ -341,6 +353,8 @@ _.extend(AssetGraph.prototype, {
      * intermediate relations are been removed and attached again.
      *
      * Will throw an error if the base asset for any relation couldn't be found.
+
+     * @return {AssetGraph} The AssetGraph instance (chaining-friendly).
      * @api public
      */
     recomputeBaseAssets: function () {
@@ -350,6 +364,7 @@ _.extend(AssetGraph.prototype, {
                 throw new Error("recomputeBaseAssets: Couldn't find base asset for " + relation);
             }
         }, this);
+        return this;
     },
 
     /**
@@ -386,33 +401,6 @@ _.extend(AssetGraph.prototype, {
             this.urlIndex[obj.url] = obj;
         }
         this.idIndex[obj.id] = obj;
-        AssetGraph.query.indices[objType].forEach(function (indexName) {
-            position = position || 'last';
-            if (indexName in obj) {
-                var type = typeof obj[indexName],
-                    key;
-                if (type === 'string' || type === 'number' || type === 'boolean') {
-                    key = obj[indexName];
-                } else if (type === 'object' && 'id' in obj[indexName]) {
-                    key = obj[indexName].id;
-                }
-                if (typeof key !== 'undefined') {
-                    var index = this._indices[objType][indexName];
-                    if (!(key in index)) {
-                        index[key] = [obj];
-                    } else {
-                        if (position === 'last') {
-                            index[key].push(obj);
-                        } else if (position === 'first') {
-                            index[key].unshift(obj);
-                        } else { // 'before' or 'after'
-                            var i = index[key].indexOf(adjacentObj) + (position === 'after' ? 1 : 0);
-                            index[key].splice(i, 0, obj);
-                        }
-                    }
-                }
-            }
-        }, this);
     },
 
     /**
@@ -443,40 +431,6 @@ _.extend(AssetGraph.prototype, {
             delete this.urlIndex[obj.url];
         }
         delete this.idIndex[obj.id];
-        AssetGraph.query.indices[objType].forEach(function (indexName) {
-            if (indexName in obj) {
-                var type = typeof obj[indexName],
-                    key;
-                if (type === 'string' || type === 'number' || type === 'boolean') {
-                    key = obj[indexName];
-                } else if (type === 'object' && 'id' in obj[indexName]) {
-                    key = obj[indexName].id;
-                }
-                if (typeof key !== 'undefined') {
-                    var index = this._indices[objType][indexName],
-                        i = index[key].indexOf(obj);
-                    if (i === -1) {
-                        throw "_removeFromIndices: object not found in index!";
-                    } else {
-                        index[key].splice(i, 1);
-                    }
-                }
-            }
-        }, this);
-    },
-
-    /**
-     * assetGraph._lookupIndex(indexType, indexName, value)
-     * ====================================================
-     *
-     * Look up a value in a relation or asset index.
-     *
-     * @param indexType The index type, either "asset" or "relation"
-     * @param indexName The index name, supported values: "type", "from" (relations only), "to" (relations only), "isInitial" (assets only).
-     * @api private
-     */
-    _lookupIndex: function (indexType, indexName, value) {
-        return this._indices[indexType][indexName][typeof value === 'object' ? value.id : value] || [];
     },
 
     /**

lib/assets/CacheManifest.js
@@ -85,15 +85,23 @@ extendWithGettersAndSetters(CacheManifest.prototype, {
         return this._text;
     },
 
-    set text(text) {
-        this._text = text;
+    set text(text) { // Duplicate of 'text' getter of assets.Text
+        var assetGraph = this.assetGraph;
+        if (assetGraph) {
+            assetGraph.removeAsset(this);
+        }
+        delete this._outgoingRelations;
         delete this._rawSrc;
         delete this._parseTree;
+        this._text = text;
+        if (assetGraph) {
+            assetGraph.addAsset(this);
+        }
         this.markDirty();
     },
 
-    createOriginalRelations: function () {
-        var originalRelations = [];
+    findOutgoingRelationsInParseTree: function () {
+        var outgoingRelations = [];
         // Traverse the sections in alphabetical order so the order of the relations is predictable
         Object.keys(this.parseTree).sort().forEach(function (sectionName) {
             var nodes = this.parseTree[sectionName];
@@ -102,7 +110,7 @@ extendWithGettersAndSetters(CacheManifest.prototype, {
                     // In the CACHE section there's only one token per entry, in FALLBACK
                     // there's the online URL followed by the offline URL (the one we want).
                     // Just pick the last token as the url.
-                    originalRelations.push(new relations.CacheManifestEntry({
+                    outgoingRelations.push(new relations.CacheManifestEntry({
                         from: this,
                         to: {
                             url: node.tokens[node.tokens.length - 1]
@@ -113,7 +121,7 @@ extendWithGettersAndSetters(CacheManifest.prototype, {
                 }, this);
             }
         }, this);
-        return originalRelations;
+        return outgoingRelations;
     }
 });
 

lib/assets/Css.js
@@ -62,10 +62,18 @@ extendWithGettersAndSetters(Css.prototype, {
         return this._text;
     },
 
-    set text(text) {
-        this._text = text;
+    set text(text) { // Duplicate of 'text' getter of assets.Text
+        var assetGraph = this.assetGraph;
+        if (assetGraph) {
+            assetGraph.removeAsset(this);
+        }
+        delete this._outgoingRelations;
         delete this._rawSrc;
         delete this._parseTree;
+        this._text = text;
+        if (assetGraph) {
+            assetGraph.addAsset(this);
+        }
         this.markDirty();
     },
 
@@ -84,11 +92,29 @@ extendWithGettersAndSetters(Css.prototype, {
         this.markDirty();
     },
 
-    createOriginalRelations: function () {
-        var originalRelations = [];
+    get isEmpty() {
+        return this.parseTree.cssRules.length === 0;
+    },
+
+    minify: function () {
+        this.isPretty = false;
+        var parseTree = this.parseTree; // So markDirty removes this._text
+        this.markDirty();
+        return this;
+    },
+
+    prettyPrint: function () {
+        this.isPretty = true;
+        var parseTree = this.parseTree; // So markDirty removes this._text
+        this.markDirty();
+        return this;
+    },
+
+    findOutgoingRelationsInParseTree: function () {
+        var outgoingRelations = [];
         Css.eachRuleInParseTree(this.parseTree, function (cssRule, parentRuleOrStylesheet) {
             if (cssRule.type === cssom.CSSRule.IMPORT_RULE) {
-                originalRelations.push(new relations.CssImport({
+                outgoingRelations.push(new relations.CssImport({
                     from: this,
                     to: {
                         url: cssRule.href
@@ -103,7 +129,7 @@ extendWithGettersAndSetters(Css.prototype, {
                     if (propertyWithImageUrlRegExp.test(style[i])) {
                         var urlMatch = style[propertyName].match(/\burl\((\'|\"|)([^\'\"]+?)\1\)/);
                         if (urlMatch) {
-                            originalRelations.push(new relations.CssImage({
+                            outgoingRelations.push(new relations.CssImage({
                                 from: this,
                                 to: {
                                     url: urlMatch[2]
@@ -121,7 +147,7 @@ extendWithGettersAndSetters(Css.prototype, {
                         // stuff like behavior(#default#VML) won't be treated as a relation.
                         var urlMatch = style[propertyName].match(/\burl\((\'|\"|)([^#\'\"][^\'\"]*?)\1\)/);
                         if (urlMatch) {
-                            originalRelations.push(new relations.CssBehavior({
+                            outgoingRelations.push(new relations.CssBehavior({
                                 from: this,
                                 to: {
                                     url: urlMatch[2]
@@ -138,7 +164,7 @@ extendWithGettersAndSetters(Css.prototype, {
                         // FIXME: Tokenize properly (parentheses in urls, etc.)
                         var alphaImageLoaderMatch = style[propertyName].match(/AlphaImageLoader\([^\)]*src=([\'\"])(.+?)(\1)[^\)]*\)/i);
                         if (alphaImageLoaderMatch) {
-                            originalRelations.push(new relations.CssAlphaImageLoader({
+                            outgoingRelations.push(new relations.CssAlphaImageLoader({
                                 from: this,
                                 to: {
                                     url: alphaImageLoaderMatch[2]
@@ -152,25 +178,7 @@ extendWithGettersAndSetters(Css.prototype, {
                 }, this);
             }
         }, this);
-        return originalRelations;
-    },
-
-    get isEmpty() {
-        return this.parseTree.cssRules.length === 0;
-    },
-
-    minify: function () {
-        this.isPretty = false;
-        var parseTree = this.parseTree; // So markDirty removes this._text
-        this.markDirty();
-        return this;
-    },
-
-    prettyPrint: function () {
-        this.isPretty = true;
-        var parseTree = this.parseTree; // So markDirty removes this._text
-        this.markDirty();
-        return this;
+        return outgoingRelations;
     }
 });
 

commit b8c7906c83f64708988d5177a4c08e29ac0ee4b2

Fixed transforms.bundleAssets errors introduced in c438177e.

lib/assets/Asset.js
@@ -482,16 +482,20 @@ extendWithGettersAndSetters(Asset.prototype, {
             throw new Error("Asset.populateRelationsToExistingAssets: Asset is not part of an AssetGraph");
         }
         this.outgoingRelations.forEach(function (outgoingRelation) {
-            if (outgoingRelation.to.url || typeof outgoingRelation.to === 'string') {
-                var targetUrl = urlTools.resolveUrl(outgoingRelation.baseAsset.url, outgoingRelation.to.url || outgoingRelation.to);
-                if (targetUrl in this.assetGraph.urlIndex) {
-                    outgoingRelation.to = this.assetGraph.urlIndex[targetUrl];
+            if (!outgoingRelation.assetGraph) {
+                if (outgoingRelation.to.url || typeof outgoingRelation.to === 'string') {
+                    var targetUrl = urlTools.resolveUrl(outgoingRelation.baseAsset.url, outgoingRelation.to.url || outgoingRelation.to);
+                    if (targetUrl in this.assetGraph.urlIndex) {
+                        outgoingRelation.to = this.assetGraph.urlIndex[targetUrl];
+                    }
+                    this.assetGraph.addRelation(outgoingRelation);
+                } else {
+                    // Inline asset
+                    this.assetGraph.addRelation(outgoingRelation);
+                    if (!outgoingRelation.to.assetGraph) {
+                        this.assetGraph.addAsset(outgoingRelation.to);
+                    }
                 }
-                this.assetGraph.addRelation(outgoingRelation);
-            } else {
-                // Inline asset
-                this.assetGraph.addRelation(outgoingRelation);
-                this.assetGraph.addAsset(outgoingRelation.to);
             }
         }, this);
         this.isPopulated = true;

lib/transforms/bundleAssets.js
@@ -16,6 +16,11 @@ function makeBundle(assetGraph, queryObj, assetsToBundle) {
         parseTree: assets[queryObj.type].mergeParseTrees(_.pluck(assetsToBundle, 'parseTree'))
     });
     bundleAsset.url = urlTools.resolveUrl(assetGraph.root, bundleAsset.id + bundleAsset.extension);
+    bundleAsset._outgoingRelations = assetGraph.findRelations({from: assetsToBundle});
+    bundleAsset._outgoingRelations.forEach(function (outgoingRelation) {
+        outgoingRelation.remove();
+        outgoingRelation.from = bundleAsset;
+    });
 
     var seenReferringAssets = {};
     assetGraph.findRelations({type: queryObj.incoming.type, to: assetsToBundle}).forEach(function (incomingRelation) {