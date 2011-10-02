2ee6bf83de00fa935d786868ce953f73cbea02eb
Andreas Lind Petersen <andreas@one.com>
2011-10-02 23:56:29 +0200

    Support percent-encoded octets in data: urls and fall back to text/plain; charset=us-ascii as per the spec.

--- test/dataUrl-test.js
+++ test/dataUrl-test.js
@@ -11,16 +11,36 @@ vows.describe('data: url').addBatch({
                 transforms.populate()
             ).run(this.callback);
         },
-        'the graph should contain 3 Html assets': function (assetGraph) {
-            assert.equal(assetGraph.findAssets({type: 'Html'}).length, 3);
+        'the graph should contain 8 assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets().length, 8);
         },
-        'the body of the first data: url Html should contain a smiley character': function (assetGraph) {
+        'the first data: url Html should contain a smiley character': function (assetGraph) {
             assert.equal(assetGraph.findAssets({type: 'Html'})[1].parseTree.body.firstChild.nodeValue,
                          "\u263a");
         },
-        'the body of the second data: url Html should contain some Danish characters': function (assetGraph) {
+        'the second data: url Html should contain some Danish characters': function (assetGraph) {
             assert.equal(assetGraph.findAssets({type: 'Html'})[2].parseTree.body.firstChild.nodeValue,
                          "æøå");
+        },
+        'the third data: url Html should contain something that looks like Html': function (assetGraph) {
+            assert.matches(assetGraph.findAssets({type: 'Html'})[3].text,
+                           /^<!DOCTYPE html>/);
+        },
+        'the first data: url Text should be decoded correctly': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Text'})[0].text,
+                         "ΩδΦ");
+        },
+        'the second data: url Text should be decoded correctly': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Text'})[1].text,
+                         "Hellö");
+        },
+        'the third data: url Text should be decoded correctly': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Text'})[2].text,
+                         "A brief note");
+        },
+        'the fourth data: url Text should be decoded correctly': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Text'})[3].text,
+                         "ΩδΦ");
         }
     }
 })['export'](module);
