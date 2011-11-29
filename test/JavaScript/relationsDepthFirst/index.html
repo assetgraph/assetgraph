assetgraph/assetgraph

commit 5bff8354d548071f04bc19e7e1e68e84d087bbc0

AMD/RequireJS: Fixed resolution of requires that don't specify an extension.

lib/relations/JavaScriptAmdRequire.js
@@ -14,10 +14,13 @@ extendWithGettersAndSetters(JavaScriptAmdRequire.prototype, {
     get baseAssetQuery() {
         var that = this;
         return function (asset) {
-            if (/^(?:[^!]*!)?\.\//.test(that.node[1])) { // Is it module-relative?
-                return asset.type === 'JavaScript';
+            // http://requirejs.org/docs/api.html#jsfiles
+            // An extension indicates that the href is relative to the Html asset:
+            if (/\.\w+$/.test(that.node[1])) {
+                return asset.type === 'Html';
             } else {
-                // Non-module-relative: Make sure that the href is relative to the first ancestor that has an incoming HtmlRequireJsMain relation:
+                // Relative to the main module: Find the first JavaScript with an incoming HtmlRequireJsMain relation,
+                // and stop if encountering an Html asset, just to be safe:
                 return asset.type === 'Html' || (asset.type === 'JavaScript' && asset.incomingRelations.some(function (incomingRelation) {
                     return incomingRelation.type === 'HtmlRequireJsMain';
                 }));

test/JavaScriptAmdRequire/withDataMain/scripts/helper/util.js
@@ -1,5 +1,5 @@
 define(["helper/yet/another"], function() {
-    //This function is called when scripts/helper/util.js is loaded.
+    // This function is called when scripts/helper/util.js is loaded.
     alert("This is helper/util speaking!");
 });
 
@@ -10,7 +10,6 @@ define('explicitName', ["helper/yet/another"], function () {
 // Callback-less require
 require(["helper/yet/another"]);
 
-// Relative to this module:
-require(["./yet/another"], function () {
+require(["./scripts/helper/yet/another.js"], function () {
     alert("Got the yet-another-helper using with the module-relative syntax");
 });

test/JavaScriptAmdRequire/withDataMain/scripts/main.js
@@ -1,3 +1,3 @@
-require(["helper/util", "text!foo.txt"], function() {
-    //This function is called when scripts/helper/util.js is loaded.
+require(["helper/util", "text!foo.txt"], function (theHelperUtil, theFooText) {
+    // This function is called when scripts/helper/util.js is loaded.
 });

commit c201955b7959c4b74fee91f09484c66aecd625e4

5bff8354 broke a JavaScript test unrelated to RequireJS/AMD, fixed.

test/JavaScript-test.js
@@ -39,12 +39,12 @@ vows.describe('assets.JavaScript').addBatch({
     'after loading test case with relations located at multiple levels in the parse tree': {
         topic: function () {
             new AssetGraph({root: __dirname + '/JavaScript/relationsDepthFirst/'}).queue(
-                transforms.loadAssets('relationsDepthFirst.js'),
+                transforms.loadAssets('index.html'),
                 transforms.populate()
             ).run(this.callback);
         },
         'the relations should be in depth-first order in the graph': function (assetGraph) {
-            assert.deepEqual(_.pluck(assetGraph.findRelations(), 'href'),
+            assert.deepEqual(_.pluck(assetGraph.findRelations({from: {type: 'JavaScript'}}), 'href'),
                              [
                                  'foo.js',
                                  'data.json',

test/JavaScript/relationsDepthFirst/index.html
@@ -1,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <script src='relationsDepthFirst.js'></script>
+</body>
+</html>