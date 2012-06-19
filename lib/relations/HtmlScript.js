assetgraph/assetgraph

commit 19ed6791702e3386064741db2bd716ad9c1b6caf

relations/HtmlScript: Escape occurrences of '</script>' so the string won't accidentally close the current script tag it is inside when inlined. See https://github.com/mishoo/UglifyJS/issues/164

lib/relations/HtmlScript.js
@@ -32,7 +32,7 @@ extendWithGettersAndSetters(HtmlScript.prototype, {
         while (this.node.firstChild) {
             this.node.removeChild(this.node.firstChild);
         }
-        this.node.appendChild(this.from.parseTree.createTextNode(this.to.text));
+        this.node.appendChild(this.from.parseTree.createTextNode(this.to.text.replace(/<\/script([\/ >])/gi, '<\\/script\1'))); // Safety hack for UglifyJS: https://github.com/mishoo/UglifyJS/issues/164
         this.from.markDirty();
         return this;
     },

commit ac8a12616cc0f1e094ceea45371597cb724bbdfe

relations/HtmlScript: Fixes error introduced in 19ed679

lib/relations/HtmlScript.js
@@ -32,7 +32,7 @@ extendWithGettersAndSetters(HtmlScript.prototype, {
         while (this.node.firstChild) {
             this.node.removeChild(this.node.firstChild);
         }
-        this.node.appendChild(this.from.parseTree.createTextNode(this.to.text.replace(/<\/script([\/ >])/gi, '<\\/script\1'))); // Safety hack for UglifyJS: https://github.com/mishoo/UglifyJS/issues/164
+        this.node.appendChild(this.from.parseTree.createTextNode(this.to.text.replace(/<\/script(?=[\/ >])/gi, '<\\/script'))); // Safety hack for UglifyJS: https://github.com/mishoo/UglifyJS/issues/164
         this.from.markDirty();
         return this;
     },