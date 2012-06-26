assetgraph/assetgraph

commit 641a8bca3155ca679755bc2d052fabff550b6f29

Fixed conversion between file: urls and file system paths (didn't account for spaces and non-ASCII chars).

lib/assets/index.js
@@ -144,9 +144,9 @@ assets.resolveConfig = function (assetConfig, fromUrl, assetGraph, cb) {
             }
         } else {
             if (/^\/(?:[^\/]|$)/.test(assetConfig.url) && /^file:/.test(fromUrl) && /^file:/.test(assetGraph.root)) {
-                assetConfig.url = urlTools.resolveUrl(assetGraph.root, assetConfig.url.substr(1));
+                assetConfig.url = urlTools.resolveUrl(assetGraph.root, encodeURI(assetConfig.url.substr(1)));
             } else {
-                assetConfig.url = urlTools.resolveUrl(fromUrl, assetConfig.url);
+                assetConfig.url = urlTools.resolveUrl(fromUrl, encodeURI(assetConfig.url));
             }
             assets.resolveConfig(assetConfig, fromUrl, assetGraph, cb);
         }

lib/relations/StaticUrlMapEntry.js
@@ -21,7 +21,7 @@ extendWithGettersAndSetters(StaticUrlMapEntry.prototype, {
         position = position || 'last'; // FIXME?
         if (adjacentRelation && !this.wildCardValues) {
             // ? wildcards disabled because they're indistinguishable from GET parameters. This should work though (after the [\.\+] replace): .replace(/\?/g, '(.)')
-            var wildCardValuesRegExp = new RegExp(adjacentRelation.to.url.replace(/[\.\+]/g, "\\$&").replace(/\/\*\*\//g, '(?:\/(.+?))?\/').replace(/\*/g, '([^/]*?)').replace(/\{([^\}]*)\}/g, function ($0, $1) {
+            var wildCardValuesRegExp = new RegExp("^" + adjacentRelation.to.url.replace(/%7B/g, '{').replace(/%7D/g, '}').replace(/[\.\+]/g, "\\$&").replace(/\/\*\*\//g, '(?:\/(.+?))?\/').replace(/\*/g, '([^/]*?)').replace(/\{([^\}]*)\}/g, function ($0, $1) {
                     // {foo,bar} type wildcards
                     return '(' + $1.replace(/,/g, '|') + ')';
                 }) + "$"),

lib/util/urlTools.js
@@ -40,20 +40,20 @@ urlTools.ensureTrailingSlash = function ensureTrailingSlash(str) { // url or pat
 };
 
 urlTools.fileUrlToFsPath = function fileUrlToFsPath(fileUrl) {
-    fileUrl = fileUrl.replace(/[?#].*$/, '');
-    return fileUrl.substr((process.platform === 'win32' ? "file:///" : 'file://').length).replace(/[#\?].*$/, '');
+    fileUrl = fileUrl.replace(/[?#].*$/, ''); // Strip query string and fragment identifier
+    return decodeURI(fileUrl).substr((process.platform === 'win32' ? "file:///" : 'file://').length).replace(/[#\?].*$/, '');
 };
 
 urlTools.fsFilePathToFileUrl = function fsFilePathToFileUrl(fsFilePath) {
     if (fsFilePath[0] === '/') {
-        return "file://" + fsFilePath;
+        return "file://" + encodeURI(fsFilePath);
     } else {
         // Interpret as relative to the current working dir
         fsFilePath = Path.resolve(process.cwd(), fsFilePath);
         if (process.platform === 'win32') {
-            return 'file:///' + fsFilePath.replace(/\\/g, '/');
+            return 'file:///' + encodeURI(fsFilePath.replace(/\\/g, '/'));
         } else {
-            return 'file://' + fsFilePath;
+            return 'file://' + encodeURI(fsFilePath);
         }
     }
 };

test/fileUrl-test.js
@@ -2,12 +2,23 @@ var vows = require('vows'),
     assert = require('assert'),
     AssetGraph = require('../lib/AssetGraph');
 
-// Asserts that protocol-relative urls are ignored when encountered in assets on file: urls
 vows.describe('file: urls').addBatch({
-    'After loading test case': {
+    'After loading test case with a protocol relative url in an asset loaded from a file:// url': {
         topic: function () {
             new AssetGraph({root: __dirname + '/fileUrl/'})
                 .loadAssets('index.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 1 asset': function (assetGraph) {
+            // Asserts that protocol-relative urls are ignored when encountered in assets on file: urls
+            assert.equal(assetGraph.findAssets().length, 1);
+        }
+    },
+    'After loading test case with non-ASCII file names': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/fileUrl/'})
+                .loadAssets('spaces, unsafe chars & ñøń-ÃßÇ¡¡.html')
                 .populate()
                 .run(this.callback);
         },

test/fileUrl/spaces, unsafe chars & ñøń-ÃßÇ¡¡.html
@@ -1,0 +1,6 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+    <a href="spaces,%20unsafe%20chars%20&%20%C3%B1%C3%B8%C5%84-%C3%83%C3%9F%C3%87%C2%A1%C2%A1.html">Myself</a>
+</body>

commit 7a9c56271534fdf296bc0ec80cf04e70f6c649b6

assets.resolveConfig: Only call encodeURI when resolving a string (bug introduced in 641a8bc).

lib/assets/index.js
@@ -118,7 +118,7 @@ assets.resolveConfig = function (assetConfig, fromUrl, assetGraph, cb) {
             ['catch'](cb);
     }
     if (typeof assetConfig === 'string') {
-        assetConfig = {url: assetConfig};
+        assetConfig = {url: encodeURI(assetConfig)};
     }
     if (assetConfig.isAsset || assetConfig.isResolved) {
         // Almost done, add .type property if possible (this is all we can do without actually fetching the asset):
@@ -145,9 +145,9 @@ assets.resolveConfig = function (assetConfig, fromUrl, assetGraph, cb) {
             }
         } else {
             if (/^\/(?:[^\/]|$)/.test(assetConfig.url) && /^file:/.test(fromUrl) && /^file:/.test(assetGraph.root)) {
-                assetConfig.url = urlTools.resolveUrl(assetGraph.root, encodeURI(assetConfig.url.substr(1)));
+                assetConfig.url = urlTools.resolveUrl(assetGraph.root, assetConfig.url.substr(1));
             } else {
-                assetConfig.url = urlTools.resolveUrl(fromUrl, encodeURI(assetConfig.url));
+                assetConfig.url = urlTools.resolveUrl(fromUrl, assetConfig.url);
             }
             assets.resolveConfig(assetConfig, fromUrl, assetGraph, cb);
         }