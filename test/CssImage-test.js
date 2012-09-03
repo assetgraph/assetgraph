25d1e0577b5d3e527e55a729eff278dd1a0721b3
Andreas Lind Petersen <andreas@one.com>
2012-09-03 17:18:00 +0200

    CssImage.href setter: Don't lose the !important suffix.

--- test/CssImage-test.js
+++ test/CssImage-test.js
@@ -1,5 +1,6 @@
 var vows = require('vows'),
     assert = require('assert'),
+    _ = require('underscore'),
     AssetGraph = require('../lib/AssetGraph');
 
 vows.describe('relations.CssImage').addBatch({
@@ -10,8 +11,31 @@ vows.describe('relations.CssImage').addBatch({
                 .populate()
                 .run(this.callback);
         },
-        'the graph should contain 9 CssImage relations': function (assetGraph) {
-            assert.equal(assetGraph.findRelations({type: 'CssImage'}).length, 9);
+        'the graph should contain 10 CssImage relations': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'CssImage'}).length, 10);
+        },
+        'then move foo.png to a different url': {
+            topic: function (assetGraph) {
+                assetGraph.findAssets({url: /\/foo\.png$/})[0].url = assetGraph.root + 'dir/foo2.png';
+                return assetGraph;
+            },
+            'the references to it should be updated': function (assetGraph) {
+                assert.deepEqual(_.pluck(assetGraph.findRelations({to: assetGraph.findAssets({url: /\/foo2\.png$/})}), 'href'), [
+                    'dir/foo2.png',
+                    'dir/foo2.png',
+                    'dir/foo2.png',
+                    'dir/foo2.png',
+                    'dir/foo2.png',
+                    'dir/foo2.png',
+                    'dir/foo2.png',
+                    'dir/foo2.png',
+                    'dir/foo2.png',
+                    'dir/foo2.png'
+                ]);
+            },
+            'the !important marker on the .baz background-image should still be there': function (assetGraph) {
+                assert.matches(assetGraph.findAssets({url: /\/index\.css$/})[0].text, /\.baz{background-image:url\(dir\/foo2\.png\)!important/);
+            }
         }
     }
 })['export'](module);
