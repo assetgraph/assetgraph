db7612553a3d8c69f8eb74418006f7dbd835c79e
Andreas Lind Petersen <andreas@one.com>
2012-09-14 11:15:55 +0200

    Model <script type='text/html'> templates as relations and assets.

---
+++ test/HtmlInlineScriptTemplate-test.js
@@ -0,0 +1,38 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    AssetGraph = require('../lib/AssetGraph');
+
+vows.describe('HtmlInlineScriptTemplate test').addBatch({
+    'After loading test case': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/HtmlInlineScriptTemplate/'})
+                .loadAssets('index.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 2 assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets().length, 2);
+        },
+        'the graph should contain 2 Html asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Html'}).length, 2);
+        },
+        'the graph should contain 1 HtmlInlineScriptTemplate relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlInlineScriptTemplate'}).length, 1);
+        },
+        'the HtmlInlineScriptTemplate relation should contain the right text': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlInlineScriptTemplate'})[0].to.text, '<div></div>');
+        },
+        'then manipulating the inline Html': {
+            topic: function (assetGraph) {
+                var inlineHtml = assetGraph.findAssets({type: 'Html', isInline: true})[0],
+                    document = inlineHtml.parseTree;
+                document.firstChild.appendChild(document.createTextNode('hello!'));
+                inlineHtml.markDirty();
+                return assetGraph;
+            },
+            'the text of the outer Html asset should contain the template': function (assetGraph) {
+                assert.matches(assetGraph.findAssets({type: 'Html', isInline: false})[0].text, /<div>hello!<\/div>/);
+            }
+        }
+    }
+})['export'](module);
