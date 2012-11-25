bc7a6e30fe4b06e24a7ffed78df84c5fd471b820
Andreas Lind Petersen <andreas@one.com>
2012-11-25 22:48:33 +0100

    Model <link rel="search" href="..."> as a relation. Fixes #86.

---
+++ test/HtmlSearchLink-test.js
@@ -0,0 +1,20 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    AssetGraph = require('../lib/AssetGraph');
+
+vows.describe('Html with <link rel="search">').addBatch({
+    'After loading test case': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/HtmlSearchLink/'})
+                .loadAssets('index.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain an HtmlSearchLink relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlSearchLink'}).length, 1);
+        },
+        'the graph should contain a single Xml asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Xml'}).length, 1);
+        }
+    }
+})['export'](module);
