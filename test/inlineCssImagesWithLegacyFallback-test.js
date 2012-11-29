assetgraph/assetgraph

commit d7bb02b9987d74777d0f50bfbe492fc712a090b1

Changed the representation of "inverted" conditional comments.
They're no longer modelled as relations, just kept in the "host" document, which is actually cleaner to work with.

Also added a workaround so most unbalanced HTML constructs in non-inverted conditional comments are preserved.

Fixes #63.

lib/transforms/inlineCssImagesWithLegacyFallback.js
@@ -25,17 +25,15 @@ module.exports = function (queryObj, sizeThreshold) {
 
                         hasCssImagesToInline = true;
                         if (cssImage.to.rawSrc.length > 32 * 1024) {
-                            ieVersion = 9; // IE 8 doesn't support data-uri's over 32kb
+                            ieVersion = 9; // IE 8 doesn't support data URLs > 32 KB
                         }
                     }
                 });
                 if (hasCssImagesToInline) {
                     var document = htmlAsset.parseTree,
-                        internetExplorerConditionalCommentBody = new assets.Html({text: ""}),
-                        nonInternetExplorerConditionalCommentBody = new assets.Html({text: ""});
+                        internetExplorerConditionalCommentBody = new assets.Html({text: ""});
 
                     assetGraph.addAsset(internetExplorerConditionalCommentBody);
-                    assetGraph.addAsset(nonInternetExplorerConditionalCommentBody);
 
                     new relations.HtmlConditionalComment({
                         to: internetExplorerConditionalCommentBody,
@@ -45,30 +43,25 @@ module.exports = function (queryObj, sizeThreshold) {
                     var htmlStyleInInternetExplorerConditionalComment = new relations.HtmlStyle({to: cssAsset});
                     htmlStyleInInternetExplorerConditionalComment.attach(internetExplorerConditionalCommentBody, 'first');
 
-                    new relations.HtmlConditionalComment({
-                        isInverted: true,
-                        to: nonInternetExplorerConditionalCommentBody,
-                        condition: 'gte IE ' + ieVersion
-                    }).attach(htmlAsset, 'before', htmlStyle);
-
-                    var htmlStyleInNonInternetExplorerConditionalComment = new relations.HtmlStyle({to: cssAsset});
-                    htmlStyleInNonInternetExplorerConditionalComment.attach(nonInternetExplorerConditionalCommentBody, 'first');
-                    var cssAssetClone = cssAsset.clone(htmlStyleInNonInternetExplorerConditionalComment);
-
-                    var media = htmlStyle.node.getAttribute('media');
+                    var parentNode = htmlStyle.node.parentNode;
+                    parentNode.insertBefore(document.createComment('[if gte IE ' + ieVersion + ']><!'), htmlStyle.node);
+                    var endMarker = document.createComment('<![endif]');
+                    if (htmlStyle.node.nextSibling) {
+                        parentNode.insertBefore(endMarker, htmlStyle.node.nextSibling);
+                    } else {
+                        parentNode.appendChild(endMarker);
+                    }
+                    var cssAssetClone = cssAsset.clone(htmlStyle),
+                        media = htmlStyle.node.getAttribute('media');
                     if (media) {
                         htmlStyleInInternetExplorerConditionalComment.node.setAttribute('media', media);
-                        htmlStyleInNonInternetExplorerConditionalComment.node.setAttribute('media', media);
                         internetExplorerConditionalCommentBody.markDirty();
-                        nonInternetExplorerConditionalCommentBody.markDirty();
                     }
 
-                    htmlStyle.detach();
-
-                    assetGraph.findRelations({from: cssAssetClone, type: 'CssImage'}).forEach(function (cssImage) {
+                    assetGraph.findRelations({from: cssAsset, type: 'CssImage'}).forEach(function (cssImage) {
                         if (cssImage.cssRule.style.getPropertyValue('-one-image-inline')) {
                             cssImage.cssRule.style.removeProperty('-one-image-inline');
-                            cssAssetClone.markDirty();
+                            cssAsset.markDirty();
                             cssImage.inline();
                         } else if (!/^_/.test(cssImage.propertyName) && // Underscore hack (IE6), don't inline
                                    sizeThreshold >= 0 &&

test/inlineCssImagesWithLegacyFallback-test.js
@@ -25,8 +25,8 @@ vows.describe('transforms.inlineCssImagesWithLegacyFallback').addBatch({
             'the graph should contain 6 Css assets': function (assetGraph) {
                 assert.equal(assetGraph.findAssets({type: 'Css'}).length, 6);
             },
-            'the graph should contain 13 HtmlConditionalComment relations': function (assetGraph) {
-                assert.equal(assetGraph.findRelations({type: 'HtmlConditionalComment'}).length, 13);
+            'the graph should contain 7 HtmlConditionalComment relations': function (assetGraph) {
+                assert.equal(assetGraph.findRelations({type: 'HtmlConditionalComment'}).length, 7);
             },
             'the graph should contain 3 inline CssImage relations': function (assetGraph) {
                 assert.equal(assetGraph.findRelations({type: 'CssImage', to: {isInline: true}}).length, 3);

test/bundleRelations/invertedConditionalCommentInTheMiddle/index.html
@@ -1,0 +1,14 @@
+<html>
+<head>
+<link rel='stylesheet' href='a.css'>
+<!--[if IE 6]>Irrelevant conditional comment<![endif]-->
+<link rel='stylesheet' href='b.css'>
+<!--[if gt IE 8]><!-->
+  <link rel='stylesheet' href='c.css'>
+  <link rel='stylesheet' href='d.css'>
+<!--<![endif]-->
+<link rel='stylesheet' href='e.css'>
+</head>
+<body>
+</body>
+</html>

lib/relations/HtmlConditionalComment.js
@@ -17,23 +17,13 @@ util.inherits(HtmlConditionalComment, HtmlRelation);
 extendWithGettersAndSetters(HtmlConditionalComment.prototype, {
     inline: function () {
         Relation.prototype.inline.call(this);
-        if (this.isInverted) {
-            // <!--[if !IE]> --> ... <!-- <![endif]--> (evaluated by all non-IE browsers)
-            // <!--[if IE gte 8]><!--> ... <!--<![endif]--> (evaluated by certain IE versions and all non-IE browsers)
-            // Clear the existing content between the start and end markers:
-            while (this.node.nextSibling && this.node.nextSibling !== this.endNode) {
-                this.node.parentNode.removeChild(this.node.nextSibling);
-            }
-            // Create copies of the nodes in the target asset and insert them between the start and end markers:
-            var div = this.from.parseTree.createElement('div');
-            div.innerHTML = this.to.text;
-            while (div.firstChild) {
-                this.endNode.parentNode.insertBefore(div.firstChild, this.endNode);
-            }
-        } else {
-            // <!--[if IE 6]> .... <![endif]-->
-            this.node.nodeValue = '[if ' + this.condition + ']>' + this.to.text + '<![endif]';
+        var text = this.to.text,
+            matchText = this.to.text.match(/<!--ASSETGRAPH DOCUMENT START MARKER-->([\s\S]*)<!--ASSETGRAPH DOCUMENT END MARKER-->/);
+        if (matchText) {
+            text = matchText[1];
         }
+
+        this.node.nodeValue = '[if ' + this.condition + ']>' + text + '<![endif]';
         this.from.markDirty();
         return this;
     },
@@ -41,19 +31,6 @@ extendWithGettersAndSetters(HtmlConditionalComment.prototype, {
     attach: function (asset, position, adjacentRelation) {
         this.node = asset.parseTree.createComment();
         this.attachNodeBeforeOrAfter(position, adjacentRelation);
-        if (this.isInverted) {
-            if (this.condition === '!IE') {
-                this.node.nodeValue = '[if !IE]>';
-            } else {
-                this.node.nodeValue = '[if ' + this.condition + ']><!';
-            }
-            this.endNode = asset.parseTree.createComment('<![endif]');
-            if (this.node.nextSibling) {
-                this.node.parentNode.insertBefore(this.endNode, this.node.nextSibling);
-            } else {
-                this.node.parentNode.appendChild(this.endNode);
-            }
-        }
         return HtmlRelation.prototype.attach.call(this, asset, position, adjacentRelation);
     }
 });

lib/transforms/addCacheManifest.js
@@ -40,10 +40,7 @@ module.exports = function (queryObj) {
 
             // Find all assets that can be reached from the Html file and add relations to them from the manifest:
 
-            assetGraph.eachAssetPostOrder(htmlAsset,
-                                          query.or({type: 'HtmlConditionalComment', condition: '!IE'},
-                                                   {type: query.not(['HtmlAnchor', 'HtmlCacheManifest', 'HtmlConditionalComment'])}),
-                                          function (asset, incomingRelation) {
+            assetGraph.eachAssetPostOrder(htmlAsset, {type: query.not(['HtmlAnchor', 'HtmlCacheManifest', 'HtmlConditionalComment'])}, function (asset, incomingRelation) {
                 // But only if the asset isn't inline and isn't already in the manifest:
                 if (!asset.isInline && asset !== htmlAsset && asset !== manifest && !assetGraph.findRelations({from: manifest, to: asset}).length) {
                     var existingManifestEntriesInCacheSection = assetGraph.findRelations({from: manifest, sectionName: 'CACHE'}),

lib/transforms/bundleRelations.js
@@ -127,7 +127,7 @@ bundleStrategyByName.oneBundlePerIncludingAsset = function (assetGraph, queryObj
 
         _.unique(_.pluck(relationsToBundle, 'type')).forEach(function (relationType) {
             var currentBundle = [],
-                bundleDiscriminator, // Only used for HtmlStyle (the media attribute)
+                bundleDiscriminator,
                 relationsOfTypeToBundle = relationsToBundle.filter(function (relation) {
                     return relation.type === relationType;
                 });
@@ -145,13 +145,18 @@ bundleStrategyByName.oneBundlePerIncludingAsset = function (assetGraph, queryObj
                     }
                 } else if (relationsOfTypeToBundle.indexOf(outgoingRelation) !== -1) {
                     // Make sure that we don't bundle HtmlStyles with different media attributes together:
-                    if (relationType === 'HtmlStyle') {
-                        var discriminator = outgoingRelation.node.getAttribute('media') || 'all';
-                        if (bundleDiscriminator && discriminator !== bundleDiscriminator) {
-                            flushBundle();
-                        }
-                        bundleDiscriminator = discriminator;
+                    var discriminatorFragments = [];
+                    if (outgoingRelation.conditionalComments) {
+                        Array.prototype.push.apply(discriminatorFragments, _.pluck(outgoingRelation.conditionalComments, 'nodeValue'));
                     }
+                    if (relationType === 'HtmlStyle') {
+                        discriminatorFragments.push(outgoingRelation.node.getAttribute('media') || 'all');
+                    }
+                    var discriminator = discriminatorFragments.join(':');
+                    if (bundleDiscriminator && discriminator !== bundleDiscriminator) {
+                        flushBundle();
+                    }
+                    bundleDiscriminator = discriminator;
                     if (assetGraph.findRelations({to: outgoingRelation.to}).length > 1) {
                         currentBundle.push(outgoingRelation.to.clone(outgoingRelation));
                     } else {

test/CacheManifest-test.js
@@ -76,18 +76,18 @@ vows.describe('Cache manifest').addBatch({
                 .populate({followRelations: {to: {url: /^file:/}}})
                 .run(this.callback);
         },
-        'the graph contains 7 assets': function (assetGraph) {
-            assert.equal(assetGraph.findAssets().length, 7);
+        'the graph contains 6 assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets().length, 6);
         },
-        'the graph contains 7 relations': function (assetGraph) {
-            assert.equal(assetGraph.findRelations().length, 7);
+        'the graph contains 6 relations': function (assetGraph) {
+            assert.equal(assetGraph.findRelations().length, 6);
         },
         'the graph contains a single Png asset': function (assetGraph) {
             assert.equal(assetGraph.findAssets({type: 'Png'}).length, 1);
         },
-        'the graph contains 3 Html assets, of which two are inline (conditional comments)': function (assetGraph) {
-            assert.equal(assetGraph.findAssets({type: 'Html'}).length, 3);
-            assert.equal(assetGraph.findAssets({type: 'Html', isInline: true}).length, 2);
+        'the graph contains 2 Html assets, of which one is inline (conditional comment)': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Html'}).length, 2);
+            assert.equal(assetGraph.findAssets({type: 'Html', isInline: true}).length, 1);
         },
         'the graph contains a single Css asset': function (assetGraph) {
             assert.equal(assetGraph.findAssets({type: 'Css'}).length, 1);

commit 0a2a35e240fdb319604051de9139976827ef30ad

inlineCssImagesWithLegacyFallback: Fixed bug with multiple Html assets pointing at the same stylesheet (introduced in d7bb02b).

lib/transforms/inlineCssImagesWithLegacyFallback.js
@@ -39,7 +39,6 @@ module.exports = function (queryObj, sizeThreshold) {
                         to: internetExplorerConditionalCommentBody,
                         condition: 'lt IE ' + ieVersion
                     }).attach(htmlAsset, 'before', htmlStyle);
-
                     var htmlStyleInInternetExplorerConditionalComment = new relations.HtmlStyle({to: cssAsset});
                     htmlStyleInInternetExplorerConditionalComment.attach(internetExplorerConditionalCommentBody, 'first');
 
@@ -58,7 +57,7 @@ module.exports = function (queryObj, sizeThreshold) {
                         internetExplorerConditionalCommentBody.markDirty();
                     }
 
-                    assetGraph.findRelations({from: cssAsset, type: 'CssImage'}).forEach(function (cssImage) {
+                    assetGraph.findRelations({from: cssAssetClone, type: 'CssImage'}).forEach(function (cssImage) {
                         if (cssImage.cssRule.style.getPropertyValue('-one-image-inline')) {
                             cssImage.cssRule.style.removeProperty('-one-image-inline');
                             cssAsset.markDirty();
@@ -70,6 +69,7 @@ module.exports = function (queryObj, sizeThreshold) {
                             cssImage.inline();
                         }
                     });
+                    htmlAsset.markDirty();
                 }
             });
         });

test/inlineCssImagesWithLegacyFallback-test.js
@@ -3,9 +3,9 @@ var vows = require('vows'),
     AssetGraph = require('../lib/AssetGraph');
 
 vows.describe('transforms.inlineCssImagesWithLegacyFallback').addBatch({
-    'After loading test case': {
+    'After loading test case with a single Html asset': {
         topic: function () {
-            new AssetGraph({root: __dirname + '/inlineCssImagesWithLegacyFallback/'})
+            new AssetGraph({root: __dirname + '/inlineCssImagesWithLegacyFallback/combo/'})
                 .loadAssets('index.html')
                 .populate()
                 .run(this.callback);
@@ -59,6 +59,58 @@ vows.describe('transforms.inlineCssImagesWithLegacyFallback').addBatch({
                 var captures = text.match(/<!--\[if lt IE 9\]><link[^>]*><!\[endif\]-->/g);
                 assert.isNotNull(captures);
                 assert.equal(captures.length, 1);
+            }
+        }
+    },
+    'After loading test case with multiple Html asset that point at the same Css': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/inlineCssImagesWithLegacyFallback/multipleHtmls/'})
+                .loadAssets('*.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 2 Html assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Html'}).length, 2);
+        },
+        'the graph should contain 1 Css asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Css'}).length, 1);
+        },
+        'the graph should contain 1 image': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({isImage: true}).length, 1);
+        },
+        'then running the inlineCssImagesWithLegacyFallback transform': {
+            topic: function (assetGraph) {
+                assetGraph
+                    .inlineCssImagesWithLegacyFallback({isInitial: true}, 32768 * 3/4)
+                    .run(this.callback);
+            },
+            'the graph should contain 3 Css assets': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({type: 'Css'}).length, 3);
+            },
+            'the graph should contain 2 HtmlConditionalComment relations': function (assetGraph) {
+                assert.equal(assetGraph.findRelations({type: 'HtmlConditionalComment'}).length, 2);
+            },
+            'the graph should contain 2 inline CssImage relations': function (assetGraph) {
+                assert.equal(assetGraph.findRelations({type: 'CssImage', to: {isInline: true}}).length, 2);
+            },
+            '1.html should contain 2 <link> tags': function (assetGraph) {
+                var htmlAsset = assetGraph.findAssets({type: 'Html', url: /\/1.html$/})[0],
+                    captures = htmlAsset.text.match(/<link[^>]*>/g);
+                assert.isNotNull(captures);
+                assert.equal(captures.length, 2);
+            },
+            '2.html should contain 2 <link> tags': function (assetGraph) {
+                var htmlAsset = assetGraph.findAssets({type: 'Html', url: /\/2.html$/})[0],
+                    captures = htmlAsset.text.match(/<link[^>]*>/g);
+                assert.isNotNull(captures);
+                assert.equal(captures.length, 2);
+            },
+            'each Html asset should contain one >= IE8 conditional comment marker': function (assetGraph) {
+                assetGraph.findAssets({type: 'Html', isInline: false}).forEach(function (htmlAsset) {
+                    var captures = htmlAsset.text.match(/<!--\[if gte IE 8\]><!-->/g);
+                    assert.isNotNull(captures);
+                    assert.equal(captures.length, 1);
+                });
             }
         }
     }

test/inlineCssImagesWithLegacyFallback/multipleHtmls/1.html
@@ -1,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>1</title>
+    <link rel='stylesheet' href='smallImage.css' />
+</head>

test/inlineCssImagesWithLegacyFallback/multipleHtmls/2.html
@@ -1,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>2</title>
+    <link rel='stylesheet' href='smallImage.css' />
+</head>

test/inlineCssImagesWithLegacyFallback/multipleHtmls/smallImage.css
@@ -1,0 +1,3 @@
+div {
+    background-image: url(foo.png);
+}