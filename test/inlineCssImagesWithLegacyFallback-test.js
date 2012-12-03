38781f8a0ec4a55718ca60a444d9dc9ee7532764
Andreas Lind Petersen <andreas@one.com>
2012-12-03 20:29:35 +0100

    inlineCssImagesWithLegacyFallback: Support ?inline=false
    Fixes #90.

--- test/inlineCssImagesWithLegacyFallback-test.js
+++ test/inlineCssImagesWithLegacyFallback-test.js
@@ -10,11 +10,11 @@ vows.describe('transforms.inlineCssImagesWithLegacyFallback').addBatch({
                 .populate()
                 .run(this.callback);
         },
-        'the graph should contain 4 Css assets': function (assetGraph) {
-            assert.equal(assetGraph.findAssets({type: 'Css'}).length, 4);
+        'the graph should contain 5 Css assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Css'}).length, 5);
         },
-        'the graph should contain 4 images': function (assetGraph) {
-            assert.equal(assetGraph.findAssets({isImage: true}).length, 4);
+        'the graph should contain 6 images': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({isImage: true}).length, 6);
         },
         'then running the inlineCssImagesWithLegacyFallback transform': {
             topic: function (assetGraph) {
@@ -22,8 +22,8 @@ vows.describe('transforms.inlineCssImagesWithLegacyFallback').addBatch({
                     .inlineCssImagesWithLegacyFallback({isInitial: true}, 32768 * 3/4)
                     .run(this.callback);
             },
-            'the graph should contain 6 Css assets': function (assetGraph) {
-                assert.equal(assetGraph.findAssets({type: 'Css'}).length, 6);
+            'the graph should contain 7 Css assets': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({type: 'Css'}).length, 7);
             },
             'the graph should contain 7 HtmlConditionalComment relations': function (assetGraph) {
                 assert.equal(assetGraph.findRelations({type: 'HtmlConditionalComment'}).length, 7);
@@ -31,10 +31,10 @@ vows.describe('transforms.inlineCssImagesWithLegacyFallback').addBatch({
             'the graph should contain 3 inline CssImage relations': function (assetGraph) {
                 assert.equal(assetGraph.findRelations({type: 'CssImage', to: {isInline: true}}).length, 3);
             },
-            'the Html asset should contain 6 <link> tags': function (assetGraph) {
+            'the Html asset should contain 7 <link> tags': function (assetGraph) {
                 var captures = assetGraph.findAssets({type: 'Html'})[0].text.match(/<link[^>]*>/g);
                 assert.isNotNull(captures);
-                assert.equal(captures.length, 6);
+                assert.equal(captures.length, 7);
             },
             'the Html asset should contain 3 non-IE conditional comment markers': function (assetGraph) {
                 var captures = assetGraph.findAssets({type: 'Html'})[0].text.match(/<!--\[if !IE\]>-->/g);
@@ -45,6 +45,11 @@ vows.describe('transforms.inlineCssImagesWithLegacyFallback').addBatch({
                 var captures = assetGraph.findAssets({type: 'Html'})[0].text.match(/media=['"]handheld/g);
                 assert.isNotNull(captures);
                 assert.equal(captures.length, 2);
+            },
+            'the media=screen attribute should occur once': function (assetGraph) {
+                var captures = assetGraph.findAssets({type: 'Html'})[0].text.match(/media=['"]screen/g);
+                assert.isNotNull(captures);
+                assert.equal(captures.length, 1);
             },
             'the Html asset should contain 1 IE 8 conditional comment marker with a link tag in it': function (assetGraph) {
                 var text = assetGraph.findAssets({type: 'Html'})[0].text;
