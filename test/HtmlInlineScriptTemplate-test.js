bc83e66861f03bba822a5d5c3dc6245bc831eb98
Andreas Lind Petersen <andreas@one.com>
2012-12-22 18:37:31 +0100

    Fixed parsing of contents of existing <script type='text/html'>...</script> with HTML comments.

--- test/HtmlInlineScriptTemplate-test.js
+++ test/HtmlInlineScriptTemplate-test.js
@@ -34,5 +34,34 @@ vows.describe('HtmlInlineScriptTemplate test').addBatch({
                 assert.matches(assetGraph.findAssets({type: 'Html', isInline: false})[0].text, /<div>hello!<\/div>/);
             }
         }
+    },
+    'After loading test case with some advanced markup in an inline template': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/HtmlInlineScriptTemplate/'})
+                .loadAssets('advancedMarkup.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 2 Html asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Html'}).length, 2);
+        },
+        'the graph should contain 1 HtmlInlineScriptTemplate relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlInlineScriptTemplate'}).length, 1);
+        },
+        'the HtmlInlineScriptTemplate relation should contain the right text': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlInlineScriptTemplate'})[0].to.text, "\n<div>foo<!--ko 'if':true-->bar<!--/ko-->quux</div>\n");
+        },
+        'then manipulating the inline Html': {
+            topic: function (assetGraph) {
+                var inlineHtml = assetGraph.findAssets({type: 'Html', isInline: true})[0],
+                    document = inlineHtml.parseTree;
+                document.appendChild(document.createTextNode('hello!'));
+                inlineHtml.markDirty();
+                return assetGraph;
+            },
+            'the text of the outer Html asset should contain the template': function (assetGraph) {
+                assert.matches(assetGraph.findAssets({type: 'Html', isInline: false})[0].text, /hello!/);
+            }
+        }
     }
 })['export'](module);
