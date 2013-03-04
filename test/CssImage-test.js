391e345d9363eb98b96ca292c043d5dbcbfc40ed
Andreas Lind Petersen <andreas@one.com>
2013-03-04 23:15:54 +0100

    Model cursor: url(...) as a CssImage relation. Fixes #96.

--- test/CssImage-test.js
+++ test/CssImage-test.js
@@ -4,9 +4,9 @@ var vows = require('vows'),
     AssetGraph = require('../lib/AssetGraph');
 
 vows.describe('CssImage').addBatch({
-    'After loading test case': {
+    'After loading a test case with a bunch of different CssImage relations': {
         topic: function () {
-            new AssetGraph({root: __dirname + '/CssImage/'})
+            new AssetGraph({root: __dirname + '/CssImage/combo/'})
                 .loadAssets('index.css')
                 .populate()
                 .run(this.callback);
@@ -42,6 +42,29 @@ vows.describe('CssImage').addBatch({
             'the !important marker on the .baz background-image should still be there': function (assetGraph) {
                 assert.matches(assetGraph.findAssets({url: /\/index\.css$/})[0].text, /\.baz{background-image:url\(dir\/foo2\.png\)!important/);
             }
+        }
+    },
+    'After loading a test case with three CssImage relations pointing at mouse cursors': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/CssImage/mouseCursors/'})
+                .loadAssets('index.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 5 assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets().length, 5);
+        },
+        'the graph should contain one Html asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Html'}).length, 1);
+        },
+        'the graph should contain one Css asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Css'}).length, 1);
+        },
+        'the graph should contain 3 Png assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Png'}).length, 3);
+        },
+        'the graph should contain 3 CssImage relations': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'CssImage'}).length, 3);
         }
     }
 })['export'](module);
