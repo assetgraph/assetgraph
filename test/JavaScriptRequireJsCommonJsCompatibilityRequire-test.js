a07fa185b429af9189148a41e1f82c183559da44
Andreas Lind Petersen <andreas@one.com>
2013-05-03 18:29:18 +0200

    WIP

--- test/JavaScriptRequireJsCommonJsCompatibilityRequire-test.js
+++ test/JavaScriptRequireJsCommonJsCompatibilityRequire-test.js
@@ -15,6 +15,9 @@ vows.describe('JavaScriptRequireJsCommonJsCompatibilityRequire').addBatch({
         'the graph should contain 5 assets': function (assetGraph) {
             assert.equal(assetGraph.findAssets().length, 7);
         },
+        'the graph should contain 0 JavaScriptAmdDefine relations': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'JavaScriptAmdDefine'}).length, 0);
+        },
         'the graph should contain one JavaScriptRequireJsCommonJsCompatibilityRequire relation': function (assetGraph) {
             assert.equal(assetGraph.findRelations({type: 'JavaScriptRequireJsCommonJsCompatibilityRequire'}).length, 1);
         },
@@ -24,14 +27,27 @@ vows.describe('JavaScriptRequireJsCommonJsCompatibilityRequire').addBatch({
         'the graph should contain over/the/rainbow/foo.js, and it should be loaded': function (assetGraph) {
             assert.equal(assetGraph.findAssets({url: /\/over\/the\/rainbow\/foo\.js$/, isLoaded: true}).length, 1);
         },
-        'then run the flattenRequireJs transform': {
+        'then run the convertJavaScriptRequireJsCommonJsCompatibilityRequireToJavaScriptAmdDefine transform': {
             topic: function (assetGraph) {
                 assetGraph
-                    .flattenRequireJs({type: 'Html'})
+                    .convertJavaScriptRequireJsCommonJsCompatibilityRequireToJavaScriptAmdDefine()
                     .run(this.callback);
             },
-            'the Html asset should have an HtmlScript relation to an asset with the contents of over/the/rainbow/foo.js': function (assetGraph) {
-                assert.equal(assetGraph.findRelations({type: 'HtmlScript', from: {type: 'Html'}, to: {text: /return 42/}}).length, 1);
+            'the graph should contain one JavaScriptAmdDefine relation': function (assetGraph) {
+                assert.equal(assetGraph.findRelations({type: 'JavaScriptAmdDefine'}).length, );
+            },
+            'commonJsIsh should have "somewhere/foo" in its dependency array': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({text: /\[\"somewhere\/foo\"\]/}).length, 1);
+            },
+            'then run flattenRequireJs': {
+                topic: function (assetGraph) {
+                    assetGraph
+                        .flattenRequireJs({type: 'Html'})
+                        .run(this.callback);
+                },
+                'the Html asset should have an HtmlScript relation to an asset with the contents of over/the/rainbow/foo.js': function (assetGraph) {
+                    assert.equal(assetGraph.findRelations({type: 'HtmlScript', from: {type: 'Html'}, to: {text: /return 42/}}).length, 1);
+                }
             }
         }
     }
