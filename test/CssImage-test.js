8bb891dae765f8a9e5839f2b0ade1c418a784eab
Andreas Lind Petersen <andreas@one.com>
2013-05-18 18:32:17 +0200

    Model content:url(...) (in CSS) as a relation. Fixes #108.

--- test/CssImage-test.js
+++ test/CssImage-test.js
@@ -11,8 +11,8 @@ vows.describe('CssImage').addBatch({
                 .populate()
                 .run(this.callback);
         },
-        'the graph should contain 16 CssImage relations': function (assetGraph) {
-            assert.equal(assetGraph.findRelations({type: 'CssImage'}).length, 16);
+        'the graph should contain 17 CssImage relations': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'CssImage'}).length, 17);
         },
         'then move foo.png to a different url': {
             topic: function (assetGraph) {
@@ -21,6 +21,7 @@ vows.describe('CssImage').addBatch({
             },
             'the references to it should be updated': function (assetGraph) {
                 assert.deepEqual(_.pluck(assetGraph.findRelations({to: assetGraph.findAssets({url: /\/foo2\.png$/})}), 'href'), [
+                    'dir/foo2.png',
                     'dir/foo2.png',
                     'dir/foo2.png',
                     'dir/foo2.png',
