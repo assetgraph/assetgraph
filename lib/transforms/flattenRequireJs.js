assetgraph/assetgraph

commit 70f34cbfc8f8a0f7906636185bc5449aa42f82ca

flattenRequireJs: Don't attempt to detach relation types that don't support it.

lib/transforms/flattenRequireJs.js
@@ -83,7 +83,11 @@ module.exports = function (queryObj) {
                                     newHtmlStyle.attach(htmlAsset, 'first');
                                 }
                                 htmlStyleInsertionPoint = newHtmlStyle;
-                                outgoingRelation.detach();
+                                if (outgoingRelation.type === 'JavaScriptAmdDefine' || outgoingRelation.type === 'JavaScriptAmdDefine') {
+                                    outgoingRelation.detach();
+                                } else {
+                                    assetGraph.removeRelation(outgoingRelation);
+                                }
                             } else if (outgoingRelation.to.isText) {
                                 var moduleName;
                                 if (outgoingRelation.to.url in nonJavaScriptModuleNameByUrl) {

commit 802114502383a6cc8c15ef0053f4256756300137

Fixed typo in flattenRequireJs introduced in 70f34cb.

lib/transforms/flattenRequireJs.js
@@ -83,7 +83,7 @@ module.exports = function (queryObj) {
                                     newHtmlStyle.attach(htmlAsset, 'first');
                                 }
                                 htmlStyleInsertionPoint = newHtmlStyle;
-                                if (outgoingRelation.type === 'JavaScriptAmdDefine' || outgoingRelation.type === 'JavaScriptAmdDefine') {
+                                if (outgoingRelation.type === 'JavaScriptAmdDefine' || outgoingRelation.type === 'JavaScriptAmdRequire') {
                                     outgoingRelation.detach();
                                 } else {
                                     assetGraph.removeRelation(outgoingRelation);