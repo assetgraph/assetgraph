d67351a423946d98b92a3eea7f0f0743cd373d81
Andreas Lind Petersen <andreas@one.com>
2013-06-14 18:27:54 +0200

    Svg: Model <image xlink:href='...'> and <script xlink:href='...'> as relations.

---
+++ test/Svg-test.js
@@ -0,0 +1,35 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    AssetGraph = require('../lib/AssetGraph');
+
+vows.describe('Svg').addBatch({
+    'After loading a test case with an Svg asset': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/Svg/'})
+                .loadAssets('index.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 4 assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets().length, 4);
+        },
+        'the graph should contain one Html asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Html'}).length, 1);
+        },
+        'the graph should contain one Svg asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Svg'}).length, 1);
+        },
+        'the graph should contain one JavaScript asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'JavaScript'}).length, 1);
+        },
+        'the graph should contain one Png asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Png'}).length, 1);
+        },
+        'the graph should contain one SvgImage relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'SvgImage'}).length, 1);
+        },
+        'the graph should contain one SvgScript relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'SvgScript'}).length, 1);
+        }
+    }
+})['export'](module);
