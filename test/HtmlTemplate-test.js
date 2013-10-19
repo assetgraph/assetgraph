f096b927e4ae8ed4952df3fcc480973a689d77ac
Peter Müller <munter@fumle.dk>
2013-10-19 23:54:46 +0200

    Added HtmlTemplate-test

---
+++ test/HtmlTemplate-test.js
@@ -0,0 +1,41 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    AssetGraph = require('../lib/AssetGraph');
+
+vows.describe('Html with <template>').addBatch({
+    'After loading test case': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/HtmlTemplate/'})
+                .loadAssets('index.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 3 HtmlImage relations': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlImage'}).length, 3);
+        },
+        'the graph should contain 1 HtmlTemplate relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlTemplate'}).length, 1);
+        },
+        'the graph should contain 1 HtmlStyle relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlStyle'}).length, 1);
+        },
+        'the Html parent asset should have 1 HtmlImage relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({
+                type: 'HtmlImage',
+                from: {
+                    type: 'Html',
+                    isFragment: false
+                }
+            }).length, 1);
+        },
+        'the Html template asset should have 2 HtmlImage relations': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({
+                type: 'HtmlImage',
+                from: {
+                    type: 'Html',
+                    isFragment: true
+                }
+            }).length, 2);
+        }
+    }
+})['export'](module);
