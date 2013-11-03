8271c4f02d0e51a65e340d3bda01b84cb99d7f8b
Peter Müller <munter@fumle.dk>
2013-11-03 19:20:07 +0100

    Don't replace requirejs with almond if requirejs is used actively as a script loader

--- test/replaceRequireJsWithAlmond-test.js
+++ test/replaceRequireJsWithAlmond-test.js
@@ -9,7 +9,10 @@ vows.describe('transforms.replaceRequireJsWithAlmond').addBatch({
     'After loading a non-almond test case': {
         topic: function () {
             new AssetGraph({root: __dirname + '/replaceRequireJsWithAlmond/'})
+                .registerRequireJsConfig({preventPopulationOfJavaScriptAssetsUntilConfigHasBeenFound: true})
                 .loadAssets('require-pure.html')
+                .populate({from: {type: 'Html'}, followRelations: {type: 'HtmlScript', to: {url: /^file:/}}})
+                .assumeRequireJsConfigHasBeenFound()
                 .populate()
                 //.drawGraph('1-before.svg')
                 .run(this.callback);
@@ -53,7 +56,10 @@ vows.describe('transforms.replaceRequireJsWithAlmond').addBatch({
                     console.warn('remove', asset.url);
                 })
                 */
+                .registerRequireJsConfig({preventPopulationOfJavaScriptAssetsUntilConfigHasBeenFound: true})
                 .loadAssets('require-almond.html')
+                .populate({from: {type: 'Html'}, followRelations: {type: 'HtmlScript', to: {url: /^file:/}}})
+                .assumeRequireJsConfigHasBeenFound()
                 .populate()
                 //.drawGraph('2-before.svg')
                 .run(this.callback);
@@ -101,6 +107,70 @@ vows.describe('transforms.replaceRequireJsWithAlmond').addBatch({
                     text = relation && relation.to.text;
 
                 assert.equal(text, 'alert(\'APP\');\n');
+            }
+        }
+    },
+    'After loading an almond test case that uses requirejs as script loader': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/replaceRequireJsWithAlmond/'})
+                /*
+                // Debugging
+                .on('addAsset', function (asset) {
+                    console.warn('add', asset.url);
+                })
+                .on('removeAsset', function (asset) {
+                    console.warn('remove', asset.url);
+                })
+                */
+                .registerRequireJsConfig({preventPopulationOfJavaScriptAssetsUntilConfigHasBeenFound: true})
+                .loadAssets('require-almond-external.html')
+                .populate({from: {type: 'Html'}, followRelations: {type: 'HtmlScript', to: {url: /^file:/}}})
+                .assumeRequireJsConfigHasBeenFound()
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 6 JavaScript assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'JavaScript'}).length, 6);
+        },
+        'the graph should contain 2 HtmlScript relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlScript'}).length, 2);
+        },
+        'the graph should contain 1 HtmlRequireJsAlmondReplacement relation': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlRequireJsAlmondReplacement'}).length, 1);
+        },
+        'the JavaScript asset text should be equal to require.js': function (assetGraph) {
+            var relation = assetGraph.findRelations({type: 'HtmlScript'})[0],
+                text = relation && relation.to.text;
+
+            assert.equal(text, requirejs);
+        },
+        'then running the replaceRequireJsWithAlmond transform': {
+            topic: function (assetGraph) {
+                assetGraph
+                    .replaceRequireJsWithAlmond()
+                    //.drawGraph('2-after.svg')
+                    .run(this.callback);
+            },
+
+            'the graph should contain 5 JavaScript assets': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({type: 'JavaScript'}).length, 5);
+            },
+            'the graph should contain 2 HtmlScript relations': function (assetGraph) {
+                assert.equal(assetGraph.findRelations({type: 'HtmlScript'}).length, 2);
+            },
+            'the graph should contain 0 HtmlRequireJsAlmondReplacement relations': function (assetGraph) {
+                assert.equal(assetGraph.findRelations({type: 'HtmlRequireJsAlmondReplacement'}).length, 0);
+            },
+            'the first JavaScript assets text should be equal to require.js': function (assetGraph) {
+                var relation = assetGraph.findRelations({type: 'HtmlScript'})[0],
+                    text = relation && relation.to.text;
+
+                assert.equal(text, requirejs);
+            },
+            'the graph should have emitted 1 warning from the replaceRequireJsWithAlmond transform': function (assetGraph) {
+                assert.equal(assetGraph._vowsEmitedEvents.warn.filter(function (warn) {
+                    return warn.transform === 'replaceRequireJsWithAlmond';
+                }), 4);
             }
         }
     }
