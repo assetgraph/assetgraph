4fb98e3b0930433863a7748970236831075f0dcb
Andreas Lind Petersen <andreas@one.com>
2013-11-18 21:09:49 +0100

    JavaScriptCommonJsRequire test: Fixed paths so they're not only valid on my dev machine (argh).

--- test/JavaScriptCommonJsRequire-test.js
+++ test/JavaScriptCommonJsRequire-test.js
@@ -15,19 +15,19 @@ vows.describe('relations.JavaScriptCommonJsRequire').addBatch({
         },
         'the graph should contain 3 JavaScript assets with the correct urls': function (assetGraph) {
             assert.equal(assetGraph.findAssets({type: 'JavaScript'}).length, 3);
-            assert.equal(assetGraph.findAssets({url: 'file:///home/andreas/work/assetgraph/test/JavaScriptCommonJsRequire/index.js'}).length, 1);
-            assert.equal(assetGraph.findAssets({url: 'file:///home/andreas/work/assetgraph/test/JavaScriptCommonJsRequire/otherModule.js'}).length, 1);
-            assert.equal(assetGraph.findAssets({url: 'file:///home/andreas/work/assetgraph/test/JavaScriptCommonJsRequire/subdir/index.js'}).length, 1);
+            assert.equal(assetGraph.findAssets({url: assetGraph.root + 'index.js'}).length, 1);
+            assert.equal(assetGraph.findAssets({url: /\/otherModule\.js$/}).length, 1);
+            assert.equal(assetGraph.findAssets({url: /\/subdir\/index\.js$/}).length, 1);
         },
         'then move otherModule.js': {
             topic: function (assetGraph) {
-                assetGraph.findAssets({url: 'file:///home/andreas/work/assetgraph/test/JavaScriptCommonJsRequire/otherModule.js'})[0].url =
-                    'file:///home/andreas/work/assetgraph/test/JavaScriptCommonJsRequire/otherSubdir/otherModule.js';
+                assetGraph.findAssets({url: /\/test\/JavaScriptCommonJsRequire\/otherModule\.js$/})[0].url =
+                    assetGraph.root + 'otherSubdir/otherModule.js';
                 return assetGraph;
             },
             'the href of the incoming relation should be updated correctly': function (assetGraph) {
-                assert.equal(assetGraph.findRelations({to: {url: 'file:///home/andreas/work/assetgraph/test/JavaScriptCommonJsRequire/otherSubdir/otherModule.js'}})[0].href,
-                             'file:///home/andreas/work/assetgraph/test/JavaScriptCommonJsRequire/otherSubdir/otherModule.js');
+                assert.match(assetGraph.findRelations({to: {url: /\/otherSubdir\/otherModule\.js$/}})[0].href,
+                             /\/otherSubdir\/otherModule\.js$/);
             },
             'the text of the including asset should be updated correctly': function (assetGraph) {
                 assert.notEqual(assetGraph.findAssets({url: /\/index\.js$/})[0].text.indexOf('require("' + __dirname + '/JavaScriptCommonJsRequire/otherSubdir/otherModule.js")'), -1);
