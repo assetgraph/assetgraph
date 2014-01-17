4478ae8ee7c1cdfffa9dc54c88545bfa0db690ee
Peter Müller <munter@fumle.dk>
2014-01-17 23:24:26 +0100

    Added test case that shows problem with resolving relative path url in HtmlImport relations

--- test/HtmlImport-test.js
+++ test/HtmlImport-test.js
@@ -6,12 +6,26 @@ vows.describe('Html with <link rel="import">').addBatch({
     'After loading test case': {
         topic: function () {
             new AssetGraph({root: __dirname + '/HtmlImport/'})
+            /*
+                .on('addAsset', function (rel) {
+                    console.log(rel.toString());
+                })
+                .on('warn', function (e) {
+                    console.log(e);
+                })
+*/
                 .loadAssets('index.html')
                 .populate()
                 .run(this.callback);
         },
-        'the graph should contain 1 HtmlImport relations': function (assetGraph) {
-            assert.equal(assetGraph.findRelations({type: 'HtmlImport'}).length, 1);
+        'the graph should contain 3 HtmlImport relations': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({type: 'HtmlImport'}).length, 3);
+        },
+        'the graph should contain 4 populated Html assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({
+                type: 'Html',
+                isPopulated: true
+            }).length, 4);
         }
     }
 })['export'](module);
