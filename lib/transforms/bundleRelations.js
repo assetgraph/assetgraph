assetgraph/assetgraph

commit e6686361588f4ea8ebf50ddcee490f23dae1de04

Added script location in <head> as a bundle discriminator

lib/transforms/bundleRelations.js
@@ -17,8 +17,17 @@ module.exports = function (queryObj, options) {
             if (relation.type === 'HtmlStyle') {
                 discriminatorFragments.push(relation.node.getAttribute('media') || 'all');
             }
+
             if (relation.type === 'HtmlScript') {
-                discriminatorFragments.push(relation.isLoaded);
+                if (relation.isLoaded) {
+                    discriminatorFragments.push('isLoaded');
+                }
+
+                if (relation.node.matchesSelector('head script')) {
+                    discriminatorFragments.push('head');
+                } else {
+                    discriminatorFragments.push('body');
+                }
             }
             return discriminatorFragments.join(':');
         }

commit 973b1c41d3cc886238c769e3732b4ecdb41a8ab9

Fix: transforms/bundleRelations getDiscriminatorForRelation would throw an error when using reation.node.matchesSelector on html fragments without a document node. This error was introduced in e668636

lib/transforms/bundleRelations.js
@@ -23,7 +23,9 @@ module.exports = function (queryObj, options) {
                     discriminatorFragments.push('isLoaded');
                 }
 
-                if (relation.node.matchesSelector('head script')) {
+                // Check relation.node.ownerDocument.documentElement,
+                // otherwise node.matchesSelector throws an error for document fragments
+                if (relation.node.ownerDocument.documentElement && relation.node.matchesSelector('head script')) {
                     discriminatorFragments.push('head');
                 } else {
                     discriminatorFragments.push('body');