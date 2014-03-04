0a85240cd71ef15750018038d62bfb56c7a5fbcc
Peter Müller <munter@fumle.dk>
2014-03-04 23:22:23 +0100

    Speed up splitCss transform tests

--- test/splitCssIfIeLimitIsReached-test.js
+++ test/splitCssIfIeLimitIsReached-test.js
@@ -1,8 +1,7 @@
 var vows = require('vows'),
     assert = require('assert'),
     AssetGraph = require('../lib'),
-    _ = require('underscore'),
-    cssText = '';
+    _ = require('underscore');
 
 vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
     'After loading a simple Css test case': {
@@ -25,7 +24,6 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
 */
                 .loadAssets('index.html')
                 .populate()
-                .minifyAssets({ type: 'Css', isLoaded: true})
                 .run(this.callback);
         },
         'the graph should contain 1 Css asset': function (assetGraph) {
@@ -33,9 +31,7 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
 
             assert.equal(cssAssets.length, 1);
 
-            cssText = cssAssets.map(function (cssAsset) {
-                return cssAsset.text;
-            }).join('');
+            assetGraph._parseTreeBefore = cssAssets[0].parseTree;
         },
         'the Css asset should contain 4096 rules': function (assetGraph) {
             assert.equal(assetGraph.findAssets({ type: 'Css' })[0].parseTree.cssRules.length, 4096);
@@ -49,7 +45,6 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
                         assetGraph.__infos.push(err);
                     })
                     .splitCssIfIeLimitIsReached()
-                    .minifyAssets({ type: 'Css', isLoaded: true})
                     .run(this.callback);
             },
             'the graph should have 1 emitted info': function (assetGraph) {
@@ -67,16 +62,17 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
             },
             'each Css asset should be smaller than the original': function (assetGraph) {
                 assetGraph.findAssets({type: 'Css'}).forEach(function (cssAsset) {
-                    assert(cssAsset.text.length < cssText.length, cssAsset.url);
+                    assert(cssAsset.parseTree.cssRules.length < assetGraph._parseTreeBefore.cssRules.length, cssAsset.url);
                 });
             },
             'the concatenated css text content should be unchanged from before': function (assetGraph) {
-                var text = '';
-                assetGraph.findAssets({type: 'Css'}).forEach(function (cssAsset) {
-                    text += cssAsset.text;
-                });
+                var cssAfter = new AssetGraph.Css({
+                    text: assetGraph.findAssets({type: 'Css'}).map(function (cssAsset) {
+                        return cssAsset.text;
+                    }).join('')
+                }).parseTree.toString();
 
-                assert(text === cssText, 'Concatenation of text of split up css assets differ from original');
+                assert(assetGraph._parseTreeBefore.toString() === cssAfter, 'Concatenation of text of split up css assets differ from original');
             },
             'the background image should have an incoming relation from the second new css asset': function (assetGraph) {
                 var cssAssets = assetGraph.findAssets({
@@ -93,16 +89,11 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
             }
         }
     },
-
     'After loading a real life huge Css test case': {
         topic: function () {
             new AssetGraph({root: __dirname + '/splitCssIfIeLimitIsReached/'})
                 .loadAssets('falcon.html')
                 .populate()
-                .minifyAssets({ type: 'Css', isLoaded: true})
-                .mergeIdenticalAssets({
-                    isLoaded: true
-                })
                 .run(this.callback);
         },
         'the graph should contain 1 Css asset': function (assetGraph) {
@@ -110,9 +101,7 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
 
             assert.equal(cssAssets.length, 1);
 
-            cssText = cssAssets.map(function (cssAsset) {
-                return cssAsset.text;
-            }).join('');
+            assetGraph._parseTreeBefore = cssAssets[0].parseTree;
         },
         'the Css asset should contain 6290 rules': function (assetGraph) {
             var rules = assetGraph.findAssets({ type: 'Css' })[0].parseTree.cssRules.length;
@@ -126,10 +115,10 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
                 }).length, 1);
             });
         },
-        'the graph should contain 6 files named fake.*': function (assetGraph) {
+        'the graph should contain 7 files named fake.*': function (assetGraph) {
             assert.equal(assetGraph.findAssets({
                 url: /fake\./
-            }).length, 6);
+            }).length, 7);
         },
         'then running the splitCssIfIeLimitIsReached transform': {
             topic: function (assetGraph) {
@@ -140,7 +129,6 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
                         assetGraph.__infos.push(err);
                     })
                     .splitCssIfIeLimitIsReached()
-                    .minifyAssets({ type: 'Css', isLoaded: true})
                     .run(this.callback);
             },
             'the graph should have 1 emitted info': function (assetGraph) {
@@ -158,12 +146,13 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
             },
             'each Css asset should be smaller than the original': function (assetGraph) {
                 var cssAssets = assetGraph.findAssets({type: 'Css'}),
+                    parseTreeBefore = assetGraph._parseTreeBefore,
                     rules = [2806, 2399, 1085],
                     sum = 0;
 
                 cssAssets.forEach(function (cssAsset, idx) {
                     var assetRules = cssAsset.parseTree.cssRules.length;
-                    assert(cssAsset.text.length < cssText.length);
+                    assert(assetRules < parseTreeBefore.cssRules.length);
                     assert.equal(assetRules, rules[idx]);
                     sum += assetRules;
                 });
@@ -171,12 +160,14 @@ vows.describe('transforms.splitCssIfIeLimitIsReached').addBatch({
                 assert.equal(sum, assetGraph.__rules);
             },
             'the concatenated css text content should be unchanged from before': function (assetGraph) {
-                var text = '';
-                assetGraph.findAssets({type: 'Css'}).forEach(function (cssAsset) {
-                    text += cssAsset.text;
-                });
+                var text = assetGraph.findAssets({type: 'Css'}).map(function (cssAsset) {
+                            return cssAsset.text;
+                        }).join('\n'),
+                    parseTreeAfter = new AssetGraph.Css({
+                        text: text
+                    }).parseTree;
 
-                assert(text === cssText, 'Concatenation of text of split up css assets differ from original');
+                assert(assetGraph._parseTreeBefore.toString() === parseTreeAfter.toString(), 'Concatenation of text of split up css assets differ from original');
             },
             'the graph should have 1 of each of asset types "png", "gif", "svg", "ttf", "eot", "woff"': function (assetGraph) {
                 ['png', 'gif', 'svg', 'ttf', 'eot', 'woff'].forEach(function (extension) {
