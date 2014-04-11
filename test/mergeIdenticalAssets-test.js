9262a8321efebe977f6936ef42f3cb815991f8f8
Andreas Lind Petersen <andreas@one.com>
2014-04-11 14:16:02 +0200

    mergeIdenticalAssets: Don't merge assets of different types.
    This caused weird symptoms in buildProduction when a project contained both an empty stylesheet and an empty JavaScript file.

--- test/mergeIdenticalAssets-test.js
+++ test/mergeIdenticalAssets-test.js
@@ -2,10 +2,10 @@ var vows = require('vows'),
     assert = require('assert'),
     AssetGraph = require('../lib');
 
-vows.describe('transforms.mergeIdenticalAssets').addBatch({
+vows.describe('mergeIdenticalAssets').addBatch({
     'After loading test case': {
         topic: function () {
-            new AssetGraph({root: __dirname + '/mergeIdenticalAssets/'})
+            new AssetGraph({root: __dirname + '/mergeIdenticalAssets/combo/'})
                 .loadAssets('index.html')
                 .populate()
                 .run(this.callback);
@@ -39,6 +39,31 @@ vows.describe('transforms.mergeIdenticalAssets').addBatch({
                 var htmlImages = assetGraph.findRelations({type: 'HtmlImage'});
                 assert.equal(htmlImages.length, 2);
                 assert.equal(htmlImages[0].to, htmlImages[1].to);
+            }
+        }
+    },
+    'After loading a test case with a JavaScript asset and a Css asset with identical contents, then running the mergeIdenticalAssets transform': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/mergeIdenticalAssets/identicalAssetsOfDifferentTypes/'})
+                .loadAssets('index.html')
+                .populate()
+                .run(this.callback);
+        },
+        'the graph should contain 2 JavaScript asset': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'JavaScript'}).length, 2);
+        },
+        'the graph should contain 2 Css assets': function (assetGraph) {
+            assert.equal(assetGraph.findAssets({type: 'Css'}).length, 2);
+        },
+        'then running the mergeIdenticalAssets transform': {
+            topic: function (assetGraph) {
+                assetGraph.mergeIdenticalAssets().run(this.callback);
+            },
+            'the graph should contain 1 JavaScript asset': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({type: 'JavaScript'}).length, 1);
+            },
+            'the graph should contain 1 Css asset': function (assetGraph) {
+                assert.equal(assetGraph.findAssets({type: 'Css'}).length, 1);
             }
         }
     }
---
+++ test/mergeIdenticalAssets/identicalAssetsOfDifferentTypes/bar.js
@@ -0,0 +1,1 @@
+/* Mostly empty */
