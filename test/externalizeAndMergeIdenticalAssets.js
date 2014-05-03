5fc55a05589db4756abc2adc8c904e9120628c2a
Andreas Lind Petersen <andreas@one.com>
2014-05-04 00:19:58 +0200

    Converted externalizeAndMergeIdenticalAssets test to mocha.

---
+++ test/externalizeAndMergeIdenticalAssets.js
@@ -0,0 +1,37 @@
+var expect = require('./unexpected-with-plugins'),
+    AssetGraph = require('../lib'),
+    query = AssetGraph.query;
+
+describe('externalizeRelations and mergeIdenticalAssets', function () {
+    it('should handle a test case with multiple inline scripts then externalizing them', function (done) {
+        new AssetGraph({root: __dirname + '/externalizeAndMergeIdenticalAssets/'})
+            .loadAssets('first.html', 'second.html')
+            .populate()
+            .externalizeRelations({type: 'HtmlScript'})
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', {type: 'JavaScript', isInline: false}, 7);
+            })
+            .mergeIdenticalAssets({type: 'JavaScript'})
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 'JavaScript', 3);
+
+                var typeTwos = assetGraph.findAssets({type: 'JavaScript', text: /TypeTwo/});
+                expect(typeTwos, 'to have length', 1);
+                expect(assetGraph, 'to contain relation', {from: {url: /first\.html$/}, to: typeTwos[0]});
+                expect(assetGraph, 'to contain relation', {from: {url: /second\.html$/}, to: typeTwos[0]});
+
+                var typeThrees = assetGraph.findAssets({type: 'JavaScript', text: /TypeThree/});
+                expect(typeThrees, 'to have length', 1);
+                expect(assetGraph, 'to contain relation', {from: {url: /first\.html$/}, to: typeThrees[0]});
+                expect(assetGraph, 'to contain relation', {from: {url: /second\.html$/}, to: typeThrees[0]});
+
+                expect(assetGraph, 'to contain relations', {
+                    from: assetGraph.findAssets({url: /first\.html$/})[0],
+                    to: {
+                        text: /TypeOne/
+                    }
+                }, 2);
+            })
+            .run(done);
+    });
+});
