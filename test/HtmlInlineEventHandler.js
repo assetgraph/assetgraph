6dec22c8b3bc66553a931d77f8c4b852c697bcba
Andreas Lind Petersen <andreas@one.com>
2014-05-05 01:12:31 +0200

    Converted HtmlInlineEventHandler test to mocha.

---
+++ test/HtmlInlineEventHandler.js
@@ -0,0 +1,21 @@
+var expect = require('./unexpected-with-plugins'),
+    AssetGraph = require('../lib');
+
+describe('HtmlInlineEventHandler', function () {
+    it('should handle a test case with existing inline event handlers', function (done) {
+        new AssetGraph({root: __dirname + '/HtmlInlineEventHandler/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 4);
+                expect(assetGraph, 'to contain asset', 'Html');
+                expect(assetGraph, 'to contain assets', 'JavaScript', 3);
+                expect(assetGraph, 'to contain relations', 'HtmlInlineEventHandler', 3);
+                assetGraph.findAssets({type: 'JavaScript'}).forEach(function (javaScript) {
+                    javaScript.text = javaScript.text.replace(/this/g, 'that');
+                });
+                expect(assetGraph.findAssets({type: 'Html'})[0].text, 'to match', /that\.focused.*that\.focused/);
+            })
+            .run(done);
+    });
+});
---
+++ test/JavaScriptInclude.js
@@ -0,0 +1,129 @@
+var expect = require('./unexpected-with-plugins'),
+    _ = require('underscore'),
+    urlTools = require('urltools'),
+    AssetGraph = require('../lib');
+
+describe('JavaScriptInclude', function () {
+    it('should handle a combo test case', function (done) {
+        new AssetGraph({root: __dirname + '/JavaScriptInclude/topLevelStatements/'})
+            .loadAssets('index.js')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 'JavaScript', 3);
+                expect(assetGraph, 'to contain relations', 'JavaScriptInclude', 2);
+
+                assetGraph.findRelations({type: 'JavaScriptInclude'})[0].detach();
+                expect(/INCLUDE\([\'\"]one\.js\1\)/.test(assetGraph.findAssets({url: /\/index\.js$/})[0].text), 'to be false');
+
+                var newJavaScriptAsset = new AssetGraph.JavaScript({
+                    url: urlTools.resolveUrl(assetGraph.root, 'quux.js'),
+                    text: "alert('quux.js');"
+                });
+                assetGraph.addAsset(newJavaScriptAsset);
+                new AssetGraph.JavaScriptInclude({
+                    to: newJavaScriptAsset
+                }).attach(assetGraph.findAssets({url: /\/index\.js$/})[0], 'first');
+
+                expect(_.pluck(assetGraph.findRelations({from: {url: /\/index\.js$/}}), 'href'), 'to equal',
+                                 ['quux.js', 'bar.js']);
+
+                newJavaScriptAsset = new AssetGraph.JavaScript({
+                    url: urlTools.resolveUrl(assetGraph.root, 'baz.js'),
+                    text: "alert('baz.js');"
+                });
+                assetGraph.addAsset(newJavaScriptAsset);
+                new AssetGraph.JavaScriptInclude({
+                    to: newJavaScriptAsset
+                }).attach(assetGraph.findAssets({url: /\/index\.js$/})[0], 'after', assetGraph.findRelations({to: {url: /\/quux\.js$/}})[0]);
+
+                expect(_.pluck(assetGraph.findRelations({from: {url: /\/index\.js$/}}), 'href'), 'to equal',
+                                 ['quux.js', 'baz.js', 'bar.js']);
+
+                newJavaScriptAsset = new AssetGraph.JavaScript({
+                    url: urlTools.resolveUrl(assetGraph.root, 'bazze.js'),
+                    text: "alert('bazze.js');"
+                });
+                assetGraph.addAsset(newJavaScriptAsset);
+                new AssetGraph.JavaScriptInclude({
+                    to: newJavaScriptAsset
+                }).attach(assetGraph.findAssets({url: /\/index\.js$/})[0], 'before', assetGraph.findRelations({to: {url: /\/bar\.js$/}})[0]);
+
+                expect(_.pluck(assetGraph.findRelations({from: {url: /\/index\.js$/}}), 'href'), 'to equal',
+                                 ['quux.js', 'baz.js', 'bazze.js', 'bar.js']);
+
+                newJavaScriptAsset = new AssetGraph.JavaScript({
+                    url: urlTools.resolveUrl(assetGraph.root, 'prinzenrolle.js'),
+                    text: "alert('prinzenrolle.js');"
+                });
+                assetGraph.addAsset(newJavaScriptAsset);
+                new AssetGraph.JavaScriptInclude({
+                    to: newJavaScriptAsset
+                }).attach(assetGraph.findAssets({url: /\/index\.js$/})[0], 'last');
+
+                expect(_.pluck(assetGraph.findRelations({from: {url: /\/index\.js$/}}), 'href'), 'to equal',
+                                 ['quux.js', 'baz.js', 'bazze.js', 'bar.js', 'prinzenrolle.js']);
+            })
+            .run(done);
+    });
+
+    it('should handle the same test case with original INCLUDE statements in one sequenced statement', function (done) {
+        new AssetGraph({root: __dirname + '/JavaScriptInclude/sequencedStatements/'})
+            .loadAssets('index.js')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 'JavaScript', 3);
+                expect(assetGraph, 'to contain relations', 'JavaScriptInclude', 2);
+                assetGraph.findRelations({type: 'JavaScriptInclude'})[0].detach();
+                expect(/INCLUDE\([\'\"]one\.js\1\)/.test(assetGraph.findAssets({url: /\/index\.js$/})[0].text), 'to be false');
+
+                var newJavaScriptAsset = new AssetGraph.JavaScript({
+                    url: urlTools.resolveUrl(assetGraph.root, 'quux.js'),
+                    text: "alert('quux.js');"
+                });
+                assetGraph.addAsset(newJavaScriptAsset);
+                new AssetGraph.JavaScriptInclude({
+                    to: newJavaScriptAsset
+                }).attach(assetGraph.findAssets({url: /\/index\.js$/})[0], 'first');
+
+                expect(_.pluck(assetGraph.findRelations({from: {url: /\/index\.js$/}}), 'href'), 'to equal',
+                                 ['quux.js', 'bar.js']);
+
+                newJavaScriptAsset = new AssetGraph.JavaScript({
+                    url: urlTools.resolveUrl(assetGraph.root, 'baz.js'),
+                    text: "alert('baz.js');"
+                });
+                assetGraph.addAsset(newJavaScriptAsset);
+                new AssetGraph.JavaScriptInclude({
+                    to: newJavaScriptAsset
+                }).attach(assetGraph.findAssets({url: /\/index\.js$/})[0], 'after', assetGraph.findRelations({to: {url: /\/quux\.js$/}})[0]);
+
+                expect(_.pluck(assetGraph.findRelations({from: {url: /\/index\.js$/}}), 'href'), 'to equal',
+                                 ['quux.js', 'baz.js', 'bar.js']);
+
+                newJavaScriptAsset = new AssetGraph.JavaScript({
+                    url: urlTools.resolveUrl(assetGraph.root, 'bazze.js'),
+                    text: "alert('bazze.js');"
+                });
+                assetGraph.addAsset(newJavaScriptAsset);
+                new AssetGraph.JavaScriptInclude({
+                    to: newJavaScriptAsset
+                }).attach(assetGraph.findAssets({url: /\/index\.js$/})[0], 'before', assetGraph.findRelations({to: {url: /\/bar\.js$/}})[0]);
+
+                expect(_.pluck(assetGraph.findRelations({from: {url: /\/index\.js$/}}), 'href'), 'to equal',
+                                 ['quux.js', 'baz.js', 'bazze.js', 'bar.js']);
+
+                newJavaScriptAsset = new AssetGraph.JavaScript({
+                    url: urlTools.resolveUrl(assetGraph.root, 'prinzenrolle.js'),
+                    text: "alert('prinzenrolle.js');"
+                });
+                assetGraph.addAsset(newJavaScriptAsset);
+                new AssetGraph.JavaScriptInclude({
+                    to: newJavaScriptAsset
+                }).attach(assetGraph.findAssets({url: /\/index\.js$/})[0], 'last');
+
+                expect(_.pluck(assetGraph.findRelations({from: {url: /\/index\.js$/}}), 'href'), 'to equal',
+                                 ['quux.js', 'baz.js', 'bazze.js', 'bar.js', 'prinzenrolle.js']);
+            })
+            .run(done);
+    });
+});
