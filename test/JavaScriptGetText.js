900398160264bac745bf9a92f60302aab6a1318f
Andreas Lind Petersen <andreas@one.com>
2014-05-04 22:12:03 +0200

    Converted JavaScriptGetText test to mocha.

---
+++ test/JavaScriptGetText.js
@@ -0,0 +1,29 @@
+var expect = require('./unexpected-with-plugins'),
+    AssetGraph = require('../lib');
+
+describe('JavaScriptGetText', function () {
+    it('should handle a test case with an existing GETTEXT function call', function (done) {
+        new AssetGraph({root: __dirname + '/JavaScriptGetText/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 3);
+                expect(assetGraph, 'to contain assets', 'Html', 2);
+            })
+            .inlineRelations({type: 'JavaScriptGetText'})
+            .queue(function (assetGraph) {
+                expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to match', /var myHtmlString\s*=\s*(['"])<html><body>Boo!<\/body><\/html>\\n\1/);
+                var htmlAsset = assetGraph.findAssets({type: 'Html', isInline: true})[0],
+                    document = htmlAsset.parseTree;
+                document.body.appendChild(document.createElement('div')).appendChild(document.createTextNode('foo'));
+                htmlAsset.markDirty();
+
+                expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to match', /var myHtmlString\s*=\s*(['"])<html><body>Boo!<div>foo<\/div><\/body><\/html>\\n\1/);
+
+                assetGraph.findAssets({type: 'Html', isInline: true})[0].url = 'http://example.com/template.html';
+
+                expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to match', /var myHtmlString\s*=\s*GETTEXT\((['"])http:\/\/example\.com\/template\.html\1\)/);
+            })
+            .run(done);
+    });
+});
