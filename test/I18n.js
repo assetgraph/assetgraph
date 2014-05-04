21d4b87b36b91137c9632df1542e39c6823404a9
Andreas Lind Petersen <andreas@one.com>
2014-05-04 21:10:39 +0200

    Converted I18n test to mocha.

---
+++ test/I18n.js
@@ -0,0 +1,19 @@
+var expect = require('./unexpected-with-plugins'),
+    AssetGraph = require('../lib');
+
+describe('I18n', function () {
+    it('should handle a test case where an I18n asset is created and pretty-printed', function () {
+        var i18nAsset = new AssetGraph.I18n({text: '{"c": {"b": {"e": false, "d": true}, "a": 3}, "b": 2}'});
+
+        expect(Object.keys(i18nAsset.parseTree), 'to equal', ['c', 'b']);
+
+        i18nAsset.prettyPrint();
+        expect(Object.keys(i18nAsset.parseTree), 'to equal', ['b', 'c']);
+        expect(Object.keys(i18nAsset.parseTree.c), 'to equal', ['a', 'b']);
+        expect(Object.keys(i18nAsset.parseTree.c.b), 'to equal', ['e', 'd']);
+
+        i18nAsset.parseTree.a = {foo: 'bar'};
+        i18nAsset.prettyPrint();
+        expect(Object.keys(i18nAsset.parseTree), 'to equal', ['a', 'b', 'c']);
+    });
+});
