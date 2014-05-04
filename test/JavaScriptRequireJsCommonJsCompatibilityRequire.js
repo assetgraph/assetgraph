3fe1d7c9b2ceef65525981d820688c6aed888cc3
Andreas Lind Petersen <andreas@one.com>
2014-05-04 22:38:45 +0200

    Converted JavaScriptRequireJsCommonJsCompatibilityRequire test to mocha.
    Except one test which wasn't run by vows due to a duplicate key in an object (and it didn't pass).

---
+++ test/JavaScriptRequireJsCommonJsCompatibilityRequire.js
@@ -0,0 +1,62 @@
+var expect = require('./unexpected-with-plugins'),
+    urlTools = require('urltools'),
+    AssetGraph = require('../lib');
+
+describe('JavaScriptRequireJsCommonJsCompatibilityRequire', function () {
+    it('should handle a test case that also has some common.js requires', function (done) {
+        new AssetGraph({root: __dirname + '/JavaScriptRequireJsCommonJsCompatibilityRequire/withCommonJsShorthand/'})
+            .registerRequireJsConfig()
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 5);
+                expect(assetGraph, 'to contain no relations', 'JavaScriptAmdDefine');
+                expect(assetGraph, 'to contain relation', 'JavaScriptRequireJsCommonJsCompatibilityRequire');
+                expect(assetGraph, 'to contain no relations', {type: 'JavaScriptCommonJsRequire'});
+                expect(assetGraph, 'to contain asset', {url: /\/over\/the\/rainbow\/foo\.js$/, isLoaded: true});
+            })
+            .liftUpJavaScriptRequireJsCommonJsCompatibilityRequire()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relation', 'JavaScriptAmdDefine');
+                expect(assetGraph, 'to contain asset', {text: /\["require","somewhere\/foo"\]/});
+            })
+            .flattenRequireJs({type: 'Html'})
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relation', {type: 'HtmlScript', from: {type: 'Html'}, to: {text: /return 42/}});
+            })
+            .run(done);
+    });
+
+    it('should handle the ACE editor', function (done) {
+        this.timeout(10000);
+        new AssetGraph({root: __dirname + '/JavaScriptRequireJsCommonJsCompatibilityRequire/ace/'})
+            .registerRequireJsConfig()
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 76);
+                expect(assetGraph, 'to contain relations', 'JavaScriptRequireJsCommonJsCompatibilityRequire', 168);
+                expect(assetGraph, 'to contain no relations', {type: 'JavaScriptAmdDefine'});
+            })
+            .liftUpJavaScriptRequireJsCommonJsCompatibilityRequire()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations', 'JavaScriptAmdDefine', 168);
+                expect(assetGraph, 'to contain relations', 'JavaScriptRequireJsCommonJsCompatibilityRequire', 168);
+            })
+            .flattenRequireJs()
+            .inlineRelations({type: 'JavaScriptGetText'})
+            .queue(function (assetGraph) {
+                // Executing all the scripts except the last one should result in no errors
+                var vm = require('vm'),
+                    context = vm.createContext();
+                assetGraph.findRelations({type: 'HtmlScript'}).forEach(function (relation) {
+                    var src = relation.to.text;
+                    if (/define\((["'])main\1/.test(src)) {
+                        return;
+                    }
+                    vm.runInContext(src, context, relation.to.url);
+                });
+            })
+            .run(done);
+    });
+});
--- test/JavaScriptRequireJsCommonJsCompatibilityRequire/withCommonJs/commonJsIsh.js
+++
@@ -1,9 +0,0 @@
-require('./loadedViaCommonJs1'); // This should be interpreted as a JavaScriptCommonJsRequire
-
-define(function (require, exports, module) {
-    var foo = require('somewhere/foo');
-    exports.someNumber = 18;
-    exports.foo = foo;
-});
-
-require('./loadedViaCommonJs2'); // This should be interpreted as a JavaScriptCommonJsRequire
