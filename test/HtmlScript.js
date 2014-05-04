e7c62594935e758886b729885350c7d6fc038cd2
Andreas Lind Petersen <andreas@one.com>
2014-05-04 13:37:38 +0200

    Converted HtmlScript test to mocha.

---
+++ test/HtmlScript.js
@@ -0,0 +1,24 @@
+var expect = require('./unexpected-with-plugins'),
+    _ = require('underscore'),
+    AssetGraph = require('../lib'),
+    query = AssetGraph.query;
+
+describe('HtmlScript', function () {
+    it('should handle a test case with existing <script> elements', function (done) {
+        new AssetGraph({root: __dirname + '/HtmlScript/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations including unresolved', 'HtmlScript', 6);
+                expect(_.pluck(assetGraph.findRelations(), 'href'), 'to equal', [
+                    'externalNoType.js',
+                    undefined,
+                    'externalWithTypeTextJavaScript.js',
+                    undefined,
+                    'externalWithTypeTextCoffeeScript.js',
+                    undefined
+                ]);
+            })
+            .run(done);
+    });
+});
