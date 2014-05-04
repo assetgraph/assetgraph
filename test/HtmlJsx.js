91fb31ae46045629bb8857980743ae1394ad9446
Andreas Lind Petersen <andreas@one.com>
2014-05-04 13:07:41 +0200

    Converted HtmlJsx test to mocha.

---
+++ test/HtmlJsx.js
@@ -0,0 +1,24 @@
+var expect = require('./unexpected-with-plugins'),
+    _ = require('underscore'),
+    AssetGraph = require('../lib');
+
+describe('HtmlJsx', function () {
+    it('should handle a test case with existing <script type="text/jsx"> elements', function (done) {
+        new AssetGraph({root: __dirname + '/HtmlJsx/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations including unresolved', 'HtmlJsx', 2);
+                expect(_.pluck(assetGraph.findRelations(), 'href'), 'to equal', [
+                    'externalWithTypeTextJsx.js',
+                    undefined
+                ]);
+
+                var assets = _.pluck(assetGraph.findRelations(), 'to');
+
+                expect(assets[0].text, 'to equal', "'external'\n");
+                expect(assets[1].text, 'to equal', "'inline'");
+            })
+            .run(done);
+    });
+});
