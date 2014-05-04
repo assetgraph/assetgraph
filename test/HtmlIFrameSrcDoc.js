5dfbd79faaf99cbaed28f4f7b247a5a1a396f531
Andreas Lind Petersen <andreas@one.com>
2014-05-04 13:05:41 +0200

    Converted HtmlInlineScriptTemplate test to mocha.

--- test/HtmlIFrameSrcDoc.js
+++ test/HtmlIFrameSrcDoc.js
@@ -1,43 +1,27 @@
-var vows = require('vows'),
-    expect = require('./unexpected-with-plugins'),
+var expect = require('./unexpected-with-plugins'),
     AssetGraph = require('../lib');
 
-vows.describe('HtmlIFrameSrcDoc').addBatch({
-    'After loading test case': {
-        topic: function () {
-            new AssetGraph({root: __dirname + '/HtmlIFrameSrcDoc/'})
-                .loadAssets('index.html')
-                .populate({
-                    followRelations: {to: {url: /^file:/}}
-                })
-                .run(done);
-        },
-        'the graph should contain 3 Html assets': function (assetGraph) {
-            expect(assetGraph, 'to contain assets', 'Html', 3);
-        },
-        'the graph should contain one inline Html asset': function (assetGraph) {
-            expect(assetGraph, 'to contain asset', {type: 'Html', isInline: true});
-        },
-        'the graph should contain one HtmlIFrame relation': function (assetGraph) {
-            expect(assetGraph, 'to contain relation', 'HtmlIFrame');
-        },
-        'the graph should contain one HtmlIFrameSrcDoc relation': function (assetGraph) {
-            expect(assetGraph, 'to contain relation', 'HtmlIFrameSrcDoc');
-        },
-        'the graph should contain one HtmlAnchor relation': function (assetGraph) {
-            expect(assetGraph, 'to contain relation', 'HtmlAnchor');
-        },
-        'then updating the inline Html asset pointed to by the HtmlIFrameSrcDoc relation': {
-            topic: function (assetGraph) {
+describe('HtmlIFrameSrcDoc', function () {
+    it('should handle a test case with an existing <iframe srcdoc=...> element', function (done) {
+        new AssetGraph({root: __dirname + '/HtmlIFrameSrcDoc/'})
+            .loadAssets('index.html')
+            .populate({
+                followRelations: {to: {url: /^file:/}}
+            })
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 'Html', 3);
+                expect(assetGraph, 'to contain asset', {type: 'Html', isInline: true});
+                expect(assetGraph, 'to contain relation', 'HtmlIFrame');
+                expect(assetGraph, 'to contain relation', 'HtmlIFrameSrcDoc');
+                expect(assetGraph, 'to contain relation', 'HtmlAnchor');
+
                 var asset = assetGraph.findRelations({type: 'HtmlIFrameSrcDoc'})[0].to,
                     document = asset.parseTree;
                 document.firstChild.appendChild(document.createTextNode('Hello from the outside!'));
                 asset.markDirty();
-                return assetGraph;
-            },
-            'the serialization of the containing document should contain the inserted text': function (assetGraph) {
+
                 expect(assetGraph.findAssets({url: /\/index\.html$/})[0].text, 'to match', /Hello from the outside!/);
-            }
-        }
-    }
-})['export'](module);
+            })
+            .run(done);
+    });
+});
--- test/HtmlImport.js
+++ test/HtmlImport.js
@@ -1,29 +1,22 @@
-var vows = require('vows'),
-    expect = require('./unexpected-with-plugins'),
+var expect = require('./unexpected-with-plugins'),
     AssetGraph = require('../lib');
 
-vows.describe('Html with <link rel="import">').addBatch({
-    'After loading test case': {
-        topic: function () {
-            new AssetGraph({root: __dirname + '/HtmlImport/'})
-                .loadAssets('index.html')
-                .populate()
-                .run(done);
-        },
-        'the graph should contain 3 HtmlImport relations': function (assetGraph) {
-            expect(assetGraph, 'to contain relations', 'HtmlImport', 3);
-        },
-        'the graph should contain 4 populated Html assets': function (assetGraph) {
-            expect(assetGraph, 'to contain assets', {
-                type: 'Html',
-                isPopulated: true
-            }, 4);
-        },
-        'the graph should contain 1 populated Css asset': function (assetGraph) {
-            expect(assetGraph, 'to contain assets', {
-                type: 'Css',
-                isPopulated: true
-            }, 1);
-        }
-    }
-})['export'](module);
+describe('HtmlImport', function () {
+    it('should handle a test case with an existing <link rel="import"> element', function (done) {
+        new AssetGraph({root: __dirname + '/HtmlImport/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations', 'HtmlImport', 3);
+                expect(assetGraph, 'to contain assets', {
+                    type: 'Html',
+                    isPopulated: true
+                }, 4);
+                expect(assetGraph, 'to contain assets', {
+                    type: 'Css',
+                    isPopulated: true
+                }, 1);
+            })
+            .run(done);
+    });
+});
