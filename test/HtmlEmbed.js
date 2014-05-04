1b24c36091edbf68c5c8f307d05b7971a67ab254
Andreas Lind Petersen <andreas@one.com>
2014-05-04 12:56:26 +0200

    Converted HtmlEmbed test to mocha.

---
+++ test/HtmlEmbed.js
@@ -0,0 +1,20 @@
+var expect = require('./unexpected-with-plugins'),
+    urlTools = require('urltools'),
+    AssetGraph = require('../lib'),
+    query = AssetGraph.query;
+
+describe('HtmlEmbed', function () {
+    it('should handle a test case with an existing <embed src="..."> element', function (done) {
+        new AssetGraph({root: __dirname + '/HtmlEmbed/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relation', 'HtmlEmbed');
+                expect(assetGraph, 'to contain asset', 'Flash');
+                expect(assetGraph, 'to contain relation including unresolved', {type: 'HtmlEmbed', href: 'foo.swf'});
+                assetGraph.findAssets({type: 'Html'})[0].url = urlTools.resolveUrl(assetGraph.root, 'foo/index.html');
+                expect(assetGraph, 'to contain relation including unresolved', {type: 'HtmlEmbed', href: '../foo.swf'});
+            })
+            .run(done);
+    });
+});
