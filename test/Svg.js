70110a6c73f2d191c27291039a8a6a2134275687
Andreas Lind Petersen <andreas@one.com>
2014-05-05 00:39:16 +0200

    Converted Svg test to mocha.

---
+++ test/Svg.js
@@ -0,0 +1,28 @@
+var expect = require('./unexpected-with-plugins'),
+    AssetGraph = require('../lib');
+
+describe('Svg', function () {
+    it('should handle a test case with an Svg asset', function (done) {
+        new AssetGraph({root: __dirname + '/Svg/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 11);
+                expect(assetGraph, 'to contain asset', 'Html');
+                expect(assetGraph, 'to contain asset', 'Svg');
+                expect(assetGraph, 'to contain assets', 'JavaScript', 2);
+                expect(assetGraph, 'to contain assets', 'Png', 2);
+                expect(assetGraph, 'to contain asset', 'Xslt');
+                expect(assetGraph, 'to contain relation', 'SvgImage');
+                expect(assetGraph, 'to contain relation', 'SvgScript');
+                expect(assetGraph, 'to contain relation', 'SvgStyleAttribute');
+                expect(assetGraph, 'to contain relation', 'CssImage');
+                expect(assetGraph, 'to contain relations', 'SvgStyle', 2);
+                expect(assetGraph, 'to contain relation', 'SvgFontFaceUri');
+                expect(assetGraph, 'to contain relation', 'XmlStylesheet');
+                expect(assetGraph, 'to contain relation', 'SvgInlineEventHandler');
+                expect(assetGraph, 'to contain relation', 'SvgAnchor');
+            })
+            .run(done);
+    });
+});
