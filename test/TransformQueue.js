86fd541ac03512ba8c3d49c8204193e8aa7ba78e
Andreas Lind Petersen <andreas@one.com>
2014-05-04 23:21:22 +0200

    Converted nestedTransforms test to mocha and renamed it to TransformQueue.

---
+++ test/TransformQueue.js
@@ -0,0 +1,60 @@
+var expect = require('./unexpected-with-plugins'),
+    AssetGraph = require('../lib');
+
+AssetGraph.registerTransform(function pushItemToArraySync(item, array) {
+    return function (assetGraph) {
+        array.push(item);
+    };
+});
+
+AssetGraph.registerTransform(function pushItemToArrayAsync(item, array) {
+    return function (assetGraph, cb) {
+        process.nextTick(function () {
+            array.push(item);
+            cb();
+        });
+    };
+});
+
+describe('TransformQueue', function () {
+    it('should handle multiple levels of nested transforms', function (done) {
+        var array = [];
+        new AssetGraph()
+            .pushItemToArrayAsync('a', array)
+            .pushItemToArraySync('b', array)
+            .pushItemToArrayAsync('c', array)
+            .pushItemToArrayAsync('d', array)
+            .queue(function (assetGraph, cb) {
+                assetGraph
+                    .pushItemToArrayAsync('e', array)
+                    .pushItemToArraySync('f', array)
+                    .pushItemToArrayAsync('g', array)
+                    .pushItemToArrayAsync('h', array)
+                    .queue(function (assetGraph, cb) {
+                        assetGraph
+                            .pushItemToArrayAsync('i', array)
+                            .pushItemToArraySync('j', array)
+                            .pushItemToArrayAsync('k', array)
+                            .run(cb);
+                    })
+                    .pushItemToArrayAsync('l', array)
+                    .pushItemToArraySync('m', array)
+                    .pushItemToArrayAsync('n', array)
+                    .queue(function (assetGraph, cb) {
+                        assetGraph
+                            .pushItemToArrayAsync('o', array)
+                            .pushItemToArraySync('p', array)
+                            .run(cb);
+                    })
+                    .pushItemToArraySync('q', array)
+                    .pushItemToArrayAsync('r', array)
+                    .run(cb);
+            })
+            .pushItemToArraySync('s', array)
+            .pushItemToArrayAsync('t', array)
+            .queue(function () {
+                expect(array, 'to equal', ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't']);
+            })
+            .run(done);
+    });
+});
