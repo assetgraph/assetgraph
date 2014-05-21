65fd193c43a279a52464994df157e309a2b72fc6
Gustav Nikolaj Olsen <gno@one.com>
2014-05-21 22:32:36 +0200

    add simplest possible jsxtransform

---
+++ test/transforms/compileJsxToJs.js
@@ -0,0 +1,34 @@
+/* global describe, it */
+var expect = require('../unexpected-with-plugins'),
+    AssetGraph = require('../../lib'),
+    query = AssetGraph.query;
+
+describe('transforms/compileJsxToJs', function () {
+    it('should compile all Jsx assets to Js', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/transforms/compileJsxToJs/'})
+            .loadAssets('index.html')
+            .populate({followRelations: {to: {url: query.not(/^http:/)}}})
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain asset', 'Jsx');
+            })
+            .compileJsxToJs({type: 'Jsx'})
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain no assets', 'Jsx');
+                expect(assetGraph, 'to contain asset', 'JavaScript');
+
+                var htmlText = assetGraph.findAssets({type: 'Html'})[0].text;
+                expect(htmlText, 'not to contain', 'helloWorld.jsx');
+                expect(htmlText, 'to contain', '<script src="helloWorld.js"></script>');
+
+                expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to equal', [
+                    '/** @jsx React.DOM */',
+                    'React.renderComponent(',
+                    '  React.DOM.h1(null, "Hello, world!"),',
+                    '  document.getElementById(\'example\')',
+                    ');',
+                    ''
+                ].join('\n'));
+            })
+            .run(done);
+    });
+});
---
+++ testdata/transforms/compileJsxToJs/helloWorld.jsx
@@ -0,0 +1,5 @@
+/** @jsx React.DOM */
+React.renderComponent(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
