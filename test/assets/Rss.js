82aa9a5d0e95ebcfe74e49c7878d256c4fc54cfe
Andreas Lind <andreas@one.com>
2014-06-14 00:15:00 +0200

    Rss: Model <channel><link>...</link></channel> as a relation.
    Relates to #198.

--- test/assets/Rss.js
+++ test/assets/Rss.js
@@ -12,6 +12,12 @@ describe('assets/Rss', function () {
               expect(assetGraph, 'to contain asset', 'Png');
               expect(assetGraph, 'to contain assets', {type: 'Html', isInline: true}, 2);
               expect(assetGraph, 'to contain assets', {type: 'Html', isInline: false}, 1);
+              expect(assetGraph, 'to contain relation', 'RssChannelLink');
+
+              assetGraph.findAssets({fileName: 'bar.html'})[0].url = 'http://example.com/bar.html';
+              assetGraph.findRelations({type: 'RssChannelLink'})[0].hrefType = 'absolute';
+
+              expect(assetGraph, 'to contain asset', {type: 'Rss', text: /<link>http:\/\/example.com\/bar.html<\/link>/});
           })
           .run(done);
     });
