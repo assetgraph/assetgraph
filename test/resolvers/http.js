aa125e4f6f2e6d2e30a574930d4da569b44fd788
Andreas Lind Petersen <andreas@one.com>
2014-06-27 16:25:25 +0200

    http resolver: Avoid swallowing errors occurring after the response has started.

--- test/resolvers/http.js
+++ test/resolvers/http.js
@@ -140,7 +140,7 @@ describe('resolvers/http', function () {
             .loadAssets('/foo.html')
             .populate()
             .queue(function (assetGraph) {
-                expect(warnings, 'to have length', 0);
+                expect(warnings, 'to equal', []);
                 expect(requestHandler, 'was called twice');
                 expect(assetGraph, 'to contain asset', {type: 'Html', text: 'Foo'});
                 server.close();
@@ -172,9 +172,70 @@ describe('resolvers/http', function () {
             .loadAssets('/foo.html')
             .populate()
             .queue(function (assetGraph) {
-                expect(warnings, 'to have length', 0);
+                expect(warnings, 'to equal', []);
                 expect(requestHandler, 'was called twice');
                 expect(assetGraph, 'to contain asset', {type: 'Html', text: 'Foo'});
+                server.close();
+            })
+            .run(done);
+    });
+
+    it('should successfully load the asset when a request that got a 500 succeeded after retrying', function (done) {
+        var requestHandler = sinon.spy(function (req, res) {
+                res.writeHead(requestHandler.callCount > 1 ? 200 : 500, {
+                    'Content-Type': 'text/plain; charset=UTF-8'
+                });
+                res.end('Foo');
+            }),
+            server = http.createServer(requestHandler).listen(0),
+            serverAddress = server.address(),
+            rootUrl = 'http://' + serverAddress.address + ':' + serverAddress.port + '/',
+            warnings = [];
+
+        new AssetGraph({root: rootUrl})
+            .on('warn', function (err) {
+                warnings.push(err);
+            })
+            .queue(function (assetGraph) {
+                assetGraph.resolverByProtocol.http.requestOptions = {numRetries: 2, timeout: 50};
+            })
+            .loadAssets('/foo.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(warnings, 'to equal', []);
+                expect(requestHandler, 'was called twice');
+                expect(assetGraph, 'to contain asset', {type: 'Html', text: 'Foo'});
+                server.close();
+            })
+            .run(done);
+    });
+
+    it('should not retry when getting a 4xx response', function (done) {
+        var requestHandler = sinon.spy(function (req, res) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain; charset=UTF-8'
+                });
+                res.end('Foo');
+            }),
+            server = http.createServer(requestHandler).listen(0),
+            serverAddress = server.address(),
+            rootUrl = 'http://' + serverAddress.address + ':' + serverAddress.port + '/',
+            warnings = [];
+
+        new AssetGraph({root: rootUrl})
+            .on('warn', function (err) {
+                warnings.push(err);
+            })
+            .queue(function (assetGraph) {
+                assetGraph.resolverByProtocol.http.requestOptions = {numRetries: 2, timeout: 50};
+            })
+            .loadAssets('/foo.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(warnings, 'to have length', 1);
+                expect(warnings[0].NotFound, 'to be true');
+                expect(requestHandler, 'was called once');
+                expect(assetGraph, 'to contain no assets', {type: 'Html', text: 'Foo'});
                 server.close();
             })
             .run(done);
@@ -199,14 +260,47 @@ describe('resolvers/http', function () {
                 warnings.push(err);
             })
             .queue(function (assetGraph) {
+                assetGraph.resolverByProtocol.http.requestOptions = {numRetries: 2, timeout: 1000};
+            })
+            .loadAssets('/foo.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(warnings, 'to equal', []);
+                expect(requestHandler, 'was called twice');
+                expect(assetGraph, 'to contain asset', {type: 'Html', text: 'Foo'});
+                server.close();
+            })
+            .run(done);
+    });
+
+    it('should successfully load the asset when a timed out request that had the headers and part of the response body sent succeeds after retrying', function (done) {
+        var requestHandler = sinon.spy(function (req, res) {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html; charset=UTF-8'
+                });
+                res.write('Foo');
+                if (requestHandler.callCount > 1) {
+                    res.end('Bar');
+                }
+            }),
+            server = http.createServer(requestHandler).listen(0),
+            serverAddress = server.address(),
+            rootUrl = 'http://' + serverAddress.address + ':' + serverAddress.port + '/',
+            warnings = [];
+
+        new AssetGraph({root: rootUrl})
+            .on('warn', function (err) {
+                warnings.push(err);
+            })
+            .queue(function (assetGraph) {
                 assetGraph.resolverByProtocol.http.requestOptions = {numRetries: 2, timeout: 50};
             })
             .loadAssets('/foo.html')
             .populate()
             .queue(function (assetGraph) {
-                expect(warnings, 'to have length', 0);
                 expect(requestHandler, 'was called twice');
-                expect(assetGraph, 'to contain asset', {type: 'Html', text: 'Foo'});
+                expect(warnings, 'to equal', []);
+                expect(assetGraph, 'to contain asset', {type: 'Html', text: 'FooBar'});
                 server.close();
             })
             .run(done);
