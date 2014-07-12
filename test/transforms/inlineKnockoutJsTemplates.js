f212eb5d2e9155f81d6c0f2f46a560e5bde3adb0
Munawwar Firoz <munawwarfiroz@hotmail.com>
2014-07-12 12:25:14 +0400

    Added test case for embedded templates without id. assetgraph/assetgraph-builder#131.

--- test/transforms/inlineKnockoutJsTemplates.js
+++ test/transforms/inlineKnockoutJsTemplates.js
@@ -8,19 +8,21 @@ describe('transforms/inlineKnockoutJsTemplates', function () {
             .loadAssets('index.html')
             .populate()
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain assets', 'Html', 5);
+                expect(assetGraph, 'to contain assets', 'Html', 7);
                 expect(assetGraph, 'to contain assets', {type: 'JavaScript', isInline: false}, 3);
                 expect(assetGraph, 'to contain relations', {type: ['JavaScriptAmdRequire', 'JavaScriptAmdDefine'], to: {type: 'Html'}}, 4);
                 expect(assetGraph, 'to contain relation', 'JavaScriptGetStaticUrl');
-                expect(assetGraph, 'to contain assets', 'Html', 5);
                 expect(assetGraph, 'to contain asset', 'Png');
             })
             .inlineKnockoutJsTemplates()
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain no relations', {type: ['JavaScriptAmdRequire', 'JavaScriptAmdDefine'], to: {type: 'KnockoutJsTemplate'}});
-                expect(assetGraph, 'to contain relations', 'HtmlInlineScriptTemplate', 4);
-                expect(assetGraph, 'to contain relations', {type: 'HtmlInlineScriptTemplate', from: {url: /\/index\.html$/}}, 4);
-                expect(assetGraph.findAssets({url: /\/index\.html$/})[0].text, 'to equal', '<!DOCTYPE html>\n<html>\n<head></head>\n<body>\n    <script src="require.js" data-main="main"></script>\n<script type="text/html" id="foo"><img data-bind="attr: {src: GETSTATICURL(\'foo.png\')}">\n</script><script type="text/html" id="bar"><div>\n    <h1>bar.ko</h1>\n</div>\n</script><script type="text/html" id="templateWithEmbeddedTemplate"><div data-bind="template: \'theEmbeddedTemplate\'"></div>\n\n</script><script type="text/html" id="theEmbeddedTemplate" foo="bar">\n    <h1>This is the embedded template, which should also end up in the main document</h1>\n</script></body>\n</html>\n');
+                expect(assetGraph, 'to contain relations', 'HtmlInlineScriptTemplate', 6);
+                expect(assetGraph, 'to contain relations', {type: 'HtmlInlineScriptTemplate', from: {url: /\/index\.html$/}}, 6);
+                expect(assetGraph.findAssets({url: /\/index\.html$/})[0].text, 'to equal', '<!DOCTYPE html>\n<html>\n<head></head>\n<body>\n    <script src="require.js" data-main="main"></script>\n<script type="text/html" id="foo"><img data-bind="attr: {src: GETSTATICURL(\'foo.png\')}">\n</script><script type="text/html" id="bar"><div>\n    <h1>bar.ko</h1>\n</div>\n</script><script type="text/html" id="templateWithEmbeddedTemplate"><div data-bind="template: \'theEmbeddedTemplate\'"></div>\n\n\n\n</script>' +
+                    '<script type="text/html" id="theEmbeddedTemplate" foo="bar">\n    <h1>This is the embedded template, which should also end up in the main document</h1>\n</script>' +
+                    '<script type="text/html" foo="bar1">\n    <h1>This embedded template has no id. This too should end up in the main document, along with it\'s attributes</h1>\n</script>' +
+                    '<script type="text/html" foo="bar2">\n    <h1>This embedded template has no id. This too should end up in the main document, along with it\'s attributes</h1>\n</script></body>\n</html>\n');
 
                 var relation = assetGraph.findRelations({type: 'HtmlInlineScriptTemplate', node: function (node) { return node.getAttribute('id') === 'foo'; }})[0];
                 expect(relation, 'to be ok');
