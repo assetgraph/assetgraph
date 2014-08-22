97929cc4ba49028639ff788c0475e861132db84a
Gustav Nikolaj Olsen <gno@one.com>
2014-08-22 09:43:18 +0200

    model params attributes that can be parsed as javascript as inline assets.
    Required to support KnockoutJS 3.2 components with translation, etc.

---
+++ test/relations/HtmlParamsAttribute.js
@@ -0,0 +1,31 @@
+/*global describe, it*/
+var expect = require('../unexpected-with-plugins'),
+    AssetGraph = require('../../lib'),
+    uglifyJs = AssetGraph.JavaScript.uglifyJs;
+
+describe('relations/HtmlParamsAttribute', function () {
+    it('should handle a simple test case', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/relations/HtmlParamsAttribute/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 4);
+                expect(assetGraph, 'to contain asset', 'Html');
+                expect(assetGraph, 'to contain relations', 'HtmlParamsAttribute', 3);
+
+                assetGraph.findAssets({type: 'JavaScript'}).forEach(function (javaScript) {
+                    expect(javaScript.parseTree, 'to be an object');
+                });
+
+                var javaScript = assetGraph.findAssets({type: 'JavaScript', isInline: true})[0];
+                javaScript.parseTree.body[0].body.properties.push(new uglifyJs.AST_ObjectKeyVal({
+                    key: 'yup',
+                    value: new uglifyJs.AST_String({value: 'right'})
+                }));
+                javaScript.markDirty();
+
+                expect(assetGraph.findAssets({type: 'Html'})[0].text, 'to match', /yup/);
+            })
+            .run(done);
+    });
+});
