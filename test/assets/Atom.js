d0cdcadbd401552831eee45e492cc0ce036b96ae
Peter Müller <munter@fumle.dk>
2014-08-29 21:53:26 +0200

    Added tests for html fragments in Atom feeds

---
+++ test/assets/Atom.js
@@ -0,0 +1,22 @@
+/*global describe, it*/
+var expect = require('../unexpected-with-plugins'),
+    AssetGraph = require('../../lib');
+
+describe('assets/Atom', function () {
+    it('should find an Atom asset', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/assets/Atom/'})
+        .loadAssets('feed.atom')
+        .populate()
+        .queue(function (assetGraph) {
+            expect(assetGraph, 'to contain asset', 'Atom');
+            expect(assetGraph, 'to contain asset', 'Png');
+            expect(assetGraph, 'to contain assets', {type: 'Html', isInline: true}, 2);
+            expect(assetGraph, 'to contain assets', {type: 'Html', isInline: false}, 1);
+
+            assetGraph.findAssets({fileName: 'bar.html'})[0].url = 'http://example.com/bar.html';
+
+            expect(assetGraph, 'to contain asset', {type: 'Atom', text: /<link>http:\/\/example.com\/bar.html<\/link>/});
+        })
+        .run(done);
+    });
+});
