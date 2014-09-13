c304fb8bc62d3dd8d7045dcb21061fa745e92c2f
Peter Müller <munter@fumle.dk>
2014-09-13 20:42:21 +0200

    Rough implmentation of sub resource ntegrity attribute on ourgoing relations from html

---
+++ test/transforms/subResourceIntegrity.js
@@ -0,0 +1,29 @@
+/*global describe, it*/
+var expect = require('../unexpected-with-plugins'),
+    AssetGraph = require('../../lib'),
+    query = AssetGraph.query;
+
+describe('transforms/subResourceIntegrity', function () {
+    it('should load external sources', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/transforms/subResourceIntegrity/'})
+            .on('addAsset', function (asset) {
+                console.log('addAsset', asset.toString());
+            })
+            .loadAssets('index.html')
+            .populate({followRelations: {to: {url: query.not(/^https?:/)}}})
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain asset', 'Html');
+                expect(assetGraph, 'to contain no assets', 'JavaScript');
+            })
+            .subResourceIntegrity()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain asset', 'Html');
+                expect(assetGraph, 'to contain asset', 'JavaScript');
+
+                expect(assetGraph.findRelations(), 'to be an array whose items satisfy', function (relation) {
+                    expect(relation.node.getAttribute('integrity'), 'to match', /^ni/);
+                });
+            })
+            .run(done);
+    });
+});
