7ac6c78d88d224148e7b686e317c71a2fccbca3c
Peter Müller <munter@fumle.dk>
2014-09-28 21:41:01 +0200

    Attempt to make transform better at hitting the right assets. Not quite there yet

--- test/transforms/subResourceIntegrity.js
+++ test/transforms/subResourceIntegrity.js
@@ -1,13 +1,11 @@
 /*global describe, it*/
 var expect = require('../unexpected-with-plugins'),
-    AssetGraph = require('../../lib'),
-    query = AssetGraph.query;
+    AssetGraph = require('../../lib');
 
 describe('transforms/subResourceIntegrity', function () {
     it('should load external sources', function (done) {
         new AssetGraph({root: __dirname + '/../../testdata/transforms/subResourceIntegrity/'})
             .loadAssets('index.html')
-            .populate({followRelations: {to: {url: query.not(/^https?:/)}}})
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain asset', 'Html');
                 expect(assetGraph, 'to contain no assets', 'JavaScript');
@@ -17,8 +15,10 @@ describe('transforms/subResourceIntegrity', function () {
                 expect(assetGraph, 'to contain asset', 'Html');
                 expect(assetGraph, 'to contain asset', 'JavaScript');
 
-                expect(assetGraph.findRelations(), 'to be an array whose items satisfy', function (relation) {
-                    expect(relation.node.getAttribute('integrity'), 'to match', /^ni/);
+                var relations = assetGraph.findRelations();
+                expect(relations, 'to have length', 1);
+                expect(relations, 'to be an array whose items satisfy', function (relation) {
+                    expect(relation.node.getAttribute('integrity'), 'to be', 'ni:///sha256;h0cGsrExGgcZtSZ/fRz4AwV+Nn6Urh/3v3jFRQ0w9dQ=?ct=application/javascript');
                 });
             })
             .run(done);
