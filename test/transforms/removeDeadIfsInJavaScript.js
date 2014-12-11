65b70f58811d743d400ca918647655f4aace3f07
Andreas Lind <andreas@one.com>
2014-12-11 14:42:05 +0100

    Added transform for removing some specific if constructs in JavaScript. Poor man's UglifyJS.

---
+++ test/transforms/removeDeadIfsInJavaScript.js
@@ -0,0 +1,45 @@
+/*global describe, it*/
+var expect = require('../unexpected-with-plugins'),
+    AssetGraph = require('../../lib');
+
+describe('transforms/removeDeadIfsInJavaScript', function () {
+    it('should remove and preserve the correct if statements', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/transforms/removeDeadIfsInJavaScript/'})
+.logEvents()
+            .loadAssets('index.js')
+            .removeDeadIfsInJavaScript()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain asset', 'JavaScript');
+                expect(assetGraph.findAssets({type: 'JavaScript'})[0], 'to have the same AST as', function () {
+                    /*jshint ignore:start*/
+                    if (foo) {
+                        doNotTouchMe();
+                    }
+
+                    if (foo) {
+                        hoistMe();
+                    }
+
+                    if (foo) {
+                        hoistMe();
+                    }
+
+                    if (foo) {}
+
+                    keepMe();
+
+                    keepMe();
+
+                    keepMe();
+
+                    if ("foo" === "bar") {
+                        keepMe();
+                    } else {
+                        keepMe();
+                    }
+                    /*jshint ignore:end*/
+                });
+            })
+            .run(done);
+    });
+});
---
+++ testdata/transforms/removeDeadIfsInJavaScript/index.js
@@ -0,0 +1,51 @@
+if (foo) {
+    doNotTouchMe();
+}
+
+if (foo) {
+    if (true) {
+        hoistMe();
+    }
+}
+
+if (true) {
+    if (foo) {
+        hoistMe();
+    }
+}
+
+if (foo) {
+    if ("foo" === "bar") {
+        removeMe();
+    }
+}
+
+if (true) {
+    keepMe();
+}
+
+if ("foo" === "bar") {
+    removeMe();
+}
+
+if ("afoo" === "afoo") {
+    keepMe();
+}
+
+if ("bfoo" === "bfoo") {
+    if (true) {
+        keepMe();
+    }
+}
+
+if ("foo" === "bar") {
+    if (true) {
+        removeMe();
+    }
+}
+
+if ("foo" === "bar") {
+    keepMe();
+} else {
+    keepMe();
+}
