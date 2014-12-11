b5d61c96b403289181fb9f02f80947628dcc13bf
Andreas Lind <andreas@one.com>
2014-12-11 14:17:13 +0100

    Test: Use magicpen-prism to get syntax highlighting.

--- test/unexpected-with-plugins.js
+++ test/unexpected-with-plugins.js
@@ -1,5 +1,9 @@
-module.exports = require('unexpected')
+var expect = require('unexpected')
     .clone()
     .installPlugin(require('unexpected-sinon'))
     .installPlugin(require('unexpected-jsdom'))
     .installPlugin(require('./unexpectedAssetGraph'));
+
+expect.output.installPlugin(require('magicpen-prism'));
+
+module.exports = expect;
