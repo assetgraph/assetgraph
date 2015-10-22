556133c1ca9ba2766cb82bdfed981398e16ed7c5
Andreas Lind <andreas@one.com>
2015-10-22 23:22:54 +0200

    Basic support for loading existing JavaScript source maps.

--- test/relations/JavaScriptSourceMappingUrl.js
+++ test/relations/JavaScriptSourceMappingUrl.js
@@ -21,4 +21,22 @@ describe('relations/JavaScriptSourceUrl', function () {
             })
             .run(done);
     });
+
+    it('should handle another test case with a JavaScript asset that has @sourceMappingURL directive', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/relations/JavaScriptSourceMappingUrl/existingSourceMap2/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relation', 'JavaScriptSourceMappingUrl');
+                expect(assetGraph, 'to contain relation', 'SourceMapSource');
+            })
+            .applySourceMap()
+            .queue(function (assetGraph) {
+                expect(assetGraph.findAssets({type: 'JavaScript'})[0].parseTree.body[0].expression.argument.loc, 'to satisfy', {
+                    start: { line: 15, column: 1 },
+                    source: 'jquery-1.11.3.js'
+                });
+            })
+            .run(done);
+    });
 });
