b6b9a8d917601efc06756a78821f711d5b93d1a5
Andreas Lind <andreas@one.com>
2015-10-24 00:31:51 +0200

    applySourceMap{ => s}

--- test/relations/JavaScriptSourceMappingUrl.js
+++ test/relations/JavaScriptSourceMappingUrl.js
@@ -2,7 +2,7 @@
 var expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib');
 
-describe('relations/JavaScriptSourceUrl', function () {
+describe('relations/JavaScriptSourceMappingUrl', function () {
     it('should handle a test case with a JavaScript asset that has @sourceMappingURL directive', function (done) {
         new AssetGraph({root: __dirname + '/../../testdata/relations/JavaScriptSourceMappingUrl/existingSourceMap/'})
             .loadAssets('index.html')
@@ -30,7 +30,7 @@ describe('relations/JavaScriptSourceUrl', function () {
                 expect(assetGraph, 'to contain relation', 'JavaScriptSourceMappingUrl');
                 expect(assetGraph, 'to contain relation', 'SourceMapSource');
             })
-            .applySourceMap()
+            .applySourceMaps()
             .queue(function (assetGraph) {
                 expect(assetGraph.findAssets({type: 'JavaScript'})[0].parseTree.body[0].expression.argument.loc, 'to satisfy', {
                     start: { line: 15, column: 1 },
