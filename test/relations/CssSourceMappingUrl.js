e66028f859b4401cbeef5c208e45080d78cbc776
Andreas Lind <andreas@one.com>
2015-11-01 02:25:02 +0100

    WIP [ci skip]

--- test/relations/CssSourceMappingUrl.js
+++ test/relations/CssSourceMappingUrl.js
@@ -3,22 +3,40 @@ var expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib');
 
 describe('relations/CssSourceMappingUrl', function () {
-    it('should handle a test case with a Css asset that has @sourceMappingURL directive', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/relations/CssSourceMappingUrl/existingExternalSourceMap/'})
-            .loadAssets('index.html')
+    it('should handle a test case with a Css asset that has @sourceMappingURL directive', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/CssSourceMappingUrl/existingExternalSourceMap/'})
+            .loadAssets('index.html', 'someMore.css')
             .populate()
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain assets', 4);
-                expect(assetGraph, 'to contain asset', 'Css');
+                expect(assetGraph, 'to contain assets', 5);
+                expect(assetGraph, 'to contain assets', 'Css', 2);
                 expect(assetGraph, 'to contain asset', 'Html');
                 expect(assetGraph, 'to contain asset', 'SourceMap');
                 expect(assetGraph, 'to contain relation', 'CssSourceMappingUrl');
                 expect(assetGraph, 'to contain relation', 'SourceMapFile');
                 expect(assetGraph, 'to contain relation', 'SourceMapSource');
-                assetGraph.findAssets({type: 'Css'})[0].url = assetGraph.root + 'foo/somewhereelse.css';
+                assetGraph.findAssets({ fileName: 'foo.css' })[0].url = assetGraph.root + 'foo/somewhereelse.css';
 
-                expect(assetGraph.findAssets({type: 'Css'})[0].text, 'to match', /#\s*sourceMappingURL=..\/foo.map/);
+                expect(assetGraph.findAssets({ fileName: 'somewhereelse.css' })[0].text, 'to match', /#\s*sourceMappingURL=..\/foo.map/);
             })
-            .run(done);
+            .applySourceMaps()
+            .queue(function (assetGraph) {
+                var css = assetGraph.findAssets({ fileName: 'somewhereelse.css' })[0];
+                css.parseTree.append(
+                    assetGraph.findAssets({ fileName: 'someMore.css' })[0].parseTree.nodes
+                );
+                css.markDirty();
+            })
+            .serializeSourceMaps()
+            .queue(function (assetGraph) {
+                expect(assetGraph.findRelations({ type: 'CssSourceMappingUrl', from: { fileName: 'somewhereelse.css' } })[0].to.parseTree, 'to satisfy', {
+                    sources: [
+                        assetGraph.root + 'foo.less',
+                        assetGraph.root + 'someMore.css'
+                    ]
+                });
+            })
+            .updateRelations({ type: 'SourceMapSource' }, { hrefType: 'rootRelative' }, { includeUnresolved: true })
+            .writeAssetsToDisc({ url: /^file:/}, 'outroot');
     });
 });
