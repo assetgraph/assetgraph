e2c863c4747d3cfdba67f4c10ac9c6e8237ecb7f
Peter Müller <munter@fumle.dk>
2015-11-06 23:21:03 +0100

    Test HtmlApplicationMaifest href getter and setter

--- test/relations/HtmlApplicationManifest.js
+++ test/relations/HtmlApplicationManifest.js
@@ -13,4 +13,40 @@ describe('relations/HtmlApplicationManifest', function () {
             })
             .run(done);
     });
+
+    it('should read the link href correctly', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/relations/HtmlApplicationManifest/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                var relation = assetGraph.findRelations({ type: 'HtmlApplicationManifest' })[0];
+
+                expect(relation, 'to satisfy', {
+                    href: 'manifest.json'
+                });
+            })
+            .run(done);
+    });
+
+    it('should write the link href correctly', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/relations/HtmlApplicationManifest/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                var relation = assetGraph.findRelations({ type: 'HtmlApplicationManifest' })[0];
+
+                relation.to.url = 'foo.json';
+
+                expect(relation, 'to satisfy', {
+                    href: 'foo.json'
+                });
+
+                relation.href = 'bar.json';
+
+                expect(relation, 'to satisfy', {
+                    href: 'bar.json'
+                });
+            })
+            .run(done);
+    });
 });
