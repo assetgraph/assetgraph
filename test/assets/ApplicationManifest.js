c991adf772516440a3c9b718e2d03e0516b42522
Peter Müller <munter@fumle.dk>
2015-11-07 19:45:55 +0100

    assets/ApplicationManifest: Implement outgoing relation detection of related_applications. Closes #293

--- test/assets/ApplicationManifest.js
+++ test/assets/ApplicationManifest.js
@@ -6,9 +6,17 @@ describe('assets/ApplicationManifest', function () {
     it('should detect .webmanifest extensions', function (done) {
         new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('basic.webmanifest')
-            .populate()
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain assets', 'ApplicationManifest', 1);
+            })
+            .run(done);
+    });
+
+    it('should detect related_applications urls', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+            .loadAssets('related_applications.webmanifest')
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations including unresolved', 'Relation', 2);
             })
             .run(done);
     });
