152b84dd4d60224417b0fb9af71d1f900b5ce7f0
Peter Müller <munter@fumle.dk>
2015-11-07 23:45:37 +0100

    Added failing tests for #290 #291 #292

--- test/assets/ApplicationManifest.js
+++ test/assets/ApplicationManifest.js
@@ -20,4 +20,31 @@ describe('assets/ApplicationManifest', function () {
             })
             .run(done);
     });
+
+    it('should detect splash_screens urls', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+            .loadAssets('splash_screens.webmanifest')
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations including unresolved', 'Relation', 2);
+            })
+            .run(done);
+    });
+
+    it('should detect icons urls', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+            .loadAssets('icons.webmanifest')
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations including unresolved', 'Relation', 2);
+            })
+            .run(done);
+    });
+
+    it('should detect start_url urls', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+            .loadAssets('start_url.webmanifest')
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations including unresolved', 'Relation', 1);
+            })
+            .run(done);
+    });
 });
