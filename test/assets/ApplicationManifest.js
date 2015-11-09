b16f230504351072b27074054129cf675dd99a10
Peter Müller <munter@fumle.dk>
2015-11-10 00:59:05 +0100

    First draft of relation/JsonUrl

--- test/assets/ApplicationManifest.js
+++ test/assets/ApplicationManifest.js
@@ -16,7 +16,7 @@ describe('assets/ApplicationManifest', function () {
         new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('related_applications.webmanifest')
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain relations including unresolved', 'Relation', 2);
+                expect(assetGraph, 'to contain relations including unresolved', 'JsonUrl', 2);
             })
             .run(done);
     });
@@ -25,11 +25,11 @@ describe('assets/ApplicationManifest', function () {
         new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('splash_screens.webmanifest')
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain relations including unresolved', 'Relation', 2);
+                expect(assetGraph, 'to contain relations including unresolved', 'JsonUrl', 2);
             })
             .populate()
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain relations', 'Relation', 2);
+                expect(assetGraph, 'to contain relations', 'JsonUrl', 2);
                 expect(assetGraph, 'to contain assets', 'Png', 1);
                 expect(assetGraph, 'to contain assets', 'Jpeg', 1);
             })
@@ -40,11 +40,11 @@ describe('assets/ApplicationManifest', function () {
         new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('icons.webmanifest')
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain relations including unresolved', 'Relation', 2);
+                expect(assetGraph, 'to contain relations including unresolved', 'JsonUrl', 2);
             })
             .populate()
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain relations', 'Relation', 2);
+                expect(assetGraph, 'to contain relations', 'JsonUrl', 2);
                 expect(assetGraph, 'to contain assets', 'Png', 1);
                 expect(assetGraph, 'to contain assets', 'Jpeg', 1);
             })
@@ -55,12 +55,12 @@ describe('assets/ApplicationManifest', function () {
         new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('start_url.webmanifest')
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain relations including unresolved', 'Relation', 1);
+                expect(assetGraph, 'to contain relations including unresolved', 'JsonUrl', 1);
                 expect(assetGraph, 'to contain assets', 'Html', 0);
             })
             .populate()
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain relations', 'Relation', 1);
+                expect(assetGraph, 'to contain relations', 'JsonUrl', 1);
                 expect(assetGraph, 'to contain assets', 'Html', 1);
             })
             .run(done);
