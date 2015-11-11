e38a1b31aa2a8f788600aa3b4ab86b226fa6fc14
Andreas Lind <andreas@one.com>
2015-11-11 23:44:15 +0100

    compileCoffeeScriptToJavaScript: Add support for source maps.

--- test/transforms/compileCoffeeScriptToJavaScript.js
+++ test/transforms/compileCoffeeScriptToJavaScript.js
@@ -1,11 +1,12 @@
 /*global describe, it*/
 var expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib'),
+    mozilla = require('source-map'),
     query = AssetGraph.query;
 
 describe('transforms/compileCoffeeScriptToJavaScript', function () {
-    it('should compile all CoffeeScript assets in the graph', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/transforms/compileCoffeeScriptToJavaScript/'})
+    it('should compile all CoffeeScript assets in the graph', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/transforms/compileCoffeeScriptToJavaScript/'})
             .loadAssets('index.html')
             .populate({followRelations: {to: {url: query.not(/^http:/)}}})
             .queue(function (assetGraph) {
@@ -22,6 +23,32 @@ describe('transforms/compileCoffeeScriptToJavaScript', function () {
                 expect(htmlText, 'not to contain', 'index.coffee');
                 expect(htmlText, 'to contain', '<script src="index.js">');
             })
-            .run(done);
+            .serializeSourceMaps()
+            .queue(function (assetGraph) {
+                var sourceMaps = assetGraph.findAssets({type: 'SourceMap'});
+                expect(sourceMaps[1].parseTree.sources, 'to satisfy', [
+                    assetGraph.root + 'index.coffee'
+                ]);
+                var consumer = new mozilla.SourceMapConsumer(sourceMaps[1].parseTree);
+                expect(consumer.generatedPositionFor({
+                    source: assetGraph.root + 'index.coffee',
+                    line: 1,
+                    column: 0
+                }), 'to equal', {
+                    line: 1,
+                    column: 0,
+                    lastColumn: null
+                });
+
+                expect(consumer.generatedPositionFor({
+                    source: assetGraph.root + 'index.coffee',
+                    line: 3,
+                    column: 0
+                }), 'to equal', {
+                    line: 2,
+                    column: 8,
+                    lastColumn: null
+                });
+            });
     });
 });
