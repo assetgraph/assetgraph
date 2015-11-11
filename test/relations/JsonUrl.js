30249f2f2c63af40648971ea2c226ae0e9d7ee67
Peter Müller <munter@fumle.dk>
2015-11-11 02:17:31 +0100

    Added tests or relations/JsonUrl

---
+++ test/relations/JsonUrl.js
@@ -0,0 +1,41 @@
+/*global describe, it*/
+var expect = require('../unexpected-with-plugins'),
+    AssetGraph = require('../../lib');
+
+describe('relations/JsonUrl', function () {
+    it('should get the href correctly', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/relations/JsonUrl/'})
+            .loadAssets('app.webmanifest')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations', 'HtmlApplicationManifest', 1);
+                expect(assetGraph, 'to contain relations', 'JsonUrl', 1);
+
+                expect(assetGraph.findRelations({ type: 'JsonUrl' }), 'to satisfy', [
+                    {
+                        href: 'index.html',
+                        hrefType: 'relative'
+                    }
+                ]);
+            })
+            .run(done);
+    });
+
+    it('should set the href correctly', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/relations/JsonUrl/'})
+            .loadAssets('app.webmanifest')
+            .populate()
+            .queue(function (assetGraph) {
+                assetGraph.findAssets({ type: 'Html'})[0].fileName = 'foo.html';
+
+                expect(assetGraph.findRelations({ type: 'JsonUrl' }), 'to satisfy', [
+                    {
+                        href: 'foo.html',
+                        hrefType: 'relative'
+                    }
+                ]);
+            })
+            .drawGraph('debug.svg')
+            .run(done);
+    });
+});
