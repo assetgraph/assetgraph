0c8dbb01a216aadd9990ae5f7de95c865c03ac0c
Andreas Lind <andreas@one.com>
2015-11-12 00:05:58 +0100

    compileJsxToJs: Add source map support, update react-tools.

--- test/transforms/compileJsxToJs.js
+++ test/transforms/compileJsxToJs.js
@@ -1,11 +1,12 @@
 /* global describe, it */
 var expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib'),
+    mozilla = require('source-map'),
     query = AssetGraph.query;
 
 describe('transforms/compileJsxToJs', function () {
-    it('should compile Jsx asset to Js', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/transforms/compileJsxToJs/singleJsxFile/'})
+    it('should compile Jsx asset to Js', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/transforms/compileJsxToJs/singleJsxFile/'})
             .loadAssets('index.html')
             .populate({followRelations: {to: {url: query.not(/^http:/)}}})
             .queue(function (assetGraph) {
@@ -23,16 +24,15 @@ describe('transforms/compileJsxToJs', function () {
                 expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to equal', [
                     '/** @jsx React.DOM */',
                     'React.renderComponent(',
-                    '  React.DOM.h1(null, "Hello, world!"),',
+                    '  React.createElement("h1", null, "Hello, world!"),',
                     '  document.getElementById(\'example\')',
                     ');',
                     ''
                 ].join('\n'));
-            })
-            .run(done);
+            });
     });
-    it('should compile all Jsx assets to Js', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/transforms/compileJsxToJs/jsxWithInclude/', followRelations: {}})
+    it('should compile all Jsx assets to Js', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/transforms/compileJsxToJs/jsxWithInclude/', followRelations: {}})
             .loadAssets('index.jsx')
             .populate()
             .queue(function (assetGraph) {
@@ -43,9 +43,37 @@ describe('transforms/compileJsxToJs', function () {
                 expect(assetGraph, 'to contain no assets', 'Jsx');
                 expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to equal', [
                     '/** @jsx React.DOM */',
-                    'React.renderComponent(React.DOM.h1(null,\'Hello, world!\'),document.getElementById(\'example\'));INCLUDE(\'include.js\');'
+                    'React.renderComponent(React.createElement(\'h1\',null,\'Hello, world!\'),document.getElementById(\'example\'));INCLUDE(\'include.js\');'
                 ].join('\n'));
             })
-            .run(done);
+            .serializeSourceMaps()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 'SourceMap', 2);
+                var sourceMap = assetGraph.findAssets({type: 'SourceMap'})[0];
+                expect(sourceMap.parseTree.sources, 'to satisfy', [
+                    assetGraph.root + 'index.jsx'
+                ]);
+                var consumer = new mozilla.SourceMapConsumer(sourceMap.parseTree);
+
+                expect(consumer.generatedPositionFor({
+                    source: assetGraph.root + 'index.jsx',
+                    line: 2,
+                    column: 0
+                }), 'to equal', {
+                    line: 1,
+                    column: 0,
+                    lastColumn: null
+                });
+
+                expect(consumer.generatedPositionFor({
+                    source: assetGraph.root + 'index.jsx',
+                    line: 3,
+                    column: 0
+                }), 'to equal', {
+                    line: 2,
+                    column: 6,
+                    lastColumn: null
+                });
+            });
     });
 });
