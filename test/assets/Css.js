assetgraph/assetgraph

commit 5674fb1ddc274070f6975e018fe01e5ed5c3d1d7

Css.minify(): Prevent mangling of the parse tree.

lib/assets/Css.js
@@ -26,6 +26,23 @@ function extractEncodingFromText(text) {
     return matchCharset && matchCharset[2]; // Will be undefined in case of no match
 }
 
+// Waiting for https://github.com/postcss/postcss/issues/364
+function cloneWithRaws(node) {
+    if (node.nodes) {
+        var oldNodes = node.nodes;
+        node.nodes = [];
+        var clone = node.clone({ raws: node.raws });
+        node.nodes = oldNodes;
+        clone.nodes = oldNodes.map(cloneWithRaws);
+        clone.nodes.forEach(function (childClone) {
+            childClone.parent = node;
+        });
+        return clone;
+    } else {
+        return node.clone({ raws: node.raws });
+    }
+}
+
 extendWithGettersAndSetters(Css.prototype, {
     contentType: 'text/css',
 
@@ -77,7 +94,7 @@ extendWithGettersAndSetters(Css.prototype, {
                         return !(/@preserve|@license|[@#]\s*sourceURL|[#@]\s*sourceMappingURL|^!/).test(comment);
                     }
                 }
-            })).process(this.parseTree, { map: sourceMapsEnabled && { inline: false, annotation: false, sourcesContent: true } });
+            })).process(cloneWithRaws(this.parseTree), { map: sourceMapsEnabled && { inline: false, annotation: false, sourcesContent: true } });
         } else {
             result = this.parseTree.toResult({ map: sourceMapsEnabled && { inline: false, annotation: false, sourcesContent: true } });
         }

commit 23dee0c58fec8eb81b21f7834e7623cb6fc9d5dd

Fix regression introduced in 5674fb1 ("raws" not passed to cssnano).

lib/assets/Css.js
@@ -26,6 +26,22 @@ function extractEncodingFromText(text) {
     return matchCharset && matchCharset[2]; // Will be undefined in case of no match
 }
 
+// Waiting for https://github.com/postcss/postcss/issues/364
+function cloneWithRaws(original) {
+    var clone = original.clone();
+    (function reattachRaws(clone, original) {
+        if (original.raws) {
+            clone.raws = _.extend({}, original.raws);
+        }
+        if (clone.nodes && original.nodes) {
+            for (var i = 0 ; i < original.nodes.length && i < clone.nodes.length ; i += 1) {
+                reattachRaws(clone.nodes[i], original.nodes[i]);
+            }
+        }
+    }(clone, original));
+    return clone;
+}
+
 extendWithGettersAndSetters(Css.prototype, {
     contentType: 'text/css',
 
@@ -77,7 +93,7 @@ extendWithGettersAndSetters(Css.prototype, {
                         return !(/@preserve|@license|[@#]\s*sourceURL|[#@]\s*sourceMappingURL|^!/).test(comment);
                     }
                 }
-            })).process(this.parseTree.clone(), { map: sourceMapsEnabled && { inline: false, annotation: false, sourcesContent: true } });
+            })).process(cloneWithRaws(this.parseTree), { map: sourceMapsEnabled && { inline: false, annotation: false, sourcesContent: true } });
         } else {
             result = this.parseTree.toResult({ map: sourceMapsEnabled && { inline: false, annotation: false, sourcesContent: true } });
         }

test/assets/Css.js
@@ -68,38 +68,66 @@ describe('assets/Css', function () {
         expect(asset.encoding, 'to be', 'iso-8859-1');
     });
 
-    it('should minify Css text', function () {
-        var cssText = 'body {\n    background: red;\n}\n';
-        var asset = new AssetGraph.Css({
-            text: cssText
+    describe('#minify', function () {
+        it('should minify the Css text', function () {
+            var cssText = 'body {\n    background: red;\n}\n';
+            var asset = new AssetGraph.Css({
+                text: cssText
+            });
+
+            expect(asset.text, 'to be', cssText);
+
+            asset.minify();
+            expect(asset.text, 'to be', 'body{background:red}');
         });
 
-        expect(asset.text, 'to be', cssText);
+        it('should propagate source map source map information', function () {
+            return new AssetGraph({root: __dirname + '/../../testdata/assets/Css/minifyWithSourceMap/'})
+                .loadAssets('index.css')
+                .minifyAssets()
+                .serializeSourceMaps()
+                .queue(function (assetGraph) {
+                    expect(assetGraph, 'to contain asset', 'SourceMap');
 
-        asset.minify();
-        expect(asset.text, 'to be', 'body{background:red}');
-    });
-
-    it('should propagate source map source map information when minifying', function () {
-        return new AssetGraph({root: __dirname + '/../../testdata/assets/Css/minifyWithSourceMap/'})
-            .loadAssets('index.css')
-            .minifyAssets()
-            .serializeSourceMaps()
-            .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain asset', 'SourceMap');
-
-                var sourceMap = assetGraph.findAssets({type: 'SourceMap'})[0];
-                var consumer = new mozilla.SourceMapConsumer(sourceMap.parseTree);
-                expect(consumer.generatedPositionFor({
-                    source: assetGraph.root + 'index.css',
-                    line: 2,
-                    column: 4
-                }), 'to equal', {
-                    line: 1,
-                    column: 5,
-                    lastColumn: null
+                    var sourceMap = assetGraph.findAssets({type: 'SourceMap'})[0];
+                    var consumer = new mozilla.SourceMapConsumer(sourceMap.parseTree);
+                    expect(consumer.generatedPositionFor({
+                        source: assetGraph.root + 'index.css',
+                        line: 2,
+                        column: 4
+                    }), 'to equal', {
+                        line: 1,
+                        column: 5,
+                        lastColumn: null
+                    });
                 });
+        });
+
+        it('should preserve CSS hacks that depend on raws being present', function () {
+            var cssText = '.foo {\n  *padding-left: 180px;\n}';
+            var asset = new AssetGraph.Css({
+                text: cssText
             });
+
+            expect(asset.text, 'to be', cssText);
+
+            asset.minify();
+            expect(asset.text, 'to be', '.foo{*padding-left:180px}');
+        });
+
+        it('should not destroy existing relations', function () {
+            var asset = new AssetGraph.Css({
+                text: '.foo {\n  background-image: url(foo.png);\n}.foo {\n  background-image: url(foo.png);\n}'
+            });
+            var relations = asset.outgoingRelations;
+            asset.minify();
+            asset.text;
+            asset.prettyPrint();
+            relations[0].href = 'blah.png';
+            relations[1].href = 'quux.png';
+            asset.markDirty();
+            expect(asset.text, 'to contain', 'blah.png').and('to contain', 'quux.png');
+        });
     });
 
     it('should pretty print Css text', function () {