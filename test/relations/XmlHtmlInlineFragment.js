57f45693c137b52d73aa112883a1db491ce81b4f
Andreas Lind <andreas@one.com>
2016-07-01 22:18:06 +0200

    Stop using http://www.someexamplerssdomain.com/ in tests. https://github.com/assetgraph/assetgraph/pull/560
    (cherry picked from commit f8b8ad1856f589bce703da312f2b9700ab62eb0f)

--- test/relations/XmlHtmlInlineFragment.js
+++ test/relations/XmlHtmlInlineFragment.js
@@ -2,14 +2,14 @@
 var expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib');
 
-describe('relations/HtmlInlineFragment', function () {
+describe('relations/XmlHtmlInlineFragment', function () {
     it('should handle a test case with an RSS feed with a <description> tag', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/relations/HtmlInlineFragment/'})
+        new AssetGraph({root: __dirname + '/../../testdata/relations/XmlHtmlInlineFragment/'})
             .loadAssets('index.html')
             .populate()
             .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain assets', 5);
-                expect(assetGraph, 'to contain assets', 'Html', 3);
+                expect(assetGraph, 'to contain assets', 4);
+                expect(assetGraph, 'to contain assets', 'Html', 2);
                 expect(assetGraph, 'to contain assets', 'Rss', 1);
                 expect(assetGraph, 'to contain assets', 'Png', 1);
                 expect(assetGraph, 'to contain relation', 'XmlHtmlInlineFragment');
@@ -22,7 +22,7 @@ describe('relations/HtmlInlineFragment', function () {
                 assetGraph.findAssets({ type: 'Png' })[0].fileName = 'bar.png';
 
                 expect(fragmentRelation.to.text, 'to equal', 'Here is some text containing an interesting description and an image: <img src="bar.png">.');
-                expect(rss.text, 'to equal', '<?xml version="1.0" encoding="UTF-8"?><rss version="2.0">\n<channel>\n <title>RSS Title</title>\n <description>This is an example of an RSS feed</description>\n <link>http://www.someexamplerssdomain.com/main.html</link>\n <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000 </lastBuildDate>\n <pubDate>Mon, 06 Sep 2009 16:20:00 +0000 </pubDate>\n <ttl>1800</ttl>\n <item>\n  <title>Example entry</title>\n  <description>Here is some text containing an interesting description and an image: &lt;img src="bar.png">.</description>\n  <link>http://www.wikipedia.org/</link>\n  <guid>unique string per item</guid>\n  <pubDate>Mon, 06 Sep 2009 16:20:00 +0000 </pubDate>\n </item>\n</channel>\n</rss>');
+                expect(rss.text, 'to equal', '<?xml version="1.0" encoding="UTF-8"?><rss version="2.0">\n<channel>\n <title>RSS Title</title>\n <description>This is an example of an RSS feed</description>\n <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000 </lastBuildDate>\n <pubDate>Mon, 06 Sep 2009 16:20:00 +0000 </pubDate>\n <ttl>1800</ttl>\n <item>\n  <title>Example entry</title>\n  <description>Here is some text containing an interesting description and an image: &lt;img src="bar.png">.</description>\n  <link>http://www.wikipedia.org/</link>\n  <guid>unique string per item</guid>\n  <pubDate>Mon, 06 Sep 2009 16:20:00 +0000 </pubDate>\n </item>\n</channel>\n</rss>');
             })
             .run(done);
     });
