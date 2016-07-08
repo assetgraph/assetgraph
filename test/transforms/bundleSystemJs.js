22dfe1f2017200da2b1bd74f300ce08a52f1e613
Andreas Lind <andreas@one.com>
2016-07-09 01:47:25 +0200

    Fix lint.

--- test/transforms/bundleSystemJs.js
+++ test/transforms/bundleSystemJs.js
@@ -1,6 +1,5 @@
 /*global describe, it*/
-var _ = require('lodash'),
-    expect = require('../unexpected-with-plugins'),
+var expect = require('../unexpected-with-plugins'),
     estraverse = require('estraverse'),
     AssetGraph = require('../../lib');
 
