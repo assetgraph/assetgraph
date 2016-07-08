2ce0aa3be54c7c2cf00b59c75060dddd8e3dd0fe
Andreas Lind <andreas@one.com>
2016-07-08 17:57:32 +0200

    Added "wishful thinking" test case.

--- test/transforms/bundleSystemJs.js
+++ test/transforms/bundleSystemJs.js
@@ -1,5 +1,6 @@
 /*global describe, it*/
-var expect = require('../unexpected-with-plugins'),
+var _ = require('lodash'),
+    expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib');
 
 describe('transforms/bundleSystemJs', function () {
@@ -529,6 +530,25 @@ describe('transforms/bundleSystemJs', function () {
                             .and('not to contain', 'Math.random');
                     });
             });
+        });
+
+        it.skip('should create separate bundles per variant', function () {
+            return new AssetGraph({root: __dirname + '/../../testdata/transforms/bundleSystemJs/conditionals/oneBundlePerVariant/'})
+                .loadAssets('index.html')
+                .populate()
+                .bundleSystemJs({
+                    conditions: [ 'lang' ]
+                })
+                .populate()
+                .queue(function (assetGraph) {
+                    expect(assetGraph.findAssets({type: 'Html'})[0].text.match(/<script src="[^"]+">/g), 'to equal', [
+                        '<script src="system.js">',
+                        '<script src="config.js">',
+                        '<script src="common-bundle.js">',
+                        '<script src="common-bundle-lang-da.js" data-cond-lang="da">',
+                        '<script src="common-bundle-lang-en_us.js" data-cond-lang="en_us">'
+                    ]);
+                });
         });
     });
 });
