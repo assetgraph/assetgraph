0d55021c1d0f8e24d3a9cab01ee2c868d695457d
Andreas Lind <andreas@one.com>
2016-07-14 01:51:01 +0200

    Remove unused requires.

--- test/assets/Text.js
+++ test/assets/Text.js
@@ -1,6 +1,5 @@
 /*global describe, it*/
 var expect = require('../unexpected-with-plugins'),
-    passError = require('passerror'),
     Promise = require('bluebird'),
     AssetGraph = require('../../lib');
 
