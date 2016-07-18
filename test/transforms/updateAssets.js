14217839c8001bdf4989a95a4b47eee59e5cbd5c
Andreas Lind <andreas@one.com>
2016-07-18 21:52:48 +0200

    updateAssets, updateRelations: Use _.merge instead of the deep-extend module, add tests.

---
+++ test/transforms/updateAssets.js
@@ -0,0 +1,105 @@
+var expect = require('../unexpected-with-plugins');
+var AssetGraph = require('../../lib/');
+
+describe('updateAssets', function () {
+    describe('without the deep flag', function () {
+        it('updates the properties of the found assets shallowly', function () {
+            return new AssetGraph()
+                .loadAssets({
+                    type: 'Json',
+                    parseTree: {
+                        foo: {
+                            bar: 123,
+                            quux: 456
+                        }
+                    }
+                })
+                .updateAssets({type: 'Json'}, {
+                    parseTree: {
+                        baz: 123,
+                        foo: {
+                            bar: 789
+                        }
+                    }
+                })
+                .queue(function (assetGraph) {
+                    expect(assetGraph.findAssets({type: 'Json'})[0], 'to satisfy', {
+                        parseTree: {
+                            baz: 123,
+                            foo: {
+                                bar: 789
+                            }
+                        }
+                    });
+                });
+        });
+    });
+
+    describe('with the deep flag', function () {
+        it('updates the properties of the found assets deeply', function () {
+            return new AssetGraph()
+                .loadAssets({
+                    type: 'Json',
+                    parseTree: {
+                        foo: {
+                            bar: 123,
+                            quux: 456
+                        }
+                    }
+                })
+                .updateAssets({type: 'Json'}, {
+                    parseTree: {
+                        baz: 123,
+                        foo: {
+                            bar: 789
+                        }
+                    }
+                }, { deep: true })
+                .queue(function (assetGraph) {
+                    expect(assetGraph.findAssets({type: 'Json'})[0], 'to satisfy', {
+                        parseTree: {
+                            baz: 123,
+                            foo: {
+                                bar: 789,
+                                quux: 456
+                            }
+                        }
+                    });
+                });
+        });
+
+        describe('passed as a boolean', function () {
+            it('updates the properties of the found assets deeply', function () {
+                return new AssetGraph()
+                    .loadAssets({
+                        type: 'Json',
+                        parseTree: {
+                            foo: {
+                                bar: 123,
+                                quux: 456
+                            }
+                        }
+                    })
+                    .updateAssets({type: 'Json'}, {
+                        parseTree: {
+                            baz: 123,
+                            foo: {
+                                bar: 789
+                            }
+                        }
+                    }, true)
+                    .queue(function (assetGraph) {
+                        expect(assetGraph.findAssets({type: 'Json'})[0], 'to satisfy', {
+                            parseTree: {
+                                baz: 123,
+                                foo: {
+                                    bar: 789,
+                                    quux: 456
+                                }
+                            }
+                        });
+                    });
+            });
+        });
+    });
+});
