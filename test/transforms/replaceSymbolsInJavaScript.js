906df6d5f8247475e65f1347ee5409aee39318da
Andreas Lind <andreas@one.com>
2016-07-18 22:11:16 +0200

    Get rid of the passerror module now that most of the code is promisified.

--- test/transforms/replaceSymbolsInJavaScript.js
+++ test/transforms/replaceSymbolsInJavaScript.js
@@ -1,6 +1,5 @@
 /*global describe, it, beforeEach*/
 var unexpected = require('../unexpected-with-plugins'),
-    passError = require('passerror'),
     AssetGraph = require('../../lib/');
 
 describe('transforms/replaceSymbolsInJavaScript', function () {
@@ -9,7 +8,7 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
         assetGraph = new AssetGraph();
     });
 
-    var expect = unexpected.clone().addAssertion('to come out as', function (expect, subject, value, done) {
+    var expect = unexpected.clone().addAssertion('to come out as', function (expect, subject, value) {
         this.args.pop(); // Don't inspect the callback when the assertion fails
         // subject.code, subject.defines
         expect(subject, 'to be an object');
@@ -23,19 +22,19 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
         } else if (Buffer.isBuffer(subject.rawSrc)) {
             assetConfig.rawSrc = subject.rawSrc;
         }
-        assetGraph
+        return assetGraph
             .loadAssets(new AssetGraph.JavaScript(assetConfig))
             .replaceSymbolsInJavaScript({type: 'JavaScript'}, subject.defines || {})
             .queue(function (assetGraph) {
                 expect(assetGraph.findAssets({fileName: 'bogus.js'})[0], 'to have the same AST as', value);
             })
-            .run(function (err) {
-                done(err, !err && assetGraph.findAssets()[0].parseTree);
+            .then(function () {
+                return assetGraph.findAssets()[0].parseTree;
             });
     });
 
-    it('should replace a primitive value', function (done) {
-        expect({
+    it('should replace a primitive value', function () {
+        return expect({
             text: 'var bar = FOO;',
             defines: {
                 FOO: '"foo"'
@@ -44,11 +43,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = 'foo';
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should not replace an undefined value', function (done) {
-        expect({
+    it('should not replace an undefined value', function () {
+        return expect({
             text: 'var bar = FOO;',
             defines: {
             }
@@ -56,11 +55,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = FOO;
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should replace an undefined value in an object with undefined', function (done) {
-        expect({
+    it('should replace an undefined value in an object with undefined', function () {
+        return expect({
             text: 'var bar = FOO.BAR;',
             defines: {
                 FOO: {}
@@ -69,11 +68,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = undefined;
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should replace an undefined value in a nested object with undefined', function (done) {
-        expect({
+    it('should replace an undefined value in a nested object with undefined', function () {
+        return expect({
             text: 'var bar = !BAZ.FOO.BAR;',
             defines: {
                 BAZ: {}
@@ -82,20 +81,20 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = !undefined;
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should not replace the LHS of an assignment', function (done) {
-        expect({
+    it('should not replace the LHS of an assignment', function () {
+        return expect({
             text: 'var FOO = "bar";',
             defines: {
                 FOO: { type: 'Literal', value: 'foo' }
             }
-        }, 'to come out as', 'var FOO = "bar";', done);
+        }, 'to come out as', 'var FOO = "bar";');
     });
 
-    it('should replace complex value', function (done) {
-        expect({
+    it('should replace complex value', function () {
+        return expect({
             text: 'var bar = FOO;',
             defines: {
                 FOO: {quux: {baz: 123}}
@@ -104,11 +103,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = {quux: {baz: 123}};
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should replace nested value with dot notation', function (done) {
-        expect({
+    it('should replace nested value with dot notation', function () {
+        return expect({
             text: 'var bar = FOO.quux;',
             defines: {
                 FOO: {quux: 'baz'}
@@ -117,11 +116,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = 'baz';
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should replace nested value with bracket notation', function (done) {
-        expect({
+    it('should replace nested value with bracket notation', function () {
+        return expect({
             text: 'var bar = FOO["quux"];',
             defines: {
                 FOO: {quux: 'baz'}
@@ -130,11 +129,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = 'baz';
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should replace nested value with mixed notation', function (done) {
-        expect({
+    it('should replace nested value with mixed notation', function () {
+        return expect({
             text: 'var bar = FOO["quux"].baz;',
             defines: {
                 FOO: { quux: { baz: 'foo' } }
@@ -143,11 +142,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = 'foo';
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should work with numbers and bracket notation', function (done) {
-        expect({
+    it('should work with numbers and bracket notation', function () {
+        return expect({
             text: 'var bar = FOO[1];',
             defines: {
                 FOO: { 1: 'baz' }
@@ -156,15 +155,15 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = 'baz';
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should replace nested value with undefined if no value is found and emit a warning', function (done) {
+    it('should replace nested value with undefined if no value is found and emit a warning', function () {
         var warnings = [];
         assetGraph.on('warn', function (err) {
             warnings.push(err);
         });
-        expect({
+        return expect({
             text: 'var qux = FOO.bar.baz.quux',
             defines: {
                 FOO: { bar: { baz: {} } }
@@ -173,14 +172,13 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var qux = undefined;
             /* eslint-enable */
-        }, passError(done, function () {
+        }).then(function () {
             expect(warnings, 'to satisfy', [ 'Could not find a value for "FOO.bar.baz.quux". Replacing with undefined.' ]);
-            done();
-        }));
+        });
     });
 
-    it('should proceed as far as possible if contents of brackets is not a constant', function (done) {
-        expect({
+    it('should proceed as far as possible if contents of brackets is not a constant', function () {
+        return expect({
             text: 'var bar = FOO[function () { return "bar"; }];',
             defines: {
                 FOO: { bar: 'baz' }
@@ -189,12 +187,12 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             var bar = { bar: 'baz' }[function () { return "bar"; }];
             /* eslint-enable */
-        }, done);
+        });
     });
 
     // Wishful thinking:
-    it('should support dot notation in the LHS', function (done) {
-        expect({
+    it('should support dot notation in the LHS', function () {
+        return expect({
             text: 'console.log(123);',
             defines: {
                 'console.log': 'foo.bar'
@@ -203,11 +201,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             foo.bar(123);
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should support bracket notation in the LHS', function (done) {
-        expect({
+    it('should support bracket notation in the LHS', function () {
+        return expect({
             text: 'alert(123 + hereIs["the thing"]);',
             defines: {
                 'hereIs["the thing"]': 987
@@ -216,11 +214,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             alert(123 + 987);
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should support a complex expression in the LHS', function (done) {
-        expect({
+    it('should support a complex expression in the LHS', function () {
+        return expect({
             text: '123 + foo(1 + 2);',
             defines: {
                 'foo(1 + 2)': '456'
@@ -229,11 +227,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             123 + 456;
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should not use the same AST node instance when replacing multiple occurrences', function (done) {
-        expect({
+    it('should not use the same AST node instance when replacing multiple occurrences', function () {
+        return expect({
             text: 'FOO + FOO',
             defines: {
                 FOO: 2
@@ -242,16 +240,15 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             2 + 2;
             /* eslint-enable */
-        }, passError(done, function (parseTree) {
+        }).then(function (parseTree) {
             var binOp = parseTree.body[0].expression;
             expect(binOp.type, 'to equal', 'BinaryExpression');
             expect(binOp.left, 'not to be', binOp.right);
-            done();
-        }));
+        });
     });
 
-    it('should support literal object properties in the RHS', function (done) {
-        expect({
+    it('should support literal object properties in the RHS', function () {
+        return expect({
             text: 'alert(123 + theThing.foo + theThing["foo"]);',
             defines: {
                 theThing: '{"foo": "bar"}'
@@ -260,11 +257,11 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             alert(123 + 'bar' + 'bar');
             /* eslint-enable */
-        }, done);
+        });
     });
 
-    it('should only touch occurrences that are not part of a larger sequence of MemberExpressions', function (done) {
-        expect({
+    it('should only touch occurrences that are not part of a larger sequence of MemberExpressions', function () {
+        return expect({
             text: 'window.FOO + window[FOO] + foo.bar.FOO + FOO',
             defines: {
                 FOO: 2
@@ -273,6 +270,6 @@ describe('transforms/replaceSymbolsInJavaScript', function () {
             /* eslint-disable */
             window.FOO + window[FOO] + foo.bar.FOO + 2;
             /* eslint-enable */
-        }, done);
+        });
     });
 });
