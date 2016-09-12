a844ef5968ea90df7cd80054d533a5400f8b1347
Andreas Lind <andreas@one.com>
2016-09-12 09:10:14 +0200

    reviewContentSecurityPolicy: Issue absolute links when updating the CSP instead of whitelisting whole host names.
    Addresses https://blog.0daylabs.com/2016/09/09/bypassing-csp/

--- test/transforms/reviewContentSecurityPolicy.js
+++ test/transforms/reviewContentSecurityPolicy.js
@@ -37,7 +37,7 @@ describe('transforms/reviewContentSecurityPolicy', function () {
             .queue(function (assetGraph) {
                 expect(assetGraph.findAssets({type: 'ContentSecurityPolicy'})[0].parseTree, 'to satisfy', {
                     defaultSrc: ['\'self\'', 'whatever.com/yadda'],
-                    scriptSrc: ['\'self\'', 'scriptland.com', 'whatever.com/yadda']
+                    scriptSrc: ['\'self\'', 'http://scriptland.com/script.js', 'whatever.com/yadda']
                 });
             });
     });
@@ -60,7 +60,7 @@ describe('transforms/reviewContentSecurityPolicy', function () {
             .queue(function (assetGraph) {
                 expect(assetGraph.findAssets({type: 'ContentSecurityPolicy'})[0].parseTree, 'to satisfy', {
                     defaultSrc: ['\'none\''],
-                    scriptSrc: ['scriptland.com']
+                    scriptSrc: ['http://scriptland.com/somewhere/script.js']
                 });
             });
     });
@@ -77,8 +77,8 @@ describe('transforms/reviewContentSecurityPolicy', function () {
                 .reviewContentSecurityPolicy(undefined, {update: true})
                 .queue(function (assetGraph) {
                     expect(assetGraph.findAssets({type: 'ContentSecurityPolicy'})[0].parseTree, 'to satisfy', {
-                        scriptSrc: ['\'self\'', 'scriptland.com'],
-                        styleSrc: ['\'self\'', 'styleland.com']
+                        scriptSrc: ['\'self\'', 'http://scriptland.com/script.js'],
+                        styleSrc: ['\'self\'', 'http://styleland.com/styles.css']
                     });
                 });
         });
@@ -134,7 +134,7 @@ describe('transforms/reviewContentSecurityPolicy', function () {
             .populate()
             .queue(function (assetGraph) {
                 var contentSecurityPolicy = assetGraph.findAssets({type: 'ContentSecurityPolicy'})[0];
-                contentSecurityPolicy.parseTree.defaultSrc = ['\'self\'', 'scriptland.com'];
+                contentSecurityPolicy.parseTree.defaultSrc = ['\'self\'', 'http://scriptland.com/script.js'];
                 contentSecurityPolicy.markDirty();
 
                 assetGraph.findAssets({type: 'Css'})[0].url = 'http://styleland.com/styles.css';
@@ -143,9 +143,9 @@ describe('transforms/reviewContentSecurityPolicy', function () {
             .reviewContentSecurityPolicy(undefined, {update: true})
             .queue(function (assetGraph) {
                 expect(assetGraph.findAssets({type: 'ContentSecurityPolicy'})[0].parseTree, 'to satisfy', {
-                    defaultSrc: ['\'self\'', 'scriptland.com'],
+                    defaultSrc: ['\'self\'', 'http://scriptland.com/script.js'],
                     scriptSrc: undefined,
-                    styleSrc: ['\'self\'', 'styleland.com']
+                    styleSrc: ['\'self\'', 'http://styleland.com/styles.css']
                 });
             });
     });
