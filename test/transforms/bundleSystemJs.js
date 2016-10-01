2c2fc1872d62c016c624145c6fa8a7a9b2cfef52
Andreas Lind <andreas@one.com>
2016-10-02 00:03:44 +0200

    Update tests to expect the data-assetgraph-conditions to reflect that the deduplication has happened.

--- test/transforms/bundleSystemJs.js
+++ test/transforms/bundleSystemJs.js
@@ -773,10 +773,10 @@ describe('transforms/bundleSystemJs', function () {
                             '<script src="system.js">',
                             '<script src="config.js">',
                             '<script src="/common-bundle.js">',
-                            '<script src="/bundle-main-da-rainy.js" data-assetgraph-conditions="\'lang.js|default\': \'da\', \'weather.js|default\': \'rainy\'">',
-                            '<script src="/bundle-main-da-sunny.js" data-assetgraph-conditions="\'lang.js|default\': \'da\', \'weather.js|default\': \'sunny\'">',
-                            '<script src="/bundle-main-en_us-rainy.js" data-assetgraph-conditions="\'lang.js|default\': \'en_us\', \'weather.js|default\': \'rainy\'">',
-                            '<script src="/bundle-main-en_us-sunny.js" data-assetgraph-conditions="\'lang.js|default\': \'en_us\', \'weather.js|default\': \'sunny\'">'
+                            '<script src="/bundle-main-rainy.js" data-assetgraph-conditions="\'weather.js|default\': \'rainy\'">',
+                            '<script src="/bundle-main-da.js" data-assetgraph-conditions="\'lang.js|default\': \'da\'">',
+                            '<script src="/bundle-main-sunny.js" data-assetgraph-conditions="\'weather.js|default\': \'sunny\'">',
+                            '<script src="/bundle-main-en_us.js" data-assetgraph-conditions="\'lang.js|default\': \'en_us\'">',
                         ]);
                         var commonBundle = assetGraph.findAssets({fileName: 'common-bundle.js'})[0];
                         expect(commonBundle.text, 'to contain', 'neededInAllLanguages')
@@ -818,7 +818,9 @@ describe('transforms/bundleSystemJs', function () {
                             '<script src="system.js">',
                             '<script src="config.js">',
                             '<script src="/common-bundle.js">',
+                            '<script src="/bundle-main-da.js" data-assetgraph-conditions="\'lang.js|default\': \'da\'">',
                             '<script src="/bundle-main-rainy-da.js" data-assetgraph-conditions="\'weather.js|default\': \'rainy\', \'lang.js|default\': \'da\'">',
+                            '<script src="/bundle-main-en_us.js" data-assetgraph-conditions="\'lang.js|default\': \'en_us\'">',
                             '<script src="/bundle-main-rainy-en_us.js" data-assetgraph-conditions="\'weather.js|default\': \'rainy\', \'lang.js|default\': \'en_us\'">',
                             '<script src="/bundle-main-sunny-da.js" data-assetgraph-conditions="\'weather.js|default\': \'sunny\', \'lang.js|default\': \'da\'">',
                             '<script src="/bundle-main-sunny-en_us.js" data-assetgraph-conditions="\'weather.js|default\': \'sunny\', \'lang.js|default\': \'en_us\'">'
