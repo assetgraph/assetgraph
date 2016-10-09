569ff4fed70fab10b44b7a8c82659c0f7c1ab481
Peter Müller <munter@fumle.dk>
2016-10-09 22:17:49 +0200

    transform/importExternalAssets: Track which assets have been added during the rtransform

--- test/transforms/importExternalAssets.js
+++ test/transforms/importExternalAssets.js
@@ -104,4 +104,105 @@ describe('transforms/importExternalAssets', function () {
             }
         ], 'not to error');
     });
+
+    it('should honor options.importPath', function () {
+        return expect(function () {
+            return new AssetGraph({root: __dirname + '/../../testdata/transforms/importExternalAssets/'})
+                .loadAssets('basic.html')
+                .populate({
+                    followRelations: {
+                        crossorigin: false
+                    }
+                })
+                .queue(function setup(assetGraph) {
+                    expect(assetGraph.findRelations({}, true), 'to satisfy', [
+                        {
+                            type: 'HtmlStyle',
+                            href: 'http://fisk.dk/basic.css',
+                            to: {
+                                text: undefined
+                            }
+                        }
+                    ]);
+                })
+                .importExternalAssets({
+                    importPath: 'iAmUserDefined'
+                })
+                .queue(function verification(assetGraph) {
+                    expect(assetGraph.findRelations({}, true), 'to satisfy', [
+                        {
+                            type: 'HtmlStyle',
+                            hrefType: 'rootRelative',
+                            href: /iAmUserDefined\/basic-\d+.css$/,
+                            to: {
+                                text: 'h1 { background: red; }',
+                                url: /iAmUserDefined\/basic-\d+.css$/
+                            }
+                        }
+                    ]);
+                });
+        }, 'with http mocked out', [
+            {
+                request: 'GET http://fisk.dk/basic.css',
+                response: {
+                    body: 'h1 { background: red; }'
+                }
+            }
+        ], 'not to error');
+    });
+
+    it('should keep assets not targeted by the populationQuery external', function () {
+        return expect(function () {
+            return new AssetGraph({root: __dirname + '/../../testdata/transforms/importExternalAssets/'})
+                .loadAssets('onlyCss.html')
+                .populate({
+                    followRelations: {
+                        crossorigin: false
+                    }
+                })
+                .queue(function setup(assetGraph) {
+                    expect(assetGraph.findRelations({}, true), 'to satisfy', [
+                        {
+                            type: 'HtmlStyle',
+                            href: 'http://fisk.dk/withBackgroundImages.css',
+                            to: {
+                                text: undefined
+                            }
+                        }
+                    ]);
+                })
+                .importExternalAssets({
+                    populationQuery: {
+                        followRelations: { type: 'HtmlStyle' }
+                    }
+                })
+                .queue(function verification(assetGraph) {
+                    expect(assetGraph.findRelations({}, true), 'to satisfy', [
+                        {
+                            type: 'HtmlStyle',
+                            hrefType: 'rootRelative',
+                            href: /external-imports\/withBackgroundImages-\d+.css$/,
+                            to: {
+                                text: 'h1 { background-image: url(\'http://fisk.dk/foo.jpg\'); }',
+                                url: /external-imports\/withBackgroundImages-\d+.css$/
+                            }
+                        },
+                        {
+                            type: 'CssImage',
+                            href: 'http://fisk.dk/foo.jpg',
+                            to: {
+                                url: 'http://fisk.dk/foo.jpg'
+                            }
+                        }
+                    ]);
+                });
+        }, 'with http mocked out', [
+            {
+                request: 'GET http://fisk.dk/withBackgroundImages.css',
+                response: {
+                    body: 'h1 { background-image: url(foo.jpg); }'
+                }
+            }
+        ], 'not to error');
+    });
 });
