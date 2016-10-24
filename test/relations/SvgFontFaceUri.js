ffaa95ed8aa56a1d4f3ce821ac8ad8876e55d16f
Andreas Lind <andreas@one.com>
2016-10-25 00:37:23 +0200

    SvgFontFaceUri: Don't require the xlink namespace.

---
+++ test/relations/SvgFontFaceUri.js
@@ -0,0 +1,29 @@
+/*global describe, it*/
+var expect = require('../unexpected-with-plugins'),
+    AssetGraph = require('../../lib');
+
+describe('relations/SvgFontFaceUri', function () {
+    it('should handle a test case with a <font-face-uri xlink:href=...> referencing an external file', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/SvgFontFaceUri/xlinkhref/'})
+            .loadAssets('image.svg')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain asset', 'Svg');
+                expect(assetGraph, 'to contain relations', 'SvgFontFaceUri', 1);
+                assetGraph.findAssets({fileName: 'fontawesome-webfont.ttf'})[0].fileName = 'foo.ttf';
+                expect(assetGraph.findAssets({fileName: 'image.svg'})[0].text, 'to contain', '<font-face-uri xlink:href="foo.ttf"/>');
+            });
+    });
+
+    it('should handle a test case with a <font-face-uri href=...> referencing an external file', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/SvgFontFaceUri/href/'})
+            .loadAssets('image.svg')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain asset', 'Svg');
+                expect(assetGraph, 'to contain relations', 'SvgFontFaceUri', 1);
+                assetGraph.findAssets({fileName: 'fontawesome-webfont.ttf'})[0].fileName = 'foo.ttf';
+                expect(assetGraph.findAssets({fileName: 'image.svg'})[0].text, 'to contain', '<font-face-uri href="foo.ttf"/>');
+            });
+    });
+});
