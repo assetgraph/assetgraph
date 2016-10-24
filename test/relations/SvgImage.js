9e74362047ec7e5866c2e356d4f5b4307a5c3514
Andreas Lind <andreas@one.com>
2016-10-25 00:15:26 +0200

    SvgImage: Don't require the xlink namespace

---
+++ test/relations/SvgImage.js
@@ -0,0 +1,31 @@
+/*global describe, it*/
+var expect = require('../unexpected-with-plugins'),
+    AssetGraph = require('../../lib');
+
+describe('relations/SvgImage', function () {
+    it('should handle a test case with <image xlink:href=...> referencing an external file', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/SvgImage/xlinkhref'})
+            .loadAssets('image.svg')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relation', 'SvgImage');
+                expect(assetGraph, 'to contain asset', 'Svg');
+                expect(assetGraph, 'to contain asset', 'Png');
+                assetGraph.findAssets({fileName: 'foo.png'})[0].fileName = 'bar.png';
+                expect(assetGraph.findAssets({fileName: 'image.svg'})[0].text, 'to contain', '<image xlink:href="bar.png"/>');
+            });
+    });
+
+    it('should handle a test case with <image href=...> referencing an external file', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/SvgImage/href/'})
+            .loadAssets('image.svg')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relation', 'SvgImage');
+                expect(assetGraph, 'to contain asset', 'Svg');
+                expect(assetGraph, 'to contain asset', 'Png');
+                assetGraph.findAssets({fileName: 'foo.png'})[0].fileName = 'bar.png';
+                expect(assetGraph.findAssets({fileName: 'image.svg'})[0].text, 'to contain', '<image href="bar.png"/>');
+            });
+    });
+});
