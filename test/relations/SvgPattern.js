7d9ce604622251257e66267c29b1fdf9d4227b89
Andreas Lind <andreas@one.com>
2016-10-25 00:27:36 +0200

    SvgPattern: Don't require the xlink namespace.

--- test/relations/SvgPattern.js
+++ test/relations/SvgPattern.js
@@ -3,14 +3,27 @@ var expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib');
 
 describe('relations/SvgPattern', function () {
-    it('should handle a test case with a <pattern xlink:href=...> referencing an external file', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/relations/SvgPattern/'})
+    it('should handle a test case with a <pattern xlink:href=...> referencing an external file', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/SvgPattern/xlinkhref/'})
             .loadAssets('pattern.svg')
             .populate()
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain assets', 'Svg', 2);
                 expect(assetGraph, 'to contain relations', 'SvgPattern', 1);
-            })
-            .run(done);
+                assetGraph.findAssets({fileName: 'gaussianBlur.svg'})[0].fileName = 'foo.svg';
+                expect(assetGraph.findAssets({fileName: 'pattern.svg'})[0].text, 'to contain', '<pattern xlink:href="foo.svg" ');
+            });
+    });
+
+    it('should handle a test case with a <pattern href=...> referencing an external file', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/SvgPattern/href/'})
+            .loadAssets('pattern.svg')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain assets', 'Svg', 2);
+                expect(assetGraph, 'to contain relations', 'SvgPattern', 1);
+                assetGraph.findAssets({fileName: 'gaussianBlur.svg'})[0].fileName = 'foo.svg';
+                expect(assetGraph.findAssets({fileName: 'pattern.svg'})[0].text, 'to contain', '<pattern href="foo.svg" ');
+            });
     });
 });
