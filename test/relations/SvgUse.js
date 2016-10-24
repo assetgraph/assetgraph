4a20826561cd8567d8afcf7cba8190f1b95d8ee5
Andreas Lind <andreas@one.com>
2016-10-24 10:48:34 +0200

    SvgUse: Don't require the xlink namespace
    Fixes #647

--- test/relations/SvgUse.js
+++ test/relations/SvgUse.js
@@ -3,14 +3,27 @@ var expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib');
 
 describe('relations/SvgUse', function () {
-    it('should handle a test case with a <use xlink:href=...> referencing an external file', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/relations/SvgUse/'})
+    it('should handle a test case with a <use xlink:href=...> referencing an external file', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/SvgUse/xlinkhref'})
             .loadAssets('user.svg')
             .populate()
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain relations', 'SvgUse', 1);
                 expect(assetGraph, 'to contain assets', 'Svg', 2);
-            })
-            .run(done);
+                assetGraph.findAssets({fileName: 'gaussianBlur.svg'})[0].fileName = 'foo.svg';
+                expect(assetGraph.findAssets({fileName: 'user.svg'})[0].text, 'to contain', '<use xlink:href="foo.svg"/>');
+            });
+    });
+
+    it('should handle a test case with a <use href=...> referencing an external file', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/SvgUse/href/'})
+            .loadAssets('user.svg')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations', 'SvgUse', 1);
+                expect(assetGraph, 'to contain assets', 'Svg', 2);
+                assetGraph.findAssets({fileName: 'gaussianBlur.svg'})[0].fileName = 'foo.svg';
+                expect(assetGraph.findAssets({fileName: 'user.svg'})[0].text, 'to contain', '<use href="foo.svg"/>');
+            });
     });
 });
