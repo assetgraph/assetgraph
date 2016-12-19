70a0875f566601ec932bda97af831f97fe4ce91e
Peter Müller <munter@fumle.dk>
2016-12-19 21:33:51 +0100

    Correctly assign postcss parstree as cloned css rule parent before constructing CSS asset

--- test/transforms/inlineCriticalCss.js
+++ test/transforms/inlineCriticalCss.js
@@ -26,4 +26,34 @@ describe('tranforms/inlineCriticalCss', function () {
                 ]);
             });
     });
+
+    it('should combine with other CSS transforms without throwing', function () {
+        return new AssetGraph({ root: __dirname + '/../../testdata/transforms/inlineCriticalCss/' })
+            .loadAssets('simple.html')
+            .populate()
+            .minifyAssets({isLoaded: true, isInline: false})
+            .inlineHtmlTemplates()
+            .bundleRelations({type: 'HtmlStyle', to: {type: 'Css', isLoaded: true}, node: function (node) {return !node.hasAttribute('nobundle');}})
+            .inlineCriticalCss()
+            .mergeIdenticalAssets({isLoaded: true, isInline: false, type: ['JavaScript', 'Css']}) // The bundling might produce several identical files, especially the 'oneBundlePerIncludingAsset' strategy.
+            .minifyAssets({isLoaded: true})
+            .queue(function (assetGraph) {
+                expect(assetGraph.findRelations({ type: 'HtmlStyle' }), 'to satisfy', [
+                    {
+                        to: {
+                            isInline: true,
+                            text: 'h1{color:red}'
+                        },
+                        node: function (node) {
+                            return node && node.parentNode && node.parentNode.tagName === 'HEAD';
+                        }
+                    },
+                    {
+                        to: {
+                            fileName: 'simple.css'
+                        }
+                    }
+                ]);
+            });
+    });
 });
