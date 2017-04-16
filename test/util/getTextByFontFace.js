51531469589f153bd082d6d5881a4f4094654e72
Peter Müller <munter@fumle.dk>
2017-04-17 00:23:11 +0200

    Added test for getTextByFontFace, failing until #729 is resolved

---
+++ test/util/getTextByFontFace.js
@@ -0,0 +1,32 @@
+var expect = require('../unexpected-with-plugins');
+var AssetGraph = require('../../lib');
+var getTextByFontFace = require('../../lib/util/getTextByFontFace');
+
+describe('util/getTextByFontFace', function () {
+    it('should take browser default stylesheet into account', function () {
+        var htmlText = [
+            '<style>@font-face { font-family: "font1"; font-style: normal; font-weight: 400; src: url(https://fonts.gstatic.com/s/notoserif/v4/Q47Ro23nlKqZrOLipd3-SwsYbbCjybiHxArTLjt7FRU.woff2) format("woff2");}</style>',
+            '<style>@font-face { font-family: "font2"; font-style: normal; font-weight: 400; src: url(https://fonts.gstatic.com/s/notoserif/v4/Q47Ro23nlKqZrOLipd3-SwsYbbCjybiHxArTLjt7FRU.woff2) format("woff2");}</style>',
+            '<style>h1 { font-family: font1; }</style>',
+            '<style>span { font-family: font2; }</style>',
+            '<h1>foo <span>bar</span></h1>'
+        ].join('\n');
+
+        return new AssetGraph({})
+            .loadAssets(new AssetGraph.Html({
+                text: htmlText
+            }))
+            .populate()
+            .queue(function (assetGraph) {
+                expect(getTextByFontFace(assetGraph), 'to satisfy', {
+                    'font1:400': {
+                        textContent: 'foo'
+                    },
+
+                    'font2:400': {
+                        textContent: 'bar'
+                    }
+                });
+            });
+    });
+});
