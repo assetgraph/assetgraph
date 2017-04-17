d8f03ff28847e0a32055855b8418c221af6e98fe
Peter Müller <munter@fumle.dk>
2017-04-17 13:41:31 +0200

    Changed getCssRulesByProperty to take a Css string as second argument instead of parsetree. Added memoization

--- test/util/fonts/getCssRulesByProperty.js
+++ test/util/fonts/getCssRulesByProperty.js
@@ -1,28 +1,27 @@
 var expect = require('unexpected');
 var getRules = require('../../../lib/util/fonts/getCssRulesByProperty');
-var postCss = require('postcss');
 
 describe('utils/fonts/getCssRulesByProperty', function () {
     it('should throw when not passing an array of properties as first argument', function () {
         expect(getRules, 'to throw', 'properties argument must be an array');
     });
 
-    it('should throw when not passing a parsetree as second argument', function () {
-        expect(function () { getRules(['padding']); }, 'to throw', 'parseTree argument must be a postcss parse result');
+    it('should throw when not passing a cssSource as second argument', function () {
+        expect(function () { getRules(['padding']); }, 'to throw', 'cssSource argument must be a string containing valid CSS');
+    });
+
+    it('should throw when not passing a valid CSS document in cssSource', function () {
+        expect(function () { getRules(['padding'], 'sdkjlasjdlk'); }, 'to throw');
     });
 
     it('should return empty arrays when no properties apply', function () {
-        var parseTree = postCss.parse('h1 { color: red; }');
-
-        expect(getRules(['padding'], parseTree), 'to exhaustively satisfy', {
+        expect(getRules(['padding'], 'h1 { color: red; }'), 'to exhaustively satisfy', {
             padding: []
         });
     });
 
     it('should return an array of matching property values', function () {
-        var parseTree = postCss.parse('h1 { color: red; } h2 { color: blue; }');
-
-        expect(getRules(['color'], parseTree), 'to exhaustively satisfy', {
+        expect(getRules(['color'], 'h1 { color: red; } h2 { color: blue; }'), 'to exhaustively satisfy', {
             color: [
                 {
                     selector: 'h1',
