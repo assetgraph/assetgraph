557f21440a8ac2e84c212a0d02c925f83bc937c1
Peter Müller <munter@fumle.dk>
2017-04-22 23:53:17 +0200

    Make all util/fonts/getTextByFontProperties tests pass

--- test/util/fonts/getTextByFontProperties.js
+++ test/util/fonts/getTextByFontProperties.js
@@ -1,38 +1,256 @@
-var expect = require('unexpected');
+var expect = require('unexpected').clone();
 var AssetGraph = require('../../../lib');
 var getTextByFontProp = require('../../../lib/util/fonts/getTextByFontProperties');
 
+expect.addAssertion('<string> to [exhaustively] satisfy computed font properties <array>', function (expect, subject, result) {
+    return new AssetGraph({})
+        .loadAssets(new AssetGraph.Html({
+            text: subject
+        }))
+        .populate({ followRelations: { crossorigin: false } })
+        .queue(function (assetGraph) {
+            expect(getTextByFontProp(assetGraph.findAssets({type: 'Html'})[0]), 'to [exhaustively] satisfy', result);
+        });
+});
+
 describe('util/fonts/getTextByFontProp', function () {
+    it('should apply inline style attribute values', function () {
+        var htmlText = [
+            '<div style="font-weight: bold">div</div>'
+        ].join('\n');
+
+        return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+            {
+                text: 'div',
+                props: {
+                    'font-family': undefined,
+                    'font-weight': 'bold',
+                    'font-style': undefined
+                }
+            }
+        ]);
+    });
+
+    it('should apply stylesheet attribute values', function () {
+        var htmlText = [
+            '<style>div { font-weight: bold; }</style>',
+            '<div>div</div>'
+        ].join('\n');
+
+        return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+            {
+                text: 'div',
+                props: {
+                    'font-family': undefined,
+                    'font-weight': 'bold',
+                    'font-style': undefined
+                }
+            }
+        ]);
+    });
+
+    it('should apply default browser styles', function () {
+        var htmlText = [
+            '<div>div</div><strong>strong</strong><em>em</em>'
+        ].join('\n');
+
+        return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+            {
+                text: 'div',
+                props: {
+                    'font-family': undefined,
+                    'font-weight': undefined,
+                    'font-style': undefined
+                }
+            },
+            {
+                text: 'strong',
+                props: {
+                    'font-family': undefined,
+                    'font-weight': 'bold',
+                    'font-style': undefined
+                }
+            },
+            {
+                text: 'em',
+                props: {
+                    'font-family': undefined,
+                    'font-weight': undefined,
+                    'font-style': 'italic'
+                }
+            }
+        ]);
+    });
+
+    describe('specificity', function () {
+        it('stylesheets should override browser defaults', function () {
+            var htmlText = [
+                '<style>h1 { font-weight: normal; }</style>',
+                '<h1>h1</h1>'
+            ].join('\n');
+
+            return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+                {
+                    text: 'h1',
+                    props: {
+                        'font-family': undefined,
+                        'font-weight': 'normal',
+                        'font-style': undefined
+                    }
+                }
+            ]);
+        });
+
+        it('style attributes should override stylesheets', function () {
+            var htmlText = [
+                '<style>div { font-weight: bold; }</style>',
+                '<div style="font-weight: normal">div</div>'
+            ].join('\n');
+
+            return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+                {
+                    text: 'div',
+                    props: {
+                        'font-family': undefined,
+                        'font-weight': 'normal',
+                        'font-style': undefined
+                    }
+                }
+            ]);
+        });
+
+        it('redefined properties in the same rule should override previous ones', function () {
+            var htmlText = [
+                '<style>div { font-weight: bold; font-weight: light }</style>',
+                '<div>div</div>'
+            ].join('\n');
+
+            return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+                {
+                    text: 'div',
+                    props: {
+                        'font-family': undefined,
+                        'font-weight': 'light',
+                        'font-style': undefined
+                    }
+                }
+            ]);
+        });
+
+        it('higher specificity selectors should override lower ones', function () {
+            var htmlText = [
+                '<style>.all {font-weight: light} div { font-weight: bold; }</style>',
+                '<div class="all">div</div>'
+            ].join('\n');
+
+            return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+                {
+                    text: 'div',
+                    props: {
+                        'font-family': undefined,
+                        'font-weight': 'light',
+                        'font-style': undefined
+                    }
+                }
+            ]);
+        });
+
+        it('last selector of equal specificity should override previous ones', function () {
+            var htmlText = [
+                '<style>div {font-weight: light} div { font-weight: bold; }</style>',
+                '<div>div</div>'
+            ].join('\n');
+
+            return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+                {
+                    text: 'div',
+                    props: {
+                        'font-family': undefined,
+                        'font-weight': 'bold',
+                        'font-style': undefined
+                    }
+                }
+            ]);
+        });
+
+        it('!important should override specificity in stylesheets', function () {
+            var htmlText = [
+                '<style>.all {font-weight: light} div { font-weight: bold !important; }</style>',
+                '<div class="all">div</div>'
+            ].join('\n');
+
+            return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+                {
+                    text: 'div',
+                    props: {
+                        'font-family': undefined,
+                        'font-weight': 'bold',
+                        'font-style': undefined
+                    }
+                }
+            ]);
+        });
+
+        it('!important in stylesheet should override style attribute', function () {
+            var htmlText = [
+                '<style>div { font-weight: bold !important; }</style>',
+                '<div style="font-weight: light">div</div>'
+            ].join('\n');
+
+            return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+                {
+                    text: 'div',
+                    props: {
+                        'font-family': undefined,
+                        'font-weight': 'bold',
+                        'font-style': undefined
+                    }
+                }
+            ]);
+        });
+
+        it('!important in style attribute should override !important in stylesheet', function () {
+            var htmlText = [
+                '<style>div { font-weight: bold !important; }</style>',
+                '<div style="font-weight: light !important">div</div>'
+            ].join('\n');
+
+            return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+                {
+                    text: 'div',
+                    props: {
+                        'font-family': undefined,
+                        'font-weight': 'light',
+                        'font-style': undefined
+                    }
+                }
+            ]);
+        });
+    });
+
     it('should take browser default stylesheet into account', function () {
         var htmlText = [
             '<style>h1 { font-family: font1; } span { font-family: font2; }</style>',
             '<h1>foo <span>bar</span></h1>'
         ].join('\n');
 
-        return new AssetGraph({})
-            .loadAssets(new AssetGraph.Html({
-                text: htmlText
-            }))
-            .populate()
-            .queue(function (assetGraph) {
-                expect(getTextByFontProp(assetGraph.findAssets({type: 'Html'})[0]), 'to exhaustively satisfy', [
-                    {
-                        text: 'foo',
-                        props: {
-                            'font-family': 'font1',
-                            'font-weight': 'bold',
-                            'font-style': undefined
-                        }
-                    },
-                    {
-                        text: 'bar',
-                        props: {
-                            'font-family': 'font2',
-                            'font-weight': 'bold',
-                            'font-style': undefined
-                        }
-                    }
-                ]);
-            });
+        return expect(htmlText, 'to exhaustively satisfy computed font properties', [
+            {
+                text: 'foo',
+                props: {
+                    'font-family': 'font1',
+                    'font-weight': 'bold',
+                    'font-style': undefined
+                }
+            },
+            {
+                text: 'bar',
+                props: {
+                    'font-family': 'font2',
+                    'font-weight': 'bold',
+                    'font-style': undefined
+                }
+            }
+        ]);
     });
 });
