7a76db92eb0f1f5eb8e95b52a7b3b0f0854ffac6
Andreas Lind <andreas@one.com>
2017-04-30 21:30:47 +0200

    Fix: Getting the .text of an unloaded CSS asset causes a stack overflow, but it should just return undefined
    Fixes #559
    
    (cherry picked from commit 3068e125ac0708f4a4066faf0647df4ef1941d6f)

--- test/assets/Css.js
+++ test/assets/Css.js
@@ -246,4 +246,8 @@ describe('assets/Css', function () {
         cssAsset.minify();
         expect(cssAsset.text, 'to equal', 'div{border-top-width:5px;border-right-width:5px;border-bottom-width:5px;border-left-width:5px;border-top-style:solid;border-right-style:solid;border-bottom-style:solid;border-left-style:solid;border-top-color:#181bff;border-right-color:#181bff;border-bottom-color:#181bff;border-left-color:#181bff}');
     });
+
+    it('should not break when attempting to retrieve the text content of an unloaded Css asset', function () {
+        expect(new AssetGraph.Css({}).text, 'to be undefined');
+    });
 });
