95921f3e4d84b01ea97d90a02b30fe98492a7aa6
Andreas Lind <andreas@one.com>
2017-05-12 00:19:53 +0200

    compressJavaScript: Report the line and column number when UglifyJS cannot parse the source code
    And just emit it as a warning instead of crashing.

--- test/transforms/compressJavaScript.js
+++ test/transforms/compressJavaScript.js
@@ -1,18 +1,34 @@
 /*global describe, it*/
-var expect = require('../unexpected-with-plugins'),
-    AssetGraph = require('../../lib');
+var expect = require('../unexpected-with-plugins');
+var sinon = require('sinon');
+var AssetGraph = require('../../lib');
+var errors = require('../../lib/errors');
 
 describe('transforms/compressJavaScript', function () {
-    [undefined, 'uglifyJs'/*, 'yuicompressor', 'closurecompiler'*/].forEach(function (compressorName) {
-        it('with compressorName=' + compressorName + ' should yield a compressed JavaScript', function (done) {
-            new AssetGraph()
+    [undefined, 'uglifyJs'].forEach(function (compressorName) {
+        it('with compressorName=' + compressorName + ' should yield a compressed JavaScript', function () {
+            return new AssetGraph()
                 .loadAssets(new AssetGraph.JavaScript({text: 'var foo = 123;'}))
                 .compressJavaScript({type: 'JavaScript'}, compressorName)
                 .queue(function (assetGraph) {
                     expect(assetGraph, 'to contain asset', 'JavaScript');
                     expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to match', /^var foo=123;?\n?$/);
-                })
-                .run(done);
+                });
         });
+    });
+
+    it('should warn when UglifyJS runs into a parse error and leave the asset unchanged', function () {
+        var warnSpy = sinon.spy();
+        return new AssetGraph()
+            .on('warn', warnSpy)
+            .loadAssets(new AssetGraph.JavaScript({text: 'var foo = `123`;'}))
+            .compressJavaScript({type: 'JavaScript'})
+            .queue(function (assetGraph) {
+                expect(warnSpy, 'to have calls satisfying', function () {
+                    warnSpy(new errors.ParseError('Parse error in inline JavaScript\nUnexpected character \'`\' (line 1, column 11)'));
+                });
+                expect(assetGraph, 'to contain asset', 'JavaScript');
+                expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to equal', 'var foo = `123`;');
+            });
     });
 });
