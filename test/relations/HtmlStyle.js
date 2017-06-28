a5c157f14977d80d2b271ec593017708a1788849
Andreas Lind <andreas@one.com>
2017-06-29 00:30:11 +0200

    HtmlStyle: Implement media getter/setter

--- test/relations/HtmlStyle.js
+++ test/relations/HtmlStyle.js
@@ -24,6 +24,29 @@ describe('relations/HtmlStyle', function () {
         assetGraph.addAsset(htmlAsset);
     }
 
+    describe('#media', function () {
+        describe('invoked as a getter', function () {
+            it('should retrieve the trimmed media attribute', function () {
+                initial('<!DOCTYPE html><html><head><style media=" projection screen ">body{color:#000};</style></head><body></body></html>');
+                expect(assetGraph.findRelations({type: 'HtmlStyle'})[0].media, 'to equal', 'projection screen');
+            });
+        });
+
+        describe('invoked as a setter', function () {
+            it('should trim and update the media attribute', function () {
+                initial('<!DOCTYPE html><html><head><style>body{color:#000};</style></head><body></body></html>');
+                assetGraph.findRelations({type: 'HtmlStyle'})[0].media = ' projection screen ';
+                expect(assetGraph.findAssets({type: 'Html'})[0].text, 'to contain', '<style media="projection screen">');
+            });
+
+            it('should remove media attribute when set to a falsy value', function () {
+                initial('<!DOCTYPE html><html><head><style media="projection">body{color:#000};</style></head><body></body></html>');
+                assetGraph.findRelations({type: 'HtmlStyle'})[0].media = undefined;
+                expect(assetGraph.findAssets({type: 'Html'})[0].text, 'to contain', '<style>');
+            });
+        });
+    });
+
     describe('#attach', function () {
         describe('when there are existing HtmlStyle relations in <head>', function () {
             beforeEach(function () {
