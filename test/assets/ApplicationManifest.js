59d4f3fd83e1e87ac1a09b0d9b51a06ff67727fe
Andreas Lind <andreas@one.com>
2017-07-13 14:34:33 +0200

    Promisify the ApplicationManifest test

--- test/assets/ApplicationManifest.js
+++ test/assets/ApplicationManifest.js
@@ -3,26 +3,24 @@ var expect = require('../unexpected-with-plugins'),
     AssetGraph = require('../../lib/AssetGraph');
 
 describe('assets/ApplicationManifest', function () {
-    it('should detect .webmanifest extensions', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+    it('should detect .webmanifest extensions', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('basic.webmanifest')
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain assets', 'ApplicationManifest', 1);
-            })
-            .run(done);
+            });
     });
 
-    it('should detect related_applications urls', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+    it('should detect related_applications urls', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('related_applications.webmanifest')
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain relations including unresolved', 'JsonUrl', 2);
-            })
-            .run(done);
+            });
     });
 
-    it('should detect splash_screens urls', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+    it('should detect splash_screens urls', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('splash_screens.webmanifest')
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain relations including unresolved', 'JsonUrl', 2);
@@ -32,12 +30,11 @@ describe('assets/ApplicationManifest', function () {
                 expect(assetGraph, 'to contain relations', 'JsonUrl', 2);
                 expect(assetGraph, 'to contain assets', 'Png', 1);
                 expect(assetGraph, 'to contain assets', 'Jpeg', 1);
-            })
-            .run(done);
+            });
     });
 
-    it('should detect icons urls', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+    it('should detect icons urls', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('icons.webmanifest')
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain relations including unresolved', 'JsonUrl', 2);
@@ -47,12 +44,11 @@ describe('assets/ApplicationManifest', function () {
                 expect(assetGraph, 'to contain relations', 'JsonUrl', 2);
                 expect(assetGraph, 'to contain assets', 'Png', 1);
                 expect(assetGraph, 'to contain assets', 'Jpeg', 1);
-            })
-            .run(done);
+            });
     });
 
-    it('should detect start_url urls', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
+    it('should detect start_url urls', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/assets/ApplicationManifest/'})
             .loadAssets('start_url.webmanifest')
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain relations including unresolved', 'JsonUrl', 1);
@@ -62,7 +58,6 @@ describe('assets/ApplicationManifest', function () {
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain relations', 'JsonUrl', 1);
                 expect(assetGraph, 'to contain assets', 'Html', 1);
-            })
-            .run(done);
+            });
     });
 });
