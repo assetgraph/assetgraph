82728389ca31c194b962c908daf57abd941e4007
Andreas Lind <andreas@one.com>
2017-07-22 00:19:14 +0200

    Promisify/es6ify CssFontFaceSrc test

--- test/relations/CssFontFaceSrc.js
+++ test/relations/CssFontFaceSrc.js
@@ -1,10 +1,10 @@
 /*global describe, it*/
-var expect = require('../unexpected-with-plugins'),
-    AssetGraph = require('../../lib/AssetGraph');
+const expect = require('../unexpected-with-plugins');
+const AssetGraph = require('../../lib/AssetGraph');
 
 describe('relations/CssFontFaceSrc', function () {
-    it('should handle a simple test case', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/relations/CssFontFaceSrc/simple/'})
+    it('should handle a simple test case', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/CssFontFaceSrc/simple/'})
             .loadAssets('index.css')
             .populate()
             .queue(function (assetGraph) {
@@ -16,12 +16,11 @@ describe('relations/CssFontFaceSrc', function () {
 
                 assetGraph.findRelations({type: 'CssFontFaceSrc'})[0].detach();
                 expect(assetGraph.findAssets({type: 'Css'})[0].text, 'not to contain', 'url(data:');
-            })
-            .run(done);
+            });
     });
 
-    it('should handle a test case with multiple src properties in one rule and multiple urls in one value', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/relations/CssFontFaceSrc/multipleSrc/'})
+    it('should handle a test case with multiple src properties in one rule and multiple urls in one value', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/relations/CssFontFaceSrc/multipleSrc/'})
             .loadAssets('index.css')
             .populate()
             .queue(function (assetGraph) {
@@ -39,7 +38,6 @@ describe('relations/CssFontFaceSrc', function () {
                     'url(\'fontawesome-webfont.svgz#FontAwesomeRegular\')',
                     'url(\'fontawesome-webfont.svg#FontAwesomeRegular\')'
                 ]);
-            })
-            .run(done);
+            });
     });
 });
