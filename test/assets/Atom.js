2b28d58b2999eb485bc661b4568151f60260de56
Andreas Lind <andreas@one.com>
2017-07-23 14:49:26 +0200

    es6ify more tests

--- test/assets/Atom.js
+++ test/assets/Atom.js
@@ -1,22 +1,20 @@
 /*global describe, it*/
-var expect = require('../unexpected-with-plugins'),
-    AssetGraph = require('../../lib/AssetGraph');
+const expect = require('../unexpected-with-plugins');
+const AssetGraph = require('../../lib/AssetGraph');
 
 describe('assets/Atom', function () {
-    it('should find an Atom asset', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/Atom/'})
-        .loadAssets('feed.atom')
-        .populate()
-        .queue(function (assetGraph) {
-            expect(assetGraph, 'to contain asset', 'Atom');
-            expect(assetGraph, 'to contain asset', 'Png');
-            expect(assetGraph, 'to contain assets', {type: 'Html', isInline: true}, 2);
-            expect(assetGraph, 'to contain assets', {type: 'Html', isInline: false}, 1);
+    it('should find an Atom asset', async function () {
+        const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/assets/Atom/'})
+            .loadAssets('feed.atom')
+            .populate();
 
-            assetGraph.findAssets({fileName: 'bar.html'})[0].url = 'http://example.com/bar.html';
+        expect(assetGraph, 'to contain asset', 'Atom');
+        expect(assetGraph, 'to contain asset', 'Png');
+        expect(assetGraph, 'to contain assets', {type: 'Html', isInline: true}, 2);
+        expect(assetGraph, 'to contain assets', {type: 'Html', isInline: false}, 1);
 
-            expect(assetGraph, 'to contain asset', {type: 'Atom', text: /and a &lt;a href="http:\/\/example.com\/bar.html"/});
-        })
-        .run(done);
+        assetGraph.findAssets({fileName: 'bar.html'})[0].url = 'http://example.com/bar.html';
+
+        expect(assetGraph, 'to contain asset', {type: 'Atom', text: /and a &lt;a href="http:\/\/example.com\/bar.html"/});
     });
 });
--- test/assets/JavaScript.js
+++ test/assets/JavaScript.js
@@ -1,68 +1,50 @@
 /*global describe, it*/
-var expect = require('../unexpected-with-plugins'),
-    AssetGraph = require('../../lib/AssetGraph'),
-    sinon = require('sinon');
+const expect = require('../unexpected-with-plugins');
+const AssetGraph = require('../../lib/AssetGraph');
+const sinon = require('sinon');
 
 describe('assets/JavaScript', function () {
-    it('should handle a test case that has a parse error in an inline JavaScript asset', function (done) {
-        var firstWarning;
-        new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
-            .on('warn', function (err) {
-                firstWarning = firstWarning || err;
-            })
-            .loadAssets('parseErrorInInlineJavaScript.html')
-            .queue(function (assetGraph) {
-                expect(firstWarning, 'to be an', Error);
-                expect(firstWarning.message, 'to match', /parseErrorInInlineJavaScript\.html/);
-                expect(firstWarning.message, 'to match', /line 2\b/);
-            })
-            .run(done);
+    it('should handle a test case that has a parse error in an inline JavaScript asset', async function () {
+        let firstWarning;
+        await new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
+            .on('warn', err => firstWarning = firstWarning || err)
+            .loadAssets('parseErrorInInlineJavaScript.html');
+
+        expect(firstWarning, 'to be an', Error);
+        expect(firstWarning.message, 'to match', /parseErrorInInlineJavaScript\.html/);
+        expect(firstWarning.message, 'to match', /line 2\b/);
     });
 
-    it('should handle a test case that has a parse error in an external JavaScript asset', function (done) {
-        var firstWarning;
-        new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
-            .on('warn', function (err) {
-                firstWarning = firstWarning || err;
-            })
+    it('should handle a test case that has a parse error in an external JavaScript asset', async function () {
+        let firstWarning;
+        await new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
+            .on('warn', err => firstWarning = firstWarning || err)
             .loadAssets('parseErrorInExternalJavaScript.html')
-            .populate()
-            .queue(function (assetGraph) {
-                expect(firstWarning, 'to be an', Error);
-                expect(firstWarning.message, 'to match', /parseError\.js/);
-                expect(firstWarning.message, 'to match', /line 6\b/);
-            })
-            .run(done);
+            .populate();
+
+        expect(firstWarning, 'to be an', Error);
+        expect(firstWarning.message, 'to match', /parseError\.js/);
+        expect(firstWarning.message, 'to match', /line 6\b/);
     });
 
-    it('should handle a test case that has a parse error in an asset not in an assetgraph', function (done) {
-        function parseError() {
-            return  new AssetGraph.JavaScript({
-                text: 'var test)'
-            }).parseTree;
-        }
-
-        expect(parseError, 'to throw');
-
-        done();
+    it('should handle a test case that has a parse error in an asset not in an assetgraph', function () {
+        expect(() => new AssetGraph.JavaScript({ text: 'var test)' }).parseTree, 'to throw');
     });
 
-    it('should handle setting a new parseTree', function (done) {
-        var one = new AssetGraph.JavaScript({ text: 'var test = "true";' });
-        var two = new AssetGraph.JavaScript({ text: 'var test = "false";' });
+    it('should handle setting a new parseTree', function () {
+        const one = new AssetGraph.JavaScript({ text: 'var test = "true";' });
+        const two = new AssetGraph.JavaScript({ text: 'var test = "false";' });
 
         expect(one, 'not to have the same AST as', two);
 
         two.parseTree = one.parseTree;
 
         expect(one, 'to have the same AST as', two);
-
-        done();
     });
 
-    it('should handle minification', function (done) {
-        var one = new AssetGraph.JavaScript({ text: 'function test (argumentName) { return argumentName; }' });
-        var two = new AssetGraph.JavaScript({ text: 'function test (argumentName) { return argumentName; }' });
+    it('should handle minification', function () {
+        const one = new AssetGraph.JavaScript({ text: 'function test (argumentName) { return argumentName; }' });
+        const two = new AssetGraph.JavaScript({ text: 'function test (argumentName) { return argumentName; }' });
 
         expect(one, 'to have the same AST as', two);
 
@@ -70,139 +52,124 @@ describe('assets/JavaScript', function () {
 
         expect(one.text, 'not to equal', two.text);
         expect(two.text, 'to equal', 'function test(argumentName){return argumentName}');
-
-        done();
     });
 
-    it('should handle invalid arguments for Amd define call', function (done) {
+    it('should handle invalid arguments for Amd define call', async function () {
         sinon.stub(console, 'info');
 
-        var invalidArgument = new AssetGraph.JavaScript({
+        const invalidArgument = new AssetGraph.JavaScript({
             isRequired: true,
             text: 'define([1], function () {})'
         });
 
         invalidArgument.findOutgoingRelationsInParseTree();
 
-        new AssetGraph({ root: '.' })
+        await new AssetGraph({ root: '.' })
             .loadAssets([
                 invalidArgument
             ])
-            .populate()
-            .run(function (assetGraph) {
-                done();
+            .populate();
+    });
+
+    it('should preserve the copyright notice in a JavaScript asset', async function () {
+        const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
+            .loadAssets('copyrightNotice.js');
+
+        const javaScript = assetGraph.findAssets({type: 'JavaScript'})[0];
+        javaScript.parseTree.body.push({
+            type: 'VariableDeclaration',
+            kind: 'var',
+            declarations: [
+                {
+                    type: 'VariableDeclarator',
+                    id: {
+                        type: 'Identifier',
+                        name: 'foo'
+                    },
+                    init: { type: 'Literal', value: 'quux' }
+                }
+            ]
+        });
+        javaScript.markDirty();
+
+        expect(javaScript.text, 'to match', /Copyright blablabla/);
+    });
+
+    it('should handle a test case with JavaScript assets that have regular comments as the first non-whitespace tokens', async function () {
+        const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
+            .loadAssets('initialComment*.js');
+
+        for (const javaScript of assetGraph.findAssets({type: 'JavaScript'})) {
+            javaScript.parseTree.body.push({
+                type: 'VariableDeclaration',
+                declarations: [
+                    {
+                        type: 'VariableDeclarator',
+                        id: {
+                            type: 'Identifier',
+                            name: 'foo'
+                        },
+                        kind: 'var',
+                        init: { type: 'Literal', value: 'quux' }
+                    }
+                ]
             });
+            javaScript.minify();
+        }
+
+        const javaScripts = assetGraph.findAssets({type: 'JavaScript'});
+        expect(javaScripts[0].text, 'not to match', /Initial comment/);
+        expect(javaScripts[1].text, 'not to match', /Initial comment/);
     });
 
-    it('should preserve the copyright notice in a JavaScript asset', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
-            .loadAssets('copyrightNotice.js')
-            .queue(function (assetGraph) {
-                var javaScript = assetGraph.findAssets({type: 'JavaScript'})[0];
-                javaScript.parseTree.body.push({
-                    type: 'VariableDeclaration',
-                    kind: 'var',
-                    declarations: [
-                        {
-                            type: 'VariableDeclarator',
-                            id: {
-                                type: 'Identifier',
-                                name: 'foo'
-                            },
-                            init: { type: 'Literal', value: 'quux' }
-                        }
-                    ]
-                });
-                javaScript.markDirty();
-
-                expect(javaScript.text, 'to match', /Copyright blablabla/);
-            })
-            .run(done);
-    });
-
-    it('should handle a test case with JavaScript assets that have regular comments as the first non-whitespace tokens', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
-            .loadAssets('initialComment*.js')
-            .queue(function (assetGraph) {
-                assetGraph.findAssets({type: 'JavaScript'}).forEach(function (javaScript) {
-                    javaScript.parseTree.body.push({
-                        type: 'VariableDeclaration',
-                        declarations: [
-                            {
-                                type: 'VariableDeclarator',
-                                id: {
-                                    type: 'Identifier',
-                                    name: 'foo'
-                                },
-                                kind: 'var',
-                                init: { type: 'Literal', value: 'quux' }
-                            }
-                        ]
-                    });
-                    javaScript.minify();
-                });
-
-                var javaScripts = assetGraph.findAssets({type: 'JavaScript'});
-                expect(javaScripts[0].text, 'not to match', /Initial comment/);
-                expect(javaScripts[1].text, 'not to match', /Initial comment/);
-            })
-            .run(done);
-    });
-
-    it('should handle a test case with a JavaScript asset that has comments right before EOF, then marking it as dirty', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
+    it('should handle a test case with a JavaScript asset that has comments right before EOF, then marking it as dirty', async function () {
+        const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
             .loadAssets('commentsBeforeEof.js')
-            .populate()
-            .queue(function (assetGraph) {
-                var javaScript = assetGraph.findAssets({type: 'JavaScript'})[0];
-                // The reserialized JavaScript asset should still contain @preserve comment before eof, but not the quux one
+            .populate();
 
-                javaScript.markDirty();
+        const javaScript = assetGraph.findAssets({type: 'JavaScript'})[0];
+        // The reserialized JavaScript asset should still contain @preserve comment before eof, but not the quux one
 
-                expect(javaScript.text, 'to match', /@preserve/);
-                expect(javaScript.text, 'to match', /quux/);
+        javaScript.markDirty();
 
-                javaScript.minify();
+        expect(javaScript.text, 'to match', /@preserve/);
+        expect(javaScript.text, 'to match', /quux/);
 
-                // The reserialized JavaScript asset should contain both the @preserve and the quux comment when pretty printed
-                expect(javaScript.text, 'to match', /@preserve/);
-                expect(javaScript.text, 'not to match', /quux/);
-            })
-            .run(done);
+        javaScript.minify();
+
+        // The reserialized JavaScript asset should contain both the @preserve and the quux comment when pretty printed
+        expect(javaScript.text, 'to match', /@preserve/);
+        expect(javaScript.text, 'not to match', /quux/);
     });
 
-    it('should handle a test case with conditional compilation (@cc_on)', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
-            .loadAssets('conditionalCompilation.js')
-            .queue(function (assetGraph) {
-                assetGraph.findAssets({type: 'JavaScript'}).forEach(function (javaScript) {
-                    javaScript.markDirty();
-                });
+    it('should handle a test case with conditional compilation (@cc_on)', async function () {
+        const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
+            .loadAssets('conditionalCompilation.js');
 
-                expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to match', /@cc_on/);
-            })
-            .compressJavaScript()
-            .queue(function (assetGraph) {
-                // The @cc_on comment should still be in the serialization of the asset
-                expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to match', /@cc_on/);
-            })
-            .run(done);
+        for (const javaScript of assetGraph.findAssets({type: 'JavaScript'})) {
+            javaScript.markDirty();
+        }
+
+        expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to match', /@cc_on/);
+
+        await assetGraph.compressJavaScript();
+
+        // The @cc_on comment should still be in the serialization of the asset
+        expect(assetGraph.findAssets({type: 'JavaScript'})[0].text, 'to match', /@cc_on/);
     });
 
-    it('should handle a test case with global strict mode', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
+    it('should handle a test case with global strict mode', async function () {
+        const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/assets/JavaScript/'})
             .loadAssets('globalstrict.html')
-            .populate()
-            .queue(function (assetGraph) {
-                expect(assetGraph.findAssets({fileName: 'globalstrict.js'})[0].strict, 'to equal', true);
-                expect(assetGraph.findAssets({fileName: 'nonstrict.js'})[0].strict, 'to equal', false);
-            })
-            .run(done);
+            .populate();
+
+        expect(assetGraph.findAssets({fileName: 'globalstrict.js'})[0].strict, 'to equal', true);
+        expect(assetGraph.findAssets({fileName: 'nonstrict.js'})[0].strict, 'to equal', false);
     });
 
     it('should tolerate ES6 syntax', function () {
-        var es6Text = 'import gql from \'graphql-tag\';\nlet a = 123;';
-        var javaScript = new AssetGraph.JavaScript({
+        const es6Text = 'import gql from \'graphql-tag\';\nlet a = 123;';
+        const javaScript = new AssetGraph.JavaScript({
             text: es6Text
         });
         expect(javaScript.parseTree, 'to satisfy', {
@@ -218,10 +185,8 @@ describe('assets/JavaScript', function () {
     });
 
     it.skip('should tolerate Object spread syntax', function () {
-        var text = 'const foo = { ...bar };';
-        var javaScript = new AssetGraph.JavaScript({
-            text: text
-        });
+        const text = 'const foo = { ...bar };';
+        const javaScript = new AssetGraph.JavaScript({ text });
         expect(javaScript.parseTree, 'to satisfy', {
             type: 'Program',
             body: [
@@ -233,8 +198,8 @@ describe('assets/JavaScript', function () {
     });
 
     it('should tolerate JSX syntax', function () {
-        var jsxText = 'function render() { return (<MyComponent />); }';
-        var javaScript = new AssetGraph.JavaScript({
+        const jsxText = 'function render() { return (<MyComponent />); }';
+        const javaScript = new AssetGraph.JavaScript({
             text: jsxText
         });
         expect(javaScript.parseTree, 'to satisfy', {
@@ -272,10 +237,8 @@ describe('assets/JavaScript', function () {
     });
 
     it('should fall back to "script" mode when a parse error is encountered', function () {
-        var text = 'await = 123;';
-        var javaScript = new AssetGraph.JavaScript({
-            text: text
-        });
+        const text = 'await = 123;';
+        const javaScript = new AssetGraph.JavaScript({ text });
         expect(javaScript.parseTree, 'to satisfy', {
             type: 'Program',
             body: [
@@ -295,11 +258,11 @@ describe('assets/JavaScript', function () {
         expect(javaScript.text, 'to equal', text);
     });
 
-    it('should emit an info event (and no warn events) when falling back to script mode', function () {
-        var errorSpy = sinon.spy().named('error');
-        var warnSpy = sinon.spy().named('warn');
-        var infoSpy = sinon.spy().named('info');
-        return new AssetGraph()
+    it('should emit an info event (and no warn events) when falling back to script mode', async function () {
+        const errorSpy = sinon.spy().named('error');
+        const warnSpy = sinon.spy().named('warn');
+        const infoSpy = sinon.spy().named('info');
+        await new AssetGraph()
             .on('error', errorSpy)
             .on('warn', warnSpy)
             .on('info', infoSpy)
@@ -307,19 +270,18 @@ describe('assets/JavaScript', function () {
                 url: 'http://example.com/script.js',
                 text: 'await = 123;'
             }))
-            .populate()
-            .queue(function () {
-                expect([errorSpy, warnSpy, infoSpy], 'to have calls satisfying', function () {
-                    infoSpy('Could not parse http://example.com/script.js as a module, fall back to script mode\nLine 1: Unexpected identifier');
-                });
-            });
+            .populate();
+
+        expect([errorSpy, warnSpy, infoSpy], 'to have calls satisfying', () => {
+            infoSpy('Could not parse http://example.com/script.js as a module, fall back to script mode\nLine 1: Unexpected identifier');
+        });
     });
 
-    it('should emit one warn event (and no other events) when neither module nor script mode works', function () {
-        var errorSpy = sinon.spy().named('error');
-        var warnSpy = sinon.spy().named('warn');
-        var infoSpy = sinon.spy().named('info');
-        return new AssetGraph()
+    it('should emit one warn event (and no other events) when neither module nor script mode works', async function () {
+        const errorSpy = sinon.spy().named('error');
+        const warnSpy = sinon.spy().named('warn');
+        const infoSpy = sinon.spy().named('info');
+        await new AssetGraph()
             .on('error', errorSpy)
             .on('warn', warnSpy)
             .on('info', infoSpy)
@@ -327,11 +289,10 @@ describe('assets/JavaScript', function () {
                 url: 'http://example.com/script.js',
                 text: 'qwvce)'
             }))
-            .populate()
-            .queue(function () {
-                expect([errorSpy, warnSpy, infoSpy], 'to have calls satisfying', function () {
-                    warnSpy('Parse error in http://example.com/script.js\nLine 1: Unexpected token ) (line 1)');
-                });
-            });
+            .populate();
+
+        expect([errorSpy, warnSpy, infoSpy], 'to have calls satisfying', () => {
+            warnSpy('Parse error in http://example.com/script.js\nLine 1: Unexpected token ) (line 1)');
+        });
     });
 });
