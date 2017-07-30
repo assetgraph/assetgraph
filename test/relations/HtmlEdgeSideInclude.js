e1f0b5eaa29196ceafac7ad0badded0f7a4e394f
Andreas Lind <andreas@one.com>
2017-07-30 11:34:39 +0200

    Fix HtmlEdgeSideInclude test, 28 failing [ci skip]

--- test/relations/HtmlEdgeSideInclude.js
+++ test/relations/HtmlEdgeSideInclude.js
@@ -1,26 +1,25 @@
 /*global describe, it*/
-var expect = require('../unexpected-with-plugins'),
-    urlTools = require('urltools'),
-    AssetGraph = require('../../lib/AssetGraph');
+const expect = require('../unexpected-with-plugins');
+const urlTools = require('urltools');
+const AssetGraph = require('../../lib/AssetGraph');
 
 describe('relations/HtmlEdgeSideInclude', function () {
-    it('should handle a test case with existing <esi ...> elements', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/relations/HtmlEdgeSideInclude/'})
+    it('should handle a test case with existing <esi ...> elements', async function () {
+        const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/relations/HtmlEdgeSideInclude/'})
             .loadAssets('index.html')
             .populate({
+                startAssets: { isInitial: true },
                 followRelations: {to: {url: /\.html$/}}
-            })
-            .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain assets', 'Html', 2);
-                expect(assetGraph, 'to contain relations', 'HtmlEdgeSideInclude', 2);
-                expect(assetGraph, 'to contain relations including unresolved', 'HtmlEdgeSideInclude', 2);
-                assetGraph.findAssets({url: /\/index\.html/})[0].url = urlTools.resolveUrl(assetGraph.root, 'foo/index.html');
-                expect(
-                    assetGraph.findRelations({to: {url: /\.php$/}, type: 'HtmlEdgeSideInclude'}, true)[0].href,
-                    'to equal',
-                    '../dynamicStuff/metaTags.php'
-                );
-            })
-            .run(done);
+            });
+
+        expect(assetGraph, 'to contain assets', 'Html', 2);
+        expect(assetGraph, 'to contain relations', 'HtmlEdgeSideInclude', 2);
+        expect(assetGraph, 'to contain relations including unresolved', 'HtmlEdgeSideInclude', 2);
+        assetGraph.findAssets({url: /\/index\.html/})[0].url = urlTools.resolveUrl(assetGraph.root, 'foo/index.html');
+        expect(
+            assetGraph.findRelations({to: {url: /\.php$/}, type: 'HtmlEdgeSideInclude'}, true)[0].href,
+            'to equal',
+            '../dynamicStuff/metaTags.php'
+        );
     });
 });
