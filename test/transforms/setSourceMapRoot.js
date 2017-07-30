408a528f3c59ee898ad6ccb680b02d49fd5c4159
Andreas Lind <andreas@one.com>
2017-07-30 12:44:45 +0200

    async/awaitify the rest of the setSourceMapRoot test

--- test/transforms/setSourceMapRoot.js
+++ test/transforms/setSourceMapRoot.js
@@ -3,55 +3,49 @@ const expect = require('../unexpected-with-plugins');
 const AssetGraph = require('../../lib/AssetGraph');
 
 describe('transforms/setSourceMapRoot', function () {
-    it('should be able to modify source root', function (done) {
-        new AssetGraph()
+    it('should be able to modify source root', async function () {
+        const assetGraph = await new AssetGraph()
             .loadAssets(new AssetGraph.SourceMap({text: '{"sourceRoot":"rootFolder"}'}))
-            .setSourceMapRoot(null, 'otherFolder')
-            .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain asset', 'SourceMap');
-                expect(assetGraph.findAssets({type: 'SourceMap'})[0].parseTree.sourceRoot, 'to equal', 'otherFolder');
-            })
-            .run(done);
+            .setSourceMapRoot(null, 'otherFolder');
+
+        expect(assetGraph, 'to contain asset', 'SourceMap');
+        expect(assetGraph.findAssets({type: 'SourceMap'})[0].parseTree.sourceRoot, 'to equal', 'otherFolder');
     });
 
-    it('should be able to delete source root', function (done) {
-        new AssetGraph()
+    it('should be able to delete source root', async function () {
+        const assetGraph = await new AssetGraph()
             .loadAssets(new AssetGraph.SourceMap({text: '{"sourceRoot":"rootFolder"}'}))
-            .setSourceMapRoot(null, null)
-            .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain asset', 'SourceMap');
-                expect(assetGraph.findAssets({type: 'SourceMap'})[0].parseTree.sourceRoot, 'to equal', void 0);
-            })
-            .run(done);
+            .setSourceMapRoot(null, null);
+
+        expect(assetGraph, 'to contain asset', 'SourceMap');
+        expect(assetGraph.findAssets({type: 'SourceMap'})[0].parseTree.sourceRoot, 'to equal', void 0);
     });
 
     describe('with a source map that has an existing sourceRoot', function () {
-        it('should update relative references in sources', function () {
-            return new AssetGraph({root: __dirname + '/../../testdata/transforms/setSourceMapRoot/existingSourceRoot/'})
+        it('should update relative references in sources', async function () {
+            const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/transforms/setSourceMapRoot/existingSourceRoot/'})
                 .loadAssets('index.html')
-                .populate()
-                .queue(function (assetGraph) {
-                    expect(assetGraph, 'to contain asset', {url: /\.less$/, isLoaded: true});
-                })
-                .setSourceMapRoot(null, 'somewhereElse')
-                .queue(function (assetGraph) {
-                    expect(assetGraph.findAssets({type: 'SourceMap'})[0].parseTree, 'to satisfy', {
-                        sources: ['../theSources/foo.less']
-                    });
-                });
+                .populate();
+
+            expect(assetGraph, 'to contain asset', {url: /\.less$/, isLoaded: true});
+
+            await assetGraph.setSourceMapRoot(null, 'somewhereElse');
+
+            expect(assetGraph.findAssets({type: 'SourceMap'})[0].parseTree, 'to satisfy', {
+                sources: ['../theSources/foo.less']
+            });
         });
 
-        it('should take the new sourceRoot into consideration when an asset is moved', function () {
-            return new AssetGraph({root: __dirname + '/../../testdata/transforms/setSourceMapRoot/existingSourceRoot/'})
+        it('should take the new sourceRoot into consideration when an asset is moved', async function () {
+            const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/transforms/setSourceMapRoot/existingSourceRoot/'})
                 .loadAssets('index.html')
                 .populate()
-                .setSourceMapRoot(null, 'somewhereElse')
-                .queue(function (assetGraph) {
-                    assetGraph.findAssets({url: /\.less$/})[0].url = assetGraph.root + 'somewhereElse/bar.less';
-                    expect(assetGraph.findAssets({type: 'SourceMap'})[0].parseTree, 'to satisfy', {
-                        sources: ['bar.less']
-                    });
-                });
+                .setSourceMapRoot(null, 'somewhereElse');
+
+            assetGraph.findAssets({url: /\.less$/})[0].url = assetGraph.root + 'somewhereElse/bar.less';
+            expect(assetGraph.findAssets({type: 'SourceMap'})[0].parseTree, 'to satisfy', {
+                sources: ['bar.less']
+            });
         });
 
         it('should allow fixing up a wrong sourceRoot before continuing population', async function () {
