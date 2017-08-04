63252c12a7c928697a3804dacf0d8a9552ebb5f3
Andreas Lind <andreas@one.com>
2017-08-05 01:03:33 +0200

    es6ify HtmlShortcutIcon test [ci skip]

--- test/relations/HtmlShortcutIcon.js
+++ test/relations/HtmlShortcutIcon.js
@@ -1,34 +1,33 @@
 /*global describe, it*/
-var expect = require('../unexpected-with-plugins'),
-    AssetGraph = require('../../lib/AssetGraph');
+const expect = require('../unexpected-with-plugins');
+const AssetGraph = require('../../lib/AssetGraph');
 
 describe('relations/HtmlShortcutIcon', function () {
-    it('should handle a test case with an existing <link rel="shortcut icon" href="..."> elements in different variants', function (done) {
-        new AssetGraph({root: __dirname + '/../../testdata/relations/HtmlShortcutIcon/'})
+    it('should handle a test case with an existing <link rel="shortcut icon" href="..."> elements in different variants', async function () {
+        const assetGraph = await new AssetGraph({root: __dirname + '/../../testdata/relations/HtmlShortcutIcon/'})
             .loadAssets('index.html')
-            .populate()
-            .queue(function (assetGraph) {
-                expect(assetGraph, 'to contain assets', 2);
-                expect(assetGraph, 'to contain relations', 'HtmlShortcutIcon', 7);
+            .populate();
 
-                var htmlAsset = assetGraph.findAssets({type: 'Html'})[0],
-                    pngAsset = assetGraph.findAssets({type: 'Png'})[0],
-                    firstExistingHtmlShortcutIconRelation = assetGraph.findRelations({type: 'HtmlShortcutIcon'})[0];
-                new AssetGraph.HtmlShortcutIcon({
-                    from: htmlAsset,
-                    to: pngAsset
-                }).attach(htmlAsset, 'after', firstExistingHtmlShortcutIconRelation);
-                new AssetGraph.HtmlShortcutIcon({
-                    from: htmlAsset,
-                    to: pngAsset
-                }).attach(htmlAsset, 'before', firstExistingHtmlShortcutIconRelation);
+        expect(assetGraph, 'to contain assets', 2);
+        expect(assetGraph, 'to contain relations', 'HtmlShortcutIcon', 7);
 
-                expect(assetGraph, 'to contain relations', 'HtmlShortcutIcon', 9);
+        const htmlAsset = assetGraph.findAssets({type: 'Html'})[0];
+        const pngAsset = assetGraph.findAssets({type: 'Png'})[0];
+        const firstExistingHtmlShortcutIconRelation = assetGraph.findRelations({type: 'HtmlShortcutIcon'})[0];
 
-                var matches = assetGraph.findAssets({type: 'Html'})[0].text.match(/<link rel="shortcut icon" href="foo.png">/g);
-                expect(matches, 'not to be null');
-                expect(matches, 'to have length', 3);
-            })
-            .run(done);
+        new AssetGraph.HtmlShortcutIcon({
+            from: htmlAsset,
+            to: pngAsset
+        }).attach(htmlAsset, 'after', firstExistingHtmlShortcutIconRelation);
+        new AssetGraph.HtmlShortcutIcon({
+            from: htmlAsset,
+            to: pngAsset
+        }).attach(htmlAsset, 'before', firstExistingHtmlShortcutIconRelation);
+
+        expect(assetGraph, 'to contain relations', 'HtmlShortcutIcon', 9);
+
+        const matches = assetGraph.findAssets({type: 'Html'})[0].text.match(/<link rel="shortcut icon" href="foo.png">/g);
+        expect(matches, 'not to be null')
+            .and('to have length', 3);
     });
 });
