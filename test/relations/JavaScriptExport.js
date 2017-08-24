332f96889f59005d51bdc3d46a90ef318352af5d
Andreas Lind <andreas@one.com>
2017-08-25 00:06:53 +0200

    JavaScriptExport: Add support for ExportAllDeclaration (export * from ...)

--- test/relations/JavaScriptExport.js
+++ test/relations/JavaScriptExport.js
@@ -2,12 +2,26 @@ const expect = require('../unexpected-with-plugins');
 const AssetGraph = require('../../lib/AssetGraph');
 
 describe('JavaScriptExport', function () {
-    it('should detect a relation', function () {
+    it('should detect an ExportNamedDeclaration node', function () {
         const javaScript = new AssetGraph().addAsset({
             type: 'JavaScript',
             url: 'https://example.com/',
             text: `
                 export { foo } from 'bar/quux.js';
+            `
+        });
+
+        expect(javaScript.outgoingRelations, 'to satisfy', [
+            { type: 'JavaScriptExport', href: 'bar/quux.js', to: { url: 'https://example.com/bar/quux.js' } }
+        ]);
+    });
+
+    it('should detect an ExportAllDeclaration node', function () {
+        const javaScript = new AssetGraph().addAsset({
+            type: 'JavaScript',
+            url: 'https://example.com/',
+            text: `
+                export * from 'bar/quux.js';
             `
         });
 
