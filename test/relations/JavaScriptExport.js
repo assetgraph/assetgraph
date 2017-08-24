7f3d35c66ab57355919be7ed5517d978f29fd1c5
Andreas Lind <andreas@one.com>
2017-08-25 00:07:18 +0200

    Implement JavaScript{Im,Ex}port#{at,de}tach

--- test/relations/JavaScriptExport.js
+++ test/relations/JavaScriptExport.js
@@ -43,4 +43,111 @@ describe('JavaScriptExport', function () {
         javaScript.markDirty();
         expect(javaScript.text, 'to contain', 'export {\n    foo\n} from \'blabla.js\';');
     });
+
+    describe('#attach', function () {
+        describe('with a position of first', function () {
+            it('should attach before the first existing export', function () {
+                const javaScript = new AssetGraph().addAsset({
+                    type: 'JavaScript',
+                    url: 'https://example.com/',
+                    text: `
+                        export { foo } from 'bar/quux.js';
+                    `
+                });
+
+                const newRelation = javaScript.addRelation({
+                    type: 'JavaScriptExport',
+                    to: 'http://blabla.com/lib.js'
+                }, 'first');
+                expect(javaScript.outgoingRelations, 'to satisfy', { 0: newRelation });
+                expect(javaScript.text, 'to begin with', 'export * from \'http://blabla.com/lib.js\';');
+            });
+        });
+
+        describe('with a position of last', function () {
+            it('should attach after the last existing export', function () {
+                const javaScript = new AssetGraph().addAsset({
+                    type: 'JavaScript',
+                    url: 'https://example.com/',
+                    text: `
+                        export { foo } from 'bar/quux.js';
+                    `
+                });
+
+                const newRelation = javaScript.addRelation({
+                    type: 'JavaScriptExport',
+                    to: 'http://blabla.com/lib.js'
+                }, 'last');
+                expect(javaScript.outgoingRelations, 'to satisfy', { 1: newRelation });
+                expect(javaScript.text, 'to end with', 'export * from \'http://blabla.com/lib.js\';');
+            });
+        });
+
+        describe('with a position of after', function () {
+            it('should attach after the given existing export', function () {
+                const javaScript = new AssetGraph().addAsset({
+                    type: 'JavaScript',
+                    url: 'https://example.com/',
+                    text: `
+                        export { foo } from 'bar/quux.js';
+                        export { baz } from 'blah.js';
+                    `
+                });
+
+                const newRelation = javaScript.addRelation({
+                    type: 'JavaScriptExport',
+                    to: 'http://blabla.com/lib.js'
+                }, 'after', javaScript.outgoingRelations[0]);
+                expect(javaScript.outgoingRelations, 'to satisfy', { 1: newRelation });
+                expect(javaScript.text, 'to equal', `
+                    export {
+                        foo
+                    } from 'bar/quux.js';
+                    export * from 'http://blabla.com/lib.js';
+                    export {
+                        baz
+                    } from 'blah.js';`.replace(/^\n/, '').replace(/^ {20}/mg, ''));
+            });
+        });
+
+        describe('with a position of before', function () {
+            it('should attach before the given existing export', function () {
+                const javaScript = new AssetGraph().addAsset({
+                    type: 'JavaScript',
+                    url: 'https://example.com/',
+                    text: `
+                        export { foo } from 'bar/quux.js';
+                        export { baz } from 'blah.js';
+                    `
+                });
+
+                const newRelation = javaScript.addRelation({
+                    type: 'JavaScriptExport',
+                    to: 'http://blabla.com/lib.js'
+                }, 'before', javaScript.outgoingRelations[1]);
+                expect(javaScript.outgoingRelations, 'to satisfy', { 1: newRelation });
+                expect(javaScript.text, 'to equal', `
+                    export {
+                        foo
+                    } from 'bar/quux.js';
+                    export * from 'http://blabla.com/lib.js';
+                    export {
+                        baz
+                    } from 'blah.js';`.replace(/^\n/, '').replace(/^ {20}/mg, ''));
+            });
+        });
+    });
+
+    describe('#detach', function () {
+        it('should remove the relation from the source code and the outgoingRelations array', function () {
+            const javaScript = new AssetGraph().addAsset({
+                type: 'JavaScript',
+                url: 'https://example.com/',
+                text: 'export { foo } from \'bar/quux.js\';'
+            });
+            javaScript.outgoingRelations[0].detach();
+            expect(javaScript.outgoingRelations, 'to equal', []);
+            expect(javaScript.text, 'to equal', '');
+        });
+    });
 });
--- test/relations/JavaScriptImport.js
+++ test/relations/JavaScriptImport.js
@@ -29,4 +29,103 @@ describe('JavaScriptImport', function () {
         javaScript.markDirty();
         expect(javaScript.text, 'to contain', 'import foo from \'blabla.js\';');
     });
+
+    describe('#attach', function () {
+        describe('with a position of first', function () {
+            it('should attach before the first existing import', function () {
+                const javaScript = new AssetGraph().addAsset({
+                    type: 'JavaScript',
+                    url: 'https://example.com/',
+                    text: `
+                        import foo from 'bar/quux.js';
+                    `
+                });
+
+                const newRelation = javaScript.addRelation({
+                    type: 'JavaScriptImport',
+                    to: 'http://blabla.com/lib.js'
+                }, 'first');
+                expect(javaScript.outgoingRelations, 'to satisfy', { 0: newRelation });
+                expect(javaScript.text, 'to begin with', 'import \'http://blabla.com/lib.js\';');
+            });
+        });
+
+        describe('with a position of last', function () {
+            it('should attach after the last existing import', function () {
+                const javaScript = new AssetGraph().addAsset({
+                    type: 'JavaScript',
+                    url: 'https://example.com/',
+                    text: `
+                        import foo from 'bar/quux.js';
+                    `
+                });
+
+                const newRelation = javaScript.addRelation({
+                    type: 'JavaScriptImport',
+                    to: 'http://blabla.com/lib.js'
+                }, 'last');
+                expect(javaScript.outgoingRelations, 'to satisfy', { 1: newRelation });
+                expect(javaScript.text, 'to end with', 'import \'http://blabla.com/lib.js\';');
+            });
+        });
+
+        describe('with a position of after', function () {
+            it('should attach after the given existing import', function () {
+                const javaScript = new AssetGraph().addAsset({
+                    type: 'JavaScript',
+                    url: 'https://example.com/',
+                    text: `
+                        import foo from 'bar/quux.js';
+                        import baz from 'blah.js';
+                    `
+                });
+
+                const newRelation = javaScript.addRelation({
+                    type: 'JavaScriptImport',
+                    to: 'http://blabla.com/lib.js'
+                }, 'after', javaScript.outgoingRelations[0]);
+                expect(javaScript.outgoingRelations, 'to satisfy', { 1: newRelation });
+                expect(javaScript.text, 'to equal', `
+                    import foo from 'bar/quux.js';
+                    import 'http://blabla.com/lib.js';
+                    import baz from 'blah.js';`.replace(/^\s+/mg, ''));
+            });
+        });
+
+        describe('with a position of before', function () {
+            it('should attach before the given existing import', function () {
+                const javaScript = new AssetGraph().addAsset({
+                    type: 'JavaScript',
+                    url: 'https://example.com/',
+                    text: `
+                        import foo from 'bar/quux.js';
+                        import baz from 'blah.js';
+                    `
+                });
+
+                const newRelation = javaScript.addRelation({
+                    type: 'JavaScriptImport',
+                    to: 'http://blabla.com/lib.js'
+                }, 'before', javaScript.outgoingRelations[1]);
+                expect(javaScript.outgoingRelations, 'to satisfy', { 1: newRelation });
+                expect(javaScript.text, 'to equal', `
+                    import foo from 'bar/quux.js';
+                    import 'http://blabla.com/lib.js';
+                    import baz from 'blah.js';`.replace(/^\s+/mg, ''));
+            });
+        });
+    });
+
+    describe('#detach', function () {
+        it('should remove the relation from the source code and the outgoingRelations array', function () {
+            const javaScript = new AssetGraph().addAsset({
+                type: 'JavaScript',
+                url: 'https://example.com/',
+                text: 'import foo from \'bar/quux.js\';'
+            });
+            javaScript.outgoingRelations[0].detach();
+            expect(javaScript.outgoingRelations, 'to equal', []);
+            expect(javaScript.text, 'to equal', '');
+        });
+    });
 });
