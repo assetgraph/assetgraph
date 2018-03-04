a40c4fddb8f15be926b38bf061de666d019cf595
Andreas Lind <andreaslindpetersen@gmail.com>
2018-03-04 12:59:53 +0100

    eslint config: Adopt @gustavnikolaj's tty trick to disable the prettier plugin unless running in a tty

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
-module.exports = {
-    extends: ['pretty-standard'],
-    plugins: ['prettier'],
-    rules: {
-        'prettier/prettier': 'error'
-    }
+const config = {
+  extends: ['pretty-standard']
 };
+
+if (process.stdin.isTTY) {
+  // Enable plugin-prettier when running in a terminal. Allows us to have
+  // eslint verify prettier formatting, while not being bothered by it in our
+  // editors.
+  config.plugins = config.plugins || [];
+  config.plugins.push('prettier');
+  config.rules = config.rules || {};
+  config.rules['prettier/prettier'] = 'error';
+}
+
+module.exports = config;
