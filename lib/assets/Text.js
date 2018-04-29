assetgraph/assetgraph

commit da206a63183b5d477810cb585438b1793bb97ce2

es6ify

lib/assets/Text.js
@@ -215,11 +215,11 @@ class Text extends Asset {
   }
 
   markDirty() {
+    super.markDirty();
     if (this._parseTree) {
       this._text = undefined;
       this._sourceMap = undefined;
     }
-    return Asset.prototype.markDirty.call(this);
   }
 
   _getTextFromRawSrc() {

lib/util/determineFileType.js
@@ -15,9 +15,7 @@ function limitConcurrency(concurrency, fn) {
       });
     }
   }
-  return function() {
-    // ...
-    const args = Array.prototype.slice.call(arguments);
+  return function(...args) {
     const thisObject = this;
     return new Promise((resolve, reject) => {
       queue.push({

commit 14e097a2e2bc1436c17600706b92c84319615797

Fix undeffing order broken by da206a6

lib/assets/Text.js
@@ -215,11 +215,11 @@ class Text extends Asset {
   }
 
   markDirty() {
-    super.markDirty();
     if (this._parseTree) {
       this._text = undefined;
       this._sourceMap = undefined;
     }
+    super.markDirty();
   }
 
   _getTextFromRawSrc() {