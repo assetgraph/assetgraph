assetgraph/assetgraph

commit ae7d28c01c18ed70d51104fad436022fd0014f0a

Transition to new URL

lib/assets/Asset.js
@@ -703,7 +703,7 @@ class Asset extends EventEmitter {
   get origin() {
     const nonInlineAncestor = this.nonInlineAncestor;
     if (nonInlineAncestor) {
-      const urlObj = urlModule.parse(nonInlineAncestor.url);
+      const urlObj = new urlModule.URL(nonInlineAncestor.url);
       if (urlObj) {
         return (
           urlObj.protocol + (urlObj.slashes ? '//' : '') + (urlObj.host || '')
@@ -1208,13 +1208,13 @@ class Asset extends EventEmitter {
     const url = this.url;
     const queryString = qs.stringify(obj);
     if (url) {
-      const urlObj = urlModule.parse(url);
+      const urlObj = new urlModule.URL(url);
       if (queryString.length > 0) {
         urlObj.search = `?${queryString}`;
       } else {
         urlObj.search = '';
       }
-      this.url = urlObj.format();
+      this.url = urlObj.toString();
     } else {
       this._query = queryString;
     }
@@ -1229,21 +1229,24 @@ class Asset extends EventEmitter {
    */
   get query() {
     if (!this._query && !this.isInline) {
-      this._query = new Proxy(qs.parse(urlModule.parse(this.url).query), {
-        get(target, parameterName) {
-          return target[parameterName];
-        },
+      this._query = new Proxy(
+        qs.parse(new urlModule.URL(this.url).search.slice(1)),
+        {
+          get(target, parameterName) {
+            return target[parameterName];
+          },
 
-        set: (target, parameterName, value) => {
-          target[parameterName] = String(value);
-          this._updateQueryString(target);
-        },
+          set: (target, parameterName, value) => {
+            target[parameterName] = String(value);
+            this._updateQueryString(target);
+          },
 
-        deleteProperty: (target, parameterName) => {
-          delete target[parameterName];
-          this._updateQueryString(target);
+          deleteProperty: (target, parameterName) => {
+            delete target[parameterName];
+            this._updateQueryString(target);
+          }
         }
-      });
+      );
     }
     return this._query;
   }

lib/matchSourceExpression.js
@@ -13,7 +13,7 @@ const defaultPortByScheme = {
 };
 
 function origin(url) {
-  if (/file:\/\//.test(url)) {
+  if (/^file:\/\//.test(url)) {
     return 'file:';
   }
   const matchOrigin = url.match(
@@ -42,9 +42,9 @@ module.exports = function matchSourceExpression(
   sourceExpression,
   protectedResourceUrl
 ) {
-  const protectedResourceUrlObj = urlModule.parse(protectedResourceUrl);
+  const protectedResourceUrlObj = new urlModule.URL(protectedResourceUrl);
   // Let url be the result of processing the URL through the URL parser.
-  const urlObj = urlModule.parse(url);
+  const urlObj = new urlModule.URL(url);
 
   // If the source expression a consists of a single U+002A ASTERISK character (*), and url’s scheme is not one of blob, data, filesystem, then return does match.
   if (
@@ -73,8 +73,8 @@ module.exports = function matchSourceExpression(
     const sourceExpressionPortPart = matchHostSource[3];
     const sourceExpressionPathPart = matchHostSource[4];
 
-    // If url’s host is null, return does not match.
-    if (urlObj.hostname === null) {
+    // If url’s host is absent, return does not match.
+    if (urlObj.hostname === '') {
       return false;
     }
     // Let url-scheme, url-host, and url-port be the scheme, host, and port of url’s origin, respectively.
@@ -82,7 +82,7 @@ module.exports = function matchSourceExpression(
     const urlScheme = urlObj.protocol.replace(/:$/, '');
     const urlHost = urlObj.hostname;
     const urlPort =
-      urlObj.port === null
+      urlObj.port === ''
         ? defaultPortByScheme[urlScheme]
         : parseInt(urlObj.port, 10);
 

lib/relations/Relation.js
@@ -216,8 +216,8 @@ class Relation {
     if (this.canonical) {
       return false;
     }
-    const fromUrlObj = urlModule.parse(fromUrl);
-    const toUrlObj = urlModule.parse(urlModule.resolve(fromUrl, toUrl));
+    const fromUrlObj = new urlModule.URL(fromUrl);
+    const toUrlObj = new urlModule.URL(toUrl, fromUrl);
     if (
       fromUrlObj.protocol !== toUrlObj.protocol ||
       fromUrlObj.hostname !== toUrlObj.hostname
@@ -258,11 +258,18 @@ class Relation {
         this.from.assetGraph &&
         this.from.assetGraph.canonicalRoot
       ) {
+        // Need to figure out another way to achieve the "slashesDenoteHost"
+        // behavior (3rd parameter to urlModule.parse)
+        // eslint-disable-next-line node/no-deprecated-api
         const canonicalRootObj = urlModule.parse(
           this.from.assetGraph.canonicalRoot,
           false,
           true
         );
+
+        // Need to figure out another way to achieve the "slashesDenoteHost"
+        // behavior (3rd parameter to urlModule.parse)
+        // eslint-disable-next-line node/no-deprecated-api
         const hrefObj = urlModule.parse(this.href, false, true);
 
         canonical =

lib/transforms/reviewContentSecurityPolicy.js
@@ -59,7 +59,7 @@ module.exports = (queryObj, { includePath = false, update = false, level }) => {
       } else if (includePath === true || includePathByDirective[directive]) {
         return url.replace(/^https?:\/\//, '');
       } else {
-        const urlObj = urlModule.parse(url);
+        const urlObj = new urlModule.URL(url);
         let host = urlObj.hostname;
         if (
           urlObj.port &&

lib/transforms/splitCssIfIeLimitIsReached.js
@@ -1,5 +1,6 @@
 const postcss = require('postcss');
 const Css = require('../assets/Css');
+const urlModule = require('url');
 
 // http://blogs.msdn.com/b/ieinternals/archive/2011/05/14/internet-explorer-stylesheet-rule-selector-import-sheet-limit-maximum.aspx
 module.exports = (
@@ -96,12 +97,12 @@ module.exports = (
           const asset = assetGraph.addAsset(replacement);
 
           if (!largeAsset.isInline) {
-            asset.url = require('url').resolve(
-              largeAsset.url,
+            asset.url = new urlModule.URL(
               `${largeAsset.fileName.split('.').shift()}-${i}${
                 largeAsset.extension
-              }`
-            );
+              }`,
+              largeAsset.url
+            ).toString();
           }
 
           assetGraph.addAsset(asset);

lib/transforms/subsetFonts.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const url = require('url');
+const urlModule = require('url');
 const Promise = require('bluebird');
 const memoizeSync = require('memoizesync');
 const urltools = require('urltools');
@@ -176,7 +176,7 @@ function asyncLoadStyleRelationWithFallback(
   );
 
   // Resource hint: prefetch google font stylesheet
-  const parsedOriginalUrl = url.parse(originalRelation.to.url);
+  const parsedOriginalUrl = new urlModule.URL(originalRelation.to.url);
   htmlAsset.addRelation(
     {
       type: 'HtmlPreconnectLink',

test/transforms/moveAssetsInOrder.js
@@ -39,7 +39,7 @@ describe('transforms/moveAssetsInOrder', function() {
     await assetGraph.populate();
 
     await expect(
-      assetGraph.moveAssetsInOrder({ type: 'Css' }, '/css/'),
+      () => assetGraph.moveAssetsInOrder({ type: 'Css' }, '/css/'),
       'to error',
       /Couldn't find a suitable rename order due to cycles in the selection/
     );

test/unexpectedAssetGraph.js
@@ -1,4 +1,4 @@
-const URL = require('url');
+const urlModule = require('url');
 const urlTools = require('urltools');
 const esprima = require('esprima');
 const escodegen = require('escodegen-papandreou');
@@ -109,7 +109,7 @@ module.exports = {
         if (!Array.isArray(urls)) {
           urls = [urls];
         }
-        urls = urls.map(url => URL.resolve(subject.root, url));
+        urls = urls.map(url => new urlModule.URL(url, subject.root).toString());
         expect.errorMode = 'nested';
         urls.forEach(function(url) {
           expect(subject.findAssets({ url }), 'to have length', 1);

commit 7ae646db157548578e0ea48ad237073353a17218

Asset#origin: Fix regression introduced in ae7d28c0
https://github.com/papandreou/bringhome/pull/22#issuecomment-460079462

lib/assets/Asset.js
@@ -704,10 +704,9 @@ class Asset extends EventEmitter {
     const nonInlineAncestor = this.nonInlineAncestor;
     if (nonInlineAncestor) {
       const urlObj = new urlModule.URL(nonInlineAncestor.url);
+      const slashes = nonInlineAncestor.url.startsWith(`${urlObj.protocol}//`);
       if (urlObj) {
-        return (
-          urlObj.protocol + (urlObj.slashes ? '//' : '') + (urlObj.host || '')
-        );
+        return urlObj.protocol + (slashes ? '//' : '') + (urlObj.host || '');
       }
     }
   }