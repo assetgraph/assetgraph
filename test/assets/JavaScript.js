assetgraph/assetgraph

commit 129a57696d6e870a89b694b3c6e590ed51127f20

Just use acorn directly

lib/assets/JavaScript.js
@@ -1,6 +1,5 @@
 /* eslint no-redeclare: "off", block-scoped-var: "off" */
 const _ = require('lodash');
-const espree = require('espree-papandreou');
 const escodegen = require('escodegen-papandreou');
 const estraverse = require('estraverse-fb');
 const espurify = require('espurify');
@@ -8,6 +7,7 @@ const mozilla = require('source-map');
 const repeatString = require('repeat-string');
 const errors = require('../errors');
 const Text = require('./Text');
+const parseJavaScript = require('../parseJavaScript');
 
 // Avoid modelling "bare" imports as relations (import { uniq } from 'lodash')
 function isImportUrl(href) {
@@ -222,25 +222,16 @@ class JavaScript extends Text {
       (nonInlineAncestor && nonInlineAncestor.url) ||
       this.url ||
       `standalone-${this.id}.js`;
-    this._parseTree = espree.parse(text, {
+    this._parseTree = parseJavaScript(text, {
       // Don't incur the penalty of tracking source locations if source maps are disabled globally:
-      loc: !this.assetGraph || this.assetGraph.sourceMaps !== false,
-      comment: true,
+      locations: !this.assetGraph || this.assetGraph.sourceMaps !== false,
       tokens: true,
-      range: true,
-      attachComment: true,
-      ecmaVersion: 2019,
-      ecmaFeatures: {
-        jsx: true
-      },
+      ranges: true,
+      ecmaVersion: 10,
       sourceFile,
       sourceType
     });
-    escodegen.attachComments(
-      this._parseTree,
-      this._parseTree.comments,
-      this._parseTree.tokens
-    );
+
     if (this._sourceMap) {
       this._applySourceMapToParseTree();
     }
@@ -257,10 +248,11 @@ class JavaScript extends Text {
           this._parse(text, 'script');
           parsedAsScript = true;
         } catch (errorParsingAsScript) {
+          const message = `Parse error in ${this.urlOrDescription}\n${
+            errorParsingAsScript.message
+          }`;
           const err = new errors.ParseError({
-            message: `Parse error in ${this.urlOrDescription}\n${
-              errorParsingAsScript.message
-            } (line ${errorParsingAsScript.lineNumber})`,
+            message,
             line: errorParsingAsScript.lineNumber,
             index: errorParsingAsScript.index,
             asset: this

test/assets/JavaScript.js
@@ -19,7 +19,7 @@ describe('assets/JavaScript', function() {
       'to match',
       /parseErrorInInlineJavaScript\.html/
     );
-    expect(firstWarning.message, 'to match', /line 2\b/);
+    expect(firstWarning.message, 'to contain', '(2:8)');
   });
 
   it('should handle a test case that has a parse error in an external JavaScript asset', async function() {
@@ -33,7 +33,7 @@ describe('assets/JavaScript', function() {
 
     expect(firstWarning, 'to be an', Error);
     expect(firstWarning.message, 'to match', /parseError\.js/);
-    expect(firstWarning.message, 'to match', /line 6\b/);
+    expect(firstWarning.message, 'to contain', '(6:13)');
   });
 
   it('should handle a test case that has a parse error in an asset not in an assetgraph', function() {
@@ -349,7 +349,7 @@ describe('assets/JavaScript', function() {
 
     expect([errorSpy, warnSpy, infoSpy], 'to have calls satisfying', () => {
       infoSpy(
-        "Could not parse http://example.com/script.js as a module, fall back to script mode\nCannot use keyword 'await' outside an async function"
+        "Could not parse http://example.com/script.js as a module, fall back to script mode\nCannot use keyword 'await' outside an async function (1:0)"
       );
     });
   });
@@ -371,7 +371,7 @@ describe('assets/JavaScript', function() {
 
     expect([errorSpy, warnSpy, infoSpy], 'to have calls satisfying', () => {
       warnSpy(
-        'Parse error in http://example.com/script.js\nUnexpected token ) (line 1)'
+        'Parse error in http://example.com/script.js\nUnexpected token (1:5)'
       );
     });
   });

lib/assets/Asset.js
@@ -316,7 +316,7 @@ class Asset extends EventEmitter {
    *     CSSOM CSSStyleSheet object (https://github.com/NV/CSSOM).
    *
    * `JavaScript`
-   *     estree AST object (via acorn/espree).
+   *     estree AST object (via acorn).
    *
    * `Json`
    *     Regular JavaScript object (the result of JSON.parse on the decoded source).

lib/assets/Html.js
@@ -1,6 +1,6 @@
 /* eslint no-redeclare: "off", block-scoped-var: "off" */
 const _ = require('lodash');
-const espree = require('espree-papandreou');
+const parseJavaScript = require('../parseJavaScript');
 const { JSDOM, VirtualConsole } = require('jsdom');
 const virtualConsole = new VirtualConsole();
 const domspace = require('domspace');
@@ -859,9 +859,8 @@ class Html extends Text {
               .replace(/^\s*\{(.*)\}\s*$/, '$1')}});`;
             let parseTree = null; // Must be set to something falsy each time we make it here
             try {
-              parseTree = espree.parse(javaScriptObjectLiteral, {
-                sourceType: 'module',
-                jsx: true
+              parseTree = parseJavaScript(javaScriptObjectLiteral, {
+                sourceType: 'module'
               });
             } catch (e) {}
 

lib/parseExpression.js
@@ -1,8 +1,7 @@
-const espree = require('espree-papandreou');
+const parseJavaScript = require('./parseJavaScript');
 
 module.exports = function parseExpression(str) {
-  return espree.parse(`(${String(str)})`, {
-    sourceType: 'module',
-    jsx: true
+  return parseJavaScript(`(${String(str)})`, {
+    sourceType: 'module'
   }).body[0].expression;
 };

lib/parseJavaScript.js
@@ -1,0 +1,81 @@
+const acorn = require('acorn');
+const Parser = acorn.Parser.extend(require('acorn-jsx')());
+const escodegen = require('escodegen-papandreou');
+
+// From espree
+function convertAcornCommentToEsprimaComment(
+  block,
+  text,
+  start,
+  end,
+  startLoc,
+  endLoc
+) {
+  const comment = {
+    type: block ? 'Block' : 'Line',
+    value: text
+  };
+
+  if (typeof start === 'number') {
+    comment.start = start;
+    comment.end = end;
+    comment.range = [start, end];
+  }
+
+  if (typeof startLoc === 'object') {
+    comment.loc = {
+      start: startLoc,
+      end: endLoc
+    };
+  }
+
+  return comment;
+}
+
+function parseJavaScript(code, options = {}) {
+  const comments = [];
+  const tokens = [];
+  const parser = new Parser(
+    {
+      ...options,
+      // Collect comments
+      onComment(block, text, start, end, startLoc, endLoc) {
+        comments.push(
+          convertAcornCommentToEsprimaComment(
+            block,
+            text,
+            start,
+            end,
+            startLoc,
+            endLoc
+          )
+        );
+      },
+      onToken(token) {
+        tokens.push(token);
+      }
+    },
+    code
+  );
+
+  const ast = parser.parse();
+  ast.comments = comments;
+  ast.tokens = tokens;
+  const lastToken = tokens.pop();
+  // Adjustments from espree:
+  if (ast.range) {
+    ast.range[0] = ast.body.length ? ast.body[0].range[0] : ast.range[0];
+    ast.range[1] = lastToken ? lastToken.range[1] : ast.range[1];
+  }
+  if (ast.loc) {
+    ast.loc.start = ast.body.length ? ast.body[0].loc.start : ast.loc.start;
+    ast.loc.end = lastToken ? lastToken.loc.end : ast.loc.end;
+  }
+
+  if (options.locations && options.ranges) {
+    escodegen.attachComments(ast, ast.comments, ast.tokens);
+  }
+  return ast;
+}
+
+module.exports = parseJavaScript;

test/replaceDescendantNode.js
@@ -1,12 +1,12 @@
 /* global describe, it */
 const replaceDescendantNode = require('../lib/replaceDescendantNode');
-const espree = require('espree-papandreou');
+const parseJavaScript = require('../lib/parseJavaScript');
 const escodegen = require('escodegen-papandreou');
 const expect = require('./unexpected-with-plugins');
 
 describe('replaceDescendantNode', function() {
   it('should replace oldNode with newNode in an AST', function() {
-    const ast = espree.parse('var foo = 123 + 456;');
+    const ast = parseJavaScript('var foo = 123 + 456;');
     const oldNode = ast.body[0].declarations[0].init.right;
     replaceDescendantNode(ast, oldNode, {
       type: 'Literal',

test/transforms/pullGlobalsIntoVariables.js
@@ -1,12 +1,12 @@
 /* global describe, it */
 const expect = require('../unexpected-with-plugins');
 const AssetGraph = require('../../lib/AssetGraph');
-const espree = require('espree-papandreou');
+const parseJavaScript = require('../../lib/parseJavaScript');
 const escodegen = require('escodegen-papandreou');
 
 function getFunctionBodySource(fn) {
   return escodegen.generate(
-    espree.parse(fn.toString().replace(/^function\s*\(\) \{\n|\}$/g, ''))
+    parseJavaScript(fn.toString().replace(/^function\s*\(\) \{\n|\}$/g, ''))
   );
 }
 

commit 243f367dca0cc12d533f3a6a77996b8b1a8d0a2c

Preserve comments despite assetGraph.sourceMaps being false
Broke in #979

lib/assets/JavaScript.js
@@ -223,8 +223,7 @@ class JavaScript extends Text {
       this.url ||
       `standalone-${this.id}.js`;
     this._parseTree = parseJavaScript(text, {
-      // Don't incur the penalty of tracking source locations if source maps are disabled globally:
-      locations: !this.assetGraph || this.assetGraph.sourceMaps !== false,
+      locations: true,
       tokens: true,
       ranges: true,
       ecmaVersion: 10,

test/assets/JavaScript.js
@@ -127,6 +127,21 @@ describe('assets/JavaScript', function() {
     expect(javaScript.text, 'to match', /Copyright blablabla/);
   });
 
+  it('should preserve comments despite assetGraph.sourceMaps being false', async function() {
+    const assetGraph = new AssetGraph();
+    assetGraph.sourceMaps = false;
+
+    const javaScript = assetGraph.addAsset({
+      type: 'JavaScript',
+      text: "// foo\nalert('bar');"
+    });
+
+    javaScript.parseTree; // eslint-disable-line no-unused-expressions
+    javaScript.markDirty();
+
+    expect(javaScript.text, 'to contain', '// foo');
+  });
+
   it('should handle a test case with JavaScript assets that have regular comments as the first non-whitespace tokens', async function() {
     const assetGraph = new AssetGraph({
       root: pathModule.resolve(__dirname, '../../testdata/assets/JavaScript/')