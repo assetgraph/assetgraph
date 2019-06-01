efa34a31b0965b44b2c76d2bd68d937d56886762
Peter Müller <munter@fumle.dk>
2019-06-01 17:57:47 +0200

    Added file based test for JavaScriptPaintWorklet

--- test/relations/JavaScriptPaintWorklet.js
+++ test/relations/JavaScriptPaintWorklet.js
@@ -5,7 +5,7 @@ const AssetGraph = require('../../lib/AssetGraph');
 
 const testRoot = pathModule.resolve(
   __dirname,
-  '../../testdata/relations/JavaScriptFetch'
+  '../../testdata/relations/JavaScriptPaintWorklet'
 );
 
 describe('relations/JavaScriptPaintWorklet', function() {
@@ -13,12 +13,31 @@ describe('relations/JavaScriptPaintWorklet', function() {
     const assetGraph = new AssetGraph({
       root: testRoot
     });
-    await assetGraph.addAsset({
-      type: 'JavaScript',
-      text: `CSS.paintWorklet.addModule('./relativeurl.js');`
-    });
+    await assetGraph.loadAssets('index.html');
+    await assetGraph.populate();
 
     expect(assetGraph, 'to contain assets', 'JavaScript', 2);
-    expect(assetGraph, 'to contain relations', 'JavaScriptPaintWorklet', 1);
+    expect(assetGraph, 'to contain relation', 'JavaScriptPaintWorklet');
   });
+
+  it('should read the href correctly', async function() {
+    const assetGraph = new AssetGraph({
+      root: testRoot
+    });
+    await assetGraph.loadAssets('index.html');
+    await assetGraph.populate();
+
+    const relation = assetGraph.findRelations({ type: 'JavaScriptPaintWorklet' })[0];
+
+    expect(relation, 'to satisfy', {
+      href: '/js/paintworklet.js',
+      to: {
+        path: '/js/',
+        fileName: 'paintworklet.js'
+      }
+    });
+  });
+
+  // TODO: Test that assetgraph population emits WARN when hitting a relative URL in the href
+  // Relative URLs are resolved from the current pages path, not the including javascript parents path
 });
