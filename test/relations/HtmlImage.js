05edfb1043dfcf9c7a70feb55d4b35f708f30347
Andreas Lind <andreaslindpetersen@gmail.com>
2019-06-28 01:28:27 +0200

    HtmlImage: Allow an alternative attribute name
    Context: #1013

--- test/relations/HtmlImage.js
+++ test/relations/HtmlImage.js
@@ -90,4 +90,18 @@ describe('relations/HtmlImage', function() {
       expect(htmlImage.decoding, 'to equal', 'async');
     });
   });
+
+  describe('with an alternative attributeName', function() {
+    it('should update that attribute when the href is changed', function() {
+      initial('<!DOCTYPE html><html><body><img src="foo.png"></body></html>');
+
+      const htmlImage = htmlAsset.outgoingRelations[0];
+      htmlImage.attributeName = 'data-src';
+      htmlImage.node.removeAttribute('src');
+      htmlImage.href = 'bar.png';
+      htmlAsset.markDirty();
+
+      expect(htmlAsset.text, 'to contain', '<img data-src="bar.png">');
+    });
+  });
 });
