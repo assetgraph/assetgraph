8a64a2b7a2f6eed933627b070b5e1c581797b6d8
Andreas Lind <andreaslindpetersen@gmail.com>
2019-10-07 00:13:04 +0200

    Update unexpected-assetgraph to ^1.1.0

--- test/unexpected-with-plugins.js
+++ test/unexpected-with-plugins.js
@@ -1,43 +1,7 @@
-const expect = require('unexpected')
+module.exports = require('unexpected')
   .clone()
   .use(require('unexpected-sinon'))
   .use(require('unexpected-dom'))
   .use(require('unexpected-set'))
   .use(require('unexpected-assetgraph'))
   .use(require('magicpen-prism'));
-
-const parseJavaScript = require('../lib/parseJavaScript');
-const escodegen = require('escodegen');
-
-function toAst(stringOrAssetOrFunctionOrAst) {
-  if (typeof stringOrAssetOrFunctionOrAst === 'string') {
-    return parseJavaScript(stringOrAssetOrFunctionOrAst);
-  } else if (stringOrAssetOrFunctionOrAst.isAsset) {
-    return stringOrAssetOrFunctionOrAst.parseTree;
-  } else if (typeof stringOrAssetOrFunctionOrAst === 'function') {
-    return {
-      type: 'Program',
-      body: parseJavaScript(`!${stringOrAssetOrFunctionOrAst.toString()}`)
-        .body[0].expression.argument.body.body
-    };
-  } else {
-    return stringOrAssetOrFunctionOrAst;
-  }
-}
-
-function prettyPrintAst(ast) {
-  return escodegen.generate(ast);
-}
-
-expect.addAssertion(
-  '<object|string|function|AssetGraph.asset> [not] to have the same AST as <object|string|function|AssetGraph.asset>',
-  function(expect, subject, value) {
-    expect(
-      prettyPrintAst(toAst(subject)),
-      '[not] to equal',
-      prettyPrintAst(toAst(value))
-    );
-  }
-);
-
-module.exports = expect;
