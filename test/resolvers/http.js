cfdf85ea66ea4a94a93bf38cf5e51daeb42de54f
Andreas Lind <andreas.lind@peakon.com>
2021-01-21 17:55:13 +0100

    Sidestep teepee's buggy expansion of {...} in urls
    Fixes #1144
    
    (cherry picked from commit 26e89cd90d4e7ef1b972c08d8df6b296463118fe)

--- test/resolvers/http.js
+++ test/resolvers/http.js
@@ -113,4 +113,30 @@ describe('resolvers/http', function() {
       warnSpy('Invalid Content-Type response header received: &');
     });
   });
+
+  // Regression test for https://github.com/assetgraph/assetgraph/issues/1144
+  it('should not break when a url contains curly braces', async function () {
+    httpception({
+      request:
+        'GET https://api.amplitude.com/httpapi?api_key=e102edba5e9caea6b89e3c04fac87a4d&event={%22event_type%22:%22Overall%20Active%20User%22,%22event_properties%22:{%22Email%20Send%20Time%22:%222019-05-10T10:43:42%22,%22Current%20Date%22:%2210/05/2019%22,%22UserId%22:5698327,%22campaign%22:%22Daily%20Digest%22,%22Platform%22:%22E-Mail%22},%22user_id%22:%225698327%22}',
+      response: {
+        statusCode: 200,
+        body: '<html><body>Hello, world!</body></html>',
+      },
+    });
+
+    const assetGraph = new AssetGraph();
+    const warnSpy = sinon.spy().named('warn');
+    assetGraph.on('warn', warnSpy);
+    await assetGraph.loadAssets(
+      'https://api.amplitude.com/httpapi?api_key=e102edba5e9caea6b89e3c04fac87a4d&event={%22event_type%22:%22Overall%20Active%20User%22,%22event_properties%22:{%22Email%20Send%20Time%22:%222019-05-10T10:43:42%22,%22Current%20Date%22:%2210/05/2019%22,%22UserId%22:5698327,%22campaign%22:%22Daily%20Digest%22,%22Platform%22:%22E-Mail%22},%22user_id%22:%225698327%22}'
+    );
+    await assetGraph.populate();
+
+    expect(assetGraph, 'to contain asset', {
+      type: 'Html',
+      url:
+        'https://api.amplitude.com/httpapi?api_key=e102edba5e9caea6b89e3c04fac87a4d&event={%22event_type%22:%22Overall%20Active%20User%22,%22event_properties%22:{%22Email%20Send%20Time%22:%222019-05-10T10:43:42%22,%22Current%20Date%22:%2210/05/2019%22,%22UserId%22:5698327,%22campaign%22:%22Daily%20Digest%22,%22Platform%22:%22E-Mail%22},%22user_id%22:%225698327%22}',
+    });
+  });
 });
